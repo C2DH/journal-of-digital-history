3609dafa2a77fbd0f37b661ba0ef501e6e32afa8
Daniele Guido <gui.daniele@gmail.com>
2020-10-12 13:54:56 +0200

    Add i18n translations

--- src/App.js
+++ src/App.js
@@ -1,192 +1,198 @@
 import React, { Suspense, lazy } from 'react';
-import logo from './assets/images/jdh-logo.svg';
 import {
   BrowserRouter,
-  Routes,
+  Switch,
   Route,
-  Link,
-  NavLink,
-  Outlet,
+  // Link,
+  Redirect,
   useParams,
+  useRouteMatch,
 } from "react-router-dom";
+import i18n from 'i18next';
+import { initReactI18next, useTranslation } from 'react-i18next'
+import { getStartLang, LANGUAGE_PATH, LANGUAGES } from './logic/language';
+import translations from './translations'
 
-import { Nav, Navbar, Container } from "react-bootstrap";
-const Home = lazy(() => import('./pages/Home'));
-const About = lazy(() => import('./pages/About'));
-const Editor = lazy(() => import('./pages/Editor'));
+import Header from './components/Header'
 
-export default function App(){
-  return (
-    <BrowserRouter>
-      <Navbar fixed="top" bg="light" variant="light" className="border-bottom">
-        <Container>
-          <Navbar.Brand href="#home" className="d-flex align-items-center">
-            <img
-              alt=""
-              src={logo}
-              height="25px"
-              className="flex-grow-1 mr-1"
-            />
-            <span>Journal of Digital history</span>
-          </Navbar.Brand>
-          <Nav className="ml-auto">
-            <Nav.Item>
-              <Nav.Link as={NavLink} to="/" end>Home</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link as={NavLink} to="#pricing" end>references</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link as={NavLink} to="#pricing" end>datasets</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link as={NavLink} to="/about">about</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link as={NavLink} to="/editor">editor</Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Container>
-      </Navbar>
-      <main>
-        {
-          // <nav>
-          //   <Link to="/">Issues</Link>
-          //   <Link to="/about">About</Link>
-          // </nav>
-        }
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/issues" element={<Issues />}>
-              <Route path="/" element={<IssueIndex />}/>
-              <Route path="/issue/:slug" element={<Issue />} /*slug the placeholder matching any value find*//>
-            </Route>
-            <Route path="/article/:slug" element={<Article />}/>
-            <Route path="/about" element={<About />}/>
-            <Route path="/editor" element={<Editor />}/>
-            <Route path="*" element={<NotFound />} /* page not found *//>
-          </Routes>
-        </Suspense>
-      </main>
-    </BrowserRouter>
-  );
-}
-
-
-function NotFound(){
+const Home = lazy(() => import('./pages/Home'))
+const About = lazy(() => import('./pages/About'))
+const AbstractSubmission = lazy(() => import('./pages/AbstractSubmission'))
+const NotFound = () => {
   return (<div>
     <h1>Not found</h1>
     <p>sorry your page was not found</p>
   </div>);
 }
 
-function Issues() {
-  return (
-    <div>
-      <h1>Issues</h1>
+const { startLangShort, lang } = getStartLang()
+console.info('start language:', lang, startLangShort)
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources: translations,
+    lng: lang,
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  })
 
-      <Outlet />
-    </div>
-  );
+function LangRoutes() {
+  const { path } = useRouteMatch()
+  console.info('LangRoutes render', path)
+  return (
+    <Switch>
+      <Route exact path={`${path}`}>
+        <Home />
+      </Route>
+      <Route exact path={`${path}/about`}>
+        <About />
+      </Route>
+      <Route exact path={`${path}/submit`}>
+        <AbstractSubmission/>
+      </Route>
+      <Route path={`${path}*`}>
+        <NotFound />
+      </Route>
+    </Switch>
+  )
 }
 
-function IssueIndex() {
+function AppRoutes() {
   return (
-    <ul>
-      {Object.entries(issues).map(([slug, { name, img }]) => (
-        <li key={slug}>
-          <Link to={`/issue/${slug}`}>
-            <h2>{name}</h2>
-            <img src={img} alt={name} />
-          </Link>
-        </li>
-      ))}
-    </ul>
-  );
+    <Switch>
+      <Redirect from="/" exact to={startLangShort} />
+      <Route path={LANGUAGE_PATH}>
+        <LangRoutes />
+      </Route>
+      <Route path="*">
+        <NotFound />
+      </Route>
+    </Switch>
+  )
 }
 
-
-function Issue(){
-  const {slug} = useParams();
-  const issue = issues[slug];
-  if (!issue){
-    return <h2>issue not found</h2>
-  }
-  const { name, img} = issue;
+export default function App() {
   return (
-    <div>
-      <div>
-        <h2>{name}</h2>
-        <img src={img} alt={name} />
-      </div>
-      <div>
-        <ul>
-        {Object.entries(articles).map(([slug, { name, img }]) => (
-          <li key={slug}>
-             <Link to={`/article/${slug}`}>
-              <h2>{name}</h2>
-              <img src={img} alt={name} />
-            </Link>
-          </li>
-        ))}
-        </ul>
-      </div>
-   </div>
-
-  );
-}
-
-
-function Article(){
-  const {slug} = useParams();
-  const article = articles[slug];
-  if (!article){
-    return <h2>article not found</h2>
-  }
-  const { name, img} = article;
-  return (
-      <div>
-        <h2>{name}</h2>
-        <img src={img} alt={name} />
-      </div>
-  );
+    <BrowserRouter>
+      <Header lang={startLangShort} availableLanguages={LANGUAGES}/>
+      <main>
+        <Suspense fallback={<div>Loading...</div>}>
+          <AppRoutes />
+        </Suspense>
+      </main>
+    </BrowserRouter>
+  )
 }
 
 
-const issues = {
-  "twitter-volume": {
-    name: "the twitter volume",
-    img:
-      "/img/JODI_twitter_volume.png?$SNKRS_COVER_WD$&align=0,1"
-  },
-  "digital-advances": {
-    name: "digital adv@nces",
-    img:
-      "/img/JODI_digital_advance.png?$SNKRS_COVER_WD$&align=0,1"
-  },
-  "network-analysis": {
-    name: "network analysis",
-    img:
-      "/img/JODI_network_analysis.png?$SNKRS_COVER_WD$&align=0,1"
-  }
-};
-
-
-const articles = {
-  "twitter-volume-article-1": {
-    name: "Le Centenaire de la bataille de Verdun sur Twitter",
-    img:
-      "/img/JODI_article_1_twitter_volume.png?$SNKRS_COVER_WD$&align=0,1"
-  },
-  "twitter-volume-article-2": {
-    name: "The history of the automobile industry is filled with partnerships",
-    img:
-      "/img/JODI_article_2_twitter_volume.png?$SNKRS_COVER_WD$&align=0,1"
-  },
-  "twitter-volume-article-3": {
-    name: "The protesters need a voice: “When we unite, we win.”",
-    img:
-      "/img/JODI_article_3_twitter_volume.png?$SNKRS_COVER_WD$&align=0,1"
-  }
-};
+// 
+// function Issues() {
+//   return (
+//     <div>
+//       <h1>Issues</h1>
+// 
+//       <Outlet />
+//     </div>
+//   );
+// }
+// 
+// function IssueIndex() {
+//   return (
+//     <ul>
+//       {Object.entries(issues).map(([slug, { name, img }]) => (
+//         <li key={slug}>
+//           <Link to={`/issue/${slug}`}>
+//             <h2>{name}</h2>
+//             <img src={img} alt={name} />
+//           </Link>
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
+// 
+// 
+// function Issue(){
+//   const {slug} = useParams();
+//   const issue = issues[slug];
+//   if (!issue){
+//     return <h2>issue not found</h2>
+//   }
+//   const { name, img} = issue;
+//   return (
+//     <div>
+//       <div>
+//         <h2>{name}</h2>
+//         <img src={img} alt={name} />
+//       </div>
+//       <div>
+//         <ul>
+//         {Object.entries(articles).map(([slug, { name, img }]) => (
+//           <li key={slug}>
+//              <Link to={`/article/${slug}`}>
+//               <h2>{name}</h2>
+//               <img src={img} alt={name} />
+//             </Link>
+//           </li>
+//         ))}
+//         </ul>
+//       </div>
+//    </div>
+// 
+//   );
+// }
+// 
+// 
+// function Article(){
+//   const {slug} = useParams();
+//   const article = articles[slug];
+//   if (!article){
+//     return <h2>article not found</h2>
+//   }
+//   const { name, img} = article;
+//   return (
+//       <div>
+//         <h2>{name}</h2>
+//         <img src={img} alt={name} />
+//       </div>
+//   );
+// }
+// 
+// 
+// const issues = {
+//   "twitter-volume": {
+//     name: "the twitter volume",
+//     img:
+//       "/img/JODI_twitter_volume.png?$SNKRS_COVER_WD$&align=0,1"
+//   },
+//   "digital-advances": {
+//     name: "digital adv@nces",
+//     img:
+//       "/img/JODI_digital_advance.png?$SNKRS_COVER_WD$&align=0,1"
+//   },
+//   "network-analysis": {
+//     name: "network analysis",
+//     img:
+//       "/img/JODI_network_analysis.png?$SNKRS_COVER_WD$&align=0,1"
+//   }
+// };
+// 
+// 
+// const articles = {
+//   "twitter-volume-article-1": {
+//     name: "Le Centenaire de la bataille de Verdun sur Twitter",
+//     img:
+//       "/img/JODI_article_1_twitter_volume.png?$SNKRS_COVER_WD$&align=0,1"
+//   },
+//   "twitter-volume-article-2": {
+//     name: "The history of the automobile industry is filled with partnerships",
+//     img:
+//       "/img/JODI_article_2_twitter_volume.png?$SNKRS_COVER_WD$&align=0,1"
+//   },
+//   "twitter-volume-article-3": {
+//     name: "The protesters need a voice: “When we unite, we win.”",
+//     img:
+//       "/img/JODI_article_3_twitter_volume.png?$SNKRS_COVER_WD$&align=0,1"
+//   }
+// };
---
+++ src/logic/language.js
@@ -0,0 +1,69 @@
+import { useParams } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+import { matchPath } from "react-router";
+import intersection from 'lodash/intersection'
+import find from 'lodash/find'
+
+const LANGUAGES = process.env.REACT_APP_LANGUAGES ?? ['en_US', 'fr_FR'];
+const LANGUAGES_SHORTS = LANGUAGES.map((l) => l.split('_')[0])
+const DEFAULT_LANGUAGE = process.env.REACT_APP_DEFAULT_LANGUAGE ?? 'en_US';
+const DEFAULT_LANGUAGE_SHORT = DEFAULT_LANGUAGE.split('_')[0]
+const LANGUAGE_PATH = `/:lang(${LANGUAGES_SHORTS.join('|')})`
+
+const getStartLang = () => {
+  const langMatch = matchPath(window.location.pathname, {
+    path: LANGUAGE_PATH,
+    exact: false,
+    strict: false,
+  })
+  let startLangShort = langMatch?.params?.lang
+  if (!startLangShort || !LANGUAGES_SHORTS.includes(startLangShort)) {
+    // get default short language from browser
+    const browserLangsShort = window.navigator?.languages ?? []
+    console.info('browser languages detected:', browserLangsShort)
+    const availablesLangsShort = intersection(browserLangsShort, LANGUAGES_SHORTS)
+    startLangShort = availablesLangsShort.length > 0
+      ? availablesLangsShort[0]
+      : DEFAULT_LANGUAGE_SHORT
+  }
+  return {
+    startLangShort,
+    lang: find(LANGUAGES, (l) => l.indexOf(startLangShort) === 0)
+  }
+}
+
+function namespacePath(path, lang) {
+  let pathWithLang = `/${lang}`
+  if (path[0] === '/') {
+    pathWithLang += path
+  } else {
+    pathWithLang += `/${path}`
+  }
+  return pathWithLang
+}
+
+const useToWithLang = (to) => {
+  const { i18n } = useTranslation()
+  let { lang } = useParams()
+  if (!lang) {
+    // NOTE: Workaround when no lang in current path
+    // fallback to current i81n language ...
+    lang = i18n.language.split('_')[0]
+  }
+
+  if (typeof to === 'string') {
+    return namespacePath(to, lang)
+  } else {
+    return {
+      ...to,
+      pathname: namespacePath(to.pathname, lang),
+    }
+  }
+}
+
+export {
+  getStartLang,
+  useToWithLang,
+  LANGUAGE_PATH,
+  LANGUAGES
+}
