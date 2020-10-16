0444376d465898c45ae9995d980987ad76dd07e4
Daniele Guido <gui.daniele@gmail.com>
2020-10-16 17:27:40 +0200

    remove unused components

--- src/components/EditorTableOfContents/EditorTableOfContents.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function EditorTableOfContents() {
-  return (
-    <div>
-      Drag n drop Table Of contents, paragraph by paragraph
-    </div>
-  )
-}
