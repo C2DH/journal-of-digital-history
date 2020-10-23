bc0f2b23b1100e19205f8dbf860f72cca5f02f52
Daniele Guido <gui.daniele@gmail.com>
2020-10-23 17:44:09 +0200

    finalize submission

--- src/pages/AbstractSubmission.js
+++ src/pages/AbstractSubmission.js
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Container, Form, Button, Col, Row, Jumbotron,Badge } from 'react-bootstrap'
 import { useStore } from '../store'
+import { getValidatorResultWithAbstractSchema } from '../logic/validation'
+import Author from '../models/Author'
+import Dataset from '../models/Dataset'
 import FormGroupWrapper from '../components/Forms/FormGroupWrapper'
-import FormAuthorsList from '../components/Forms/FormAuthorsList'
 import FormAuthorContact from '../components/Forms/FormAuthorContact'
-import FormAbstractDataset from '../components/Forms/FormAbstractDataset'
+import FormAbstractGenericSortableList from '../components/Forms/FormAbstractGenericSortableList'
+
 
 const AbstractSubmissionPreview = ({ results }) => {
   const { t } = useTranslation()
@@ -17,15 +20,21 @@ const AbstractSubmissionPreview = ({ results }) => {
         maxHeight: '50vh',
         overflow: 'scroll'
       }}>
-      {results.map(({ value, isValid=false, label }) => (
-        <div >
-          <Badge variant={ isValid ? 'success': 'transparent' } pill>{t(label)}</Badge>
+      {results.map(({ value, isValid, label }, i) => (
+        <div key={`result-${i}`} >
+          <Badge variant={ isValid ? 'success': 'transparent' } pill>{t(label)}
+            {value === null && ' (empty)'}
+            {isValid === 'false' && ' (error)'}
+          </Badge>
           <p style={{
             height: '25px',
             overflow: 'hidden',
             textOverflow: 'ellipsis',
             whiteSpace: 'nowrap',
-          }}>{value}</p>
+          }}>
+          {typeof value === 'string' && value}
+          {typeof value === 'object' && value && JSON.stringify(value)}
+          </p>
         </div>
       ))}
       </div>
@@ -33,12 +42,16 @@ const AbstractSubmissionPreview = ({ results }) => {
   )
 }
 
+
 export default function AbstractSubmission(props) {
   const { t } = useTranslation()
+  const [errors, setErrors] = useState([])
   const [ results, setResults ] = useState([
     { id: 'title', value: null, label: 'pages.abstractSubmission.articleTitle' },
     { id: 'abstract', value: null, label: 'pages.abstractSubmission.articleAbstract' },
     { id: 'contact', value: null, label: 'pages.abstractSubmission.articleContact' },
+    { id: 'authors', value: null, label: 'pages.abstractSubmission.AuthorsSectionTitle' },
+    { id: 'datasets', value: null, label: 'pages.abstractSubmission.DataSectionTitle' }
   ])
 
   useEffect(() => {
@@ -47,12 +60,16 @@ export default function AbstractSubmission(props) {
   });
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.info('handleSubmit', results)
+    const submission = results.reduce((acc, el) => {
+      acc[el.id] = el.value
+      return acc
+    } , {})
+    const result = getValidatorResultWithAbstractSchema(submission)
+    setErrors(result.errors)
+    console.info('handleSubmit', submission, result.valid)
   }
 
-  console.info('changed', props)
   const handleChange = ({ id, value, isValid }) => {
-    console.info(id, value, isValid)
     setResults(results.map((d) => {
       if (d.id === id) {
         return { ...d, value, isValid }
@@ -78,25 +95,47 @@ export default function AbstractSubmission(props) {
         <Row>
           <Col md={8}>
             <h3>A title and an abstract</h3>
-            <FormGroupWrapper id='title' as='textarea' schemaId='#/properties/title' rows={3} 
+            <FormGroupWrapper as='textarea' schemaId='#/definitions/title' rows={3}
               label='pages.abstractSubmission.articleTitle'
               ignoreWhenLengthIslessThan={1}
-              onChange={handleChange}
+              onChange={({ value, isValid }) => handleChange({ id: 'title', value, isValid })}
             />
-            <FormGroupWrapper id='abstract' as='textarea' schemaId='#/properties/abstract' rows={5}
+            <FormGroupWrapper as='textarea' schemaId='#/definitions/abstract' rows={5}
               label='pages.abstractSubmission.articleAbstract'
-              ignoreWhenLengthIslessThan={1} 
-              onChange={handleChange}
+              ignoreWhenLengthIslessThan={1}
+              onChange={({ value, isValid }) => handleChange({ id: 'abstract', value, isValid })}
             />
-            <hr/>
+
+            <hr />
+
             <h3>{t('pages.abstractSubmission.ContactPointSectionTitle')}</h3>
-            <FormAuthorContact groupId='contact' onChange={handleChange} />
+            <FormAuthorContact
+              onChange={({ value, isValid }) => handleChange({ id: 'contact', value, isValid })}
+            />
+
             <hr />
+
             <h2>{t('pages.abstractSubmission.AuthorsSectionTitle')}</h2>
-            <FormAuthorsList />
+            <FormAbstractGenericSortableList
+              onChange={({ items, isValid }) => handleChange({
+                id: 'authors',
+                value: items,
+                isValid
+              })}
+              ItemClass={Author}
+              listItemComponentTagName='FormAbstractAuthorsListItem' />
+
             <hr />
+
             <h2>{t('pages.abstractSubmission.DataSectionTitle')}</h2>
-            <FormAbstractDataset />
+            <FormAbstractGenericSortableList
+              onChange={({ items, isValid }) => handleChange({
+                id: 'datasets',
+                value: items,
+                isValid
+              })}
+              ItemClass={Dataset}
+              listItemComponentTagName='FormAbstractDatasetsListItem' />
             <hr />
           </Col>
           <Col md={4}>
@@ -104,14 +143,15 @@ export default function AbstractSubmission(props) {
               position: 'sticky',
               top: '120px'
             }}>
-            <AbstractSubmissionPreview results={results} /> 
+            <AbstractSubmissionPreview results={results} />
             </div>
           </Col>
         </Row>
-        
+
         <Button variant="primary" size="lg" className="px-4" type="submit">
           Submit
         </Button>
+        <pre>{JSON.stringify(errors, null, 2)}</pre>
       </Form>
     </Container>
   )
