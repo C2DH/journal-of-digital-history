20b509e5bfffdd100f58e4c1a7a9f25da347ac8c
Daniele Guido <gui.daniele@gmail.com>
2020-10-29 13:33:04 +0100

    add transitions to list order

--- src/components/Forms/FormAbstractGenericSortableList.js
+++ src/components/Forms/FormAbstractGenericSortableList.js
@@ -4,6 +4,8 @@ import { Button } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
 import FormAbstractAuthorsListItem from './FormAbstractAuthorsListItem'
 import FormAbstractDatasetsListItem from './FormAbstractDatasetsListItem'
+import { useTransition, animated } from 'react-spring'
+
 
 const components = {
   'FormAbstractAuthorsListItem': FormAbstractAuthorsListItem,
@@ -40,29 +42,55 @@ const FormAbstractGenericSortableList = ({ onChange, ItemClass, listItemComponen
   }
   const ListItemComponent = components[listItemComponentTagName]
 
+  let height = 0
+  const transitions = useTransition(
+    items.map((data, i) => {
+      const y = +height
+      height += 300
+
+      console.info(data, i, height)
+      return { ...data, y, keykey: 'aaaaaa-' + data.id }
+    }),
+    d => d.keykey,
+    {
+      from: { opacity: 0.5 },
+      leave: { opacity: 0.5 },
+      enter: ({ y }) => ({ y, opacity:1 }),
+      update: ({ y }) => ({ y })
+    }
+  )
+  console.info(transitions)
+
   return (
     <div>
-      {items.map((item, idx) => (
-        <div key={`dataset-url-${item.id}`} className="d-flex align-items-top mb-2 pl-2 pr-1 pb-2 pt-0 border rounded shadow-sm">
+    <div className="position-relative w-100" style={{ height }}>
+      {transitions.map(({ item, props: { y, ...rest }, key }, index) => (
+        <animated.div
+          key={key} className="generic-list-item"
+          style={{ zIndex: items.length - index, transform: y.interpolate(y => `translate3d(0,${y}px,0)`), ...rest }}
+        >
+        <div className="d-flex align-items-top mb-2 pl-2 pr-1 pb-2 pt-0 border rounded shadow-sm">
           <ListItemComponent className="w-100 mt-2"  item={item} onChange={handleChange} />
           <div className="flex-shrink-1">
             <Button size="sm" className="d-block rounded-circle p-0 m-3" style={{height: '25px', width:'25px', lineHeight: '25px'}} variant="warning"
               onClick={() => removeItem(item)}>✕</Button>
-            { idx > 0 && (
+            { index > 0 && (
               <Button size="sm" className="d-block rounded-circle p-0 m-3" style={{height: '25px', width:'25px', lineHeight: '25px'}} variant="secondary"
-                onClick={() => moveItem(idx, idx-1)}>↑</Button>
+                onClick={() => moveItem(index, index-1)}>↑</Button>
             )}
-            { idx < items.length - 1 && (
+            { index < items.length - 1 && (
               <Button size="sm" className="d-block rounded-circle p-0 m-3" style={{height: '25px', width:'25px', lineHeight: '25px'}} variant="secondary"
-                onClick={() => moveItem(idx, idx+1)}>↓</Button>
+                onClick={() => moveItem(index, index+1)}>↓</Button>
             )}
           </div>
-        </div>
+          </div>
+        </animated.div>
       ))}
-      <pre>{JSON.stringify(items)}</pre>
-      <div className="text-center">
-        <Button size="sm" variant="secondary" onClick={addNewItem}>{t('actions.addLink')}</Button>
-      </div>
+    </div>
+    <pre>{JSON.stringify(items)}</pre>
+    <div className="text-center">
+      <Button size="sm" variant="secondary" onClick={addNewItem}>{t('actions.addLink')}</Button>
+    </div>
     </div>
   )
 }
