e305d36c57efacec1ceca03e0659ce27a1adc66f
Daniele Guido <gui.daniele@gmail.com>
2020-11-05 17:32:27 +0100

    Update Abstract.js

--- src/pages/Abstract.js
+++ src/pages/Abstract.js
@@ -1,16 +1,41 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
-
+import mockAbstract from '../data/mock-api/mock-abstract.json'
+import ArticleAuthor from '../components/ArticleText/ArticleAuthor'
+import Author from '../models/Author'
 
 const Abstract = ({ results }) => {
   const { t } = useTranslation()
   return (
     <>
-      <Container>
-        <h1 className="display-4">
-            {t('abstract')} submitted!
-        </h1>
+      <Container className="page">
+        <Row>
+          <Col md={{offset: 2, span:6}}>
+        <h3 >
+            {t('pages.abstract')} submitted!
+        </h3>
+          <h1 className="mt-5">{mockAbstract.title}</h1>
+          <p>{mockAbstract.abstract}</p>
+
+          </Col>
+        </Row>
+        <Row className="pt-4">
+          <Col md={{offset: 2, span:6}}>
+            <h3>{t('pages.abstract.authors')}</h3>
+          </Col>
+          {mockAbstract.authors.map((author,i) => (
+            <Col key={i} md={{offset: 2, span: 3}}>
+              <ArticleAuthor author={new Author(author)} />
+            </Col>
+          ))}
+        </Row>
+        <Row className="pt-4">
+          <Col md={{offset: 2, span:6}}>
+            <h3>{t('pages.abstract.contact')}</h3>
+            <ArticleAuthor author={new Author(mockAbstract.contact)} />
+          </Col>
+        </Row>
       </Container>
     </>
   )
