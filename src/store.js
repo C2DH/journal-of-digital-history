3a958492581d355ca6f47fa7f5db5a994ee26a7e
Daniele Guido <gui.daniele@gmail.com>
2020-11-11 16:56:41 +0100

    use localstorage

--- src/store.js
+++ src/store.js
@@ -1,6 +1,18 @@
-import create from 'zustand';
+import create from 'zustand'
+import { persist } from 'zustand/middleware'
+import AbstractSubmission from './models/AbstractSubmission'
 
-export const useStore = create((set) => ({
-  backgroundColor: '#ffffff',
-  mode: 'dark', // or dark
-}));
+export const useStore = create(persist(
+  (set, get) => ({
+    backgroundColor: '#ffffff',
+    mode: 'dark', // or light
+    temporaryAbstractSubmission: new AbstractSubmission(),
+    setTemporaryAbstractSubmission: (payload) => {
+      const state = get();
+      set({ ...state, temporaryAbstractSubmission: new AbstractSubmission(payload) });
+    },
+  }),
+  {
+    name: 'JournalOfDigitalHistory',
+  }
+))
