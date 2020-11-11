9b1f9061f3bd96592a33b2a422f86155293edb65
Daniele Guido <gui.daniele@gmail.com>
2020-11-11 17:47:00 +0100

    create terms of use page, add class plugin for markdown

---
+++ src/pages/TermsOfUse.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import { getArticleTreeFromIpynb } from '../logic/ipynb'
+
+import pageContents from '../data/mock-api/mock-terms-of-use-ipynb.json'
+import ArticleCell from '../components/ArticleText/ArticleCell'
+
+const articleTree = getArticleTreeFromIpynb(pageContents)
+// const sectionsCells = articleTree.paragraphs.reduce((acc, d) => {
+// 
+// }, {})
+
+const TermsOfUse = ({ results }) => {
+  return (
+    <>
+      <Container className="page">
+        <Row>
+          <Col md={{offset: 2, span:6}}>
+          {articleTree.paragraphs.map((props, i) => (
+            <ArticleCell {...props} idx=""/>
+          ))}
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
+
+export default TermsOfUse
