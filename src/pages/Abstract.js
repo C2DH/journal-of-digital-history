fa71d5bc949f31881228b27739bfc3b250dd50c3
Daniele Guido <gui.daniele@gmail.com>
2020-11-13 09:17:30 +0100

    load abstract data from mock api

--- src/pages/Abstract.js
+++ src/pages/Abstract.js
@@ -1,23 +1,29 @@
 import React from 'react'
+import {useParams} from 'react-router-dom'
 import { Container, Row, Col } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
 import mockAbstract from '../data/mock-api/mock-abstract.json'
 import ArticleAuthor from '../components/ArticleText/ArticleAuthor'
 import Author from '../models/Author'
+import { useGetAbstractSubmission } from '../logic/api'
+import LangLink from '../components/LangLink'
+
 
 const Abstract = ({ results }) => {
   const { t } = useTranslation()
+  const { id } = useParams()
+  const { status, item} = useGetAbstractSubmission(id)
+  
   return (
     <>
       <Container className="page">
         <Row>
           <Col md={{offset: 2, span:6}}>
-        <h3 >
-            {t('pages.abstract')} submitted!
-        </h3>
-          <h1 className="mt-5">{mockAbstract.title}</h1>
-          <p>{mockAbstract.abstract}</p>
-
+            <h3>{item.id}</h3>
+            <h1 className="my-5">{item.title}</h1>
+            <h2 style={{fontWeight: 'normal'}}>{item.abstract}</h2>
+            <pre>{status}
+            {JSON.stringify(item, null, 2)}</pre>
           </Col>
         </Row>
         <Row className="pt-4">
@@ -33,9 +39,15 @@ const Abstract = ({ results }) => {
         <Row className="pt-4">
           <Col md={{offset: 2, span:6}}>
             <h3>{t('pages.abstract.contact')}</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={{offset: 2, span:6}}>
             <ArticleAuthor author={new Author(mockAbstract.contact)} />
           </Col>
         </Row>
+        <LangLink to='abstract/24'> to 24 </LangLink>
+        <LangLink to='abstract/20'> to 20 </LangLink>
       </Container>
     </>
   )
