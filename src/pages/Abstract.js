041722d0719b8754211b1cf20fed1c9fd0671944
Daniele Guido <gui.daniele@gmail.com>
2020-11-13 13:30:55 +0100

    Update Abstract.js

--- src/pages/Abstract.js
+++ src/pages/Abstract.js
@@ -6,7 +6,6 @@ import mockAbstract from '../data/mock-api/mock-abstract.json'
 import ArticleAuthor from '../components/ArticleText/ArticleAuthor'
 import Author from '../models/Author'
 import { useGetAbstractSubmission } from '../logic/api/fetchData'
-import LangLink from '../components/LangLink'
 
 
 const Abstract = ({ results }) => {
@@ -26,26 +25,25 @@ const Abstract = ({ results }) => {
         </Row>
         <Row className="pt-4">
           <Col md={{offset: 2, span:6}}>
-            <h3>{t('pages.abstract.authors')}</h3>
+            <h3 className="font-weight-normal font-italic">{t('pages.abstract.authors')}</h3>
           </Col>
           {item.authors.map((author,i) => (
-            <Col key={i} md={{offset: 2, span: 3}}>
-              {(new Author(author)).asText()}
+            <Col key={i} md={{offset: 2, span: 4}}>
+              <ArticleAuthor author={new Author(author)} />
             </Col>
           ))}
         </Row>
         <Row className="pt-4">
           <Col md={{offset: 2, span:6}}>
-            <h3>{t('pages.abstract.contact')}</h3>
+            <h3 className="font-weight-normal font-italic">
+            {t('pages.abstract.contact')}
+            </h3>
           </Col>
-        </Row>
-        <Row>
           <Col md={{offset: 2, span:6}}>
             {(new Author(item.contact)).asText()}
           </Col>
         </Row>
-        <LangLink to='abstract/24'> to 24 </LangLink>
-        <LangLink to='abstract/20'> to 20 </LangLink>
+        <pre>{status}</pre>
       </Container>
     </>
   )
