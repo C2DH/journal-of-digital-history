4f8025dd2fa5e8e9526edeffa846f14cf869843a
Daniele Guido <gui.daniele@gmail.com>
2020-11-13 13:30:50 +0100

    add info

--- src/components/ArticleText/ArticleAuthor.js
+++ src/components/ArticleText/ArticleAuthor.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React from 'react'
+import {Badge} from 'react-bootstrap'
+
 
 const ArticleAuthor = ({ author }) => {
   return (
-    <><b>{author.lastname}</b> {author.firstname} ({author.affiliation})</>
+    <>
+      <h3>{author.lastname} {author.firstname}</h3>
+      <br/>
+      {author.affiliation}
+      <br/>
+      <Badge variant="dark">orcid</Badge> <a href={author.orcid}>
+        {author.orcid.replace('https://orcid.org/','')}
+      </a>
+    </>
   )
 }
 
