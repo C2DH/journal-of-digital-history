f15d8911b07ddb88d0f333de0bd0b8526c1a49a5
danieleguido <gui.daniele@gmail.com>
2020-11-25 11:16:31 +0100

    update terms of use with ostbelgien text

--- src/pages/TermsOfUse.js
+++ src/pages/TermsOfUse.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import { getArticleTreeFromIpynb } from '../logic/ipynb'
-
+import { useStore } from '../store'
 import pageContents from '../data/mock-api/mock-terms-of-use-ipynb.json'
 import ArticleCell from '../components/ArticleText/ArticleCell'
 
 const articleTree = getArticleTreeFromIpynb(pageContents)
 
 const TermsOfUse = ({ results }) => {
+  useEffect(() => {
+    useStore.setState({ backgroundColor: 'var(--yellow)' });
+  })
   return (
     <>
       <Container className="page">
