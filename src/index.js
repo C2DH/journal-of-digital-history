aaa781e5b7d8847b6a08e22a0e5058d92a9867f9
danieleguido <gui.daniele@gmail.com>
2020-12-03 18:32:42 +0100

    add italic to fira sans

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ WebFontLoader.load({
     families: [
       'Source+Serif+Pro:400,700',
       'Fira+Code:400,700:latin-ext',
-      'Fira+Sans:400,700:latin-ext'
+      'Fira+Sans:400,700,ital:latin-ext'
     ]
   }
 })
