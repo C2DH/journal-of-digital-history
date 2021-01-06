7f01ae044a0e9af7f3b09b11f039833d0de03123
danieleguido <gui.daniele@gmail.com>
2021-01-06 16:32:46 +0100

    add article references

--- src/logic/ipynb.js
+++ src/logic/ipynb.js
@@ -1,11 +1,11 @@
 import MarkdownIt from 'markdown-it'
 import Cite from 'citation-js'
+import MarkdownItAttrs from '@gerhobbelt/markdown-it-attrs'
 
 import ArticleTree from '../models/ArticleTree'
 import ArticleHeading from '../models/ArticleHeading'
 import ArticleCell from '../models/ArticleCell'
-
-import MarkdownItAttrs from '@gerhobbelt/markdown-it-attrs'
+import ArticleReference from '../models/ArticleReference'
 
 const markdownParser = MarkdownIt({
   html: false,
@@ -29,29 +29,63 @@ const getArticleTreeFromIpynb = ({ cells=[], metadata={} }) => {
   if (citationsFromMetadata instanceof Object) {
     bibliography = new Cite(Object.values(citationsFromMetadata))
   }
-  console.info('getArticleTreeFromIpynb', cells, citationsFromMetadata)
-  cells.forEach((cell, idx) => {
+  console.info('getArticleTreeFromIpynb', citationsFromMetadata)
+  // this contain footnotes => zotero id to remap reference at paragraph level
+  const referenceIndex = {}
+  cells.map((cell, idx) => {
+    const citation = cell.source.join('\n\n').match(/<span id=.fn(\d+).><cite data-cite=.([/\dA-Z]+).>/)
+    if(citation) {
+      referenceIndex[citation[1]] = citation[2]
+      cell.hidden = true
+    }
+    return cell
+  }).forEach((cell, idx) => {
     // console.info(p.cell_type, idx)
     if (cell.cell_type === 'markdown') {
-       const tokens = markdownParser.parse(cell.source.join('\n\n'));
-       const content = markdownParser.render(cell.source.join('\n\n'))
-       // get tokens 'heading_open' to get all h1,h2,h3 etc...
-       const headerIdx = tokens.findIndex(t => t.type === 'heading_open');
-       if (headerIdx > -1) {
-         headings.push(new ArticleHeading({
-           tag: tokens[headerIdx].tag,
-           content: tokens[headerIdx + 1].content,
-           idx
-         }))
-       }
-       paragraphs.push(new ArticleCell({
-         type: 'markdown',
-         content,
-         source: cell.source,
-         metadata: cell.metadata,
-         idx,
-         level: headerIdx > -1 ? tokens[headerIdx].tag : 'p'
-       }))
+      const sources = cell.source.join('\n\n')
+      // exclude rendering of reference references
+      const references = []
+      const tokens = markdownParser.parse(sources);
+      const content = markdownParser.render(sources)
+        .replace(/&lt;sup&gt;(\d+)&lt;\/sup&gt;/, (m, num) => {
+          // add footnote nuber and optionally the Author, year
+          const reference = new ArticleReference({
+            num,
+            id: referenceIndex[num],
+            ref: citationsFromMetadata[referenceIndex[num]],
+          })
+          references.push(reference)
+          return `
+            <span class="ArticleReference d-inline-block">
+            <sup class="font-weight-bold">${num}</sup>
+            <span class=" d-inline-block">
+              <span class="ArticleReference_pointer"></span>
+              <span class="ArticleReference_shortRef">${reference.shortRef}</span>
+            </span>`
+        })
+
+      if ( idx === 36 ) {
+       console.info('id', idx, cell, content)
+      }
+      // get tokens 'heading_open' to get all h1,h2,h3 etc...
+      const headerIdx = tokens.findIndex(t => t.type === 'heading_open');
+      if (headerIdx > -1) {
+        headings.push(new ArticleHeading({
+          tag: tokens[headerIdx].tag,
+          content: tokens[headerIdx + 1].content,
+          idx
+        }))
+      }
+      paragraphs.push(new ArticleCell({
+        type: 'markdown',
+        content,
+        source: cell.source,
+        metadata: cell.metadata,
+        idx,
+        references,
+        hidden: !!cell.hidden,
+        level: headerIdx > -1 ? tokens[headerIdx].tag : 'p'
+      }))
     } else if (cell.cell_type === 'code') {
       paragraphs.push(new ArticleCell({
         type: 'code',
