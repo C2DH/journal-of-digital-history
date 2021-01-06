d6556e4d7c84e032766b17e477547b7b07face2b
danieleguido <gui.daniele@gmail.com>
2021-01-06 16:33:19 +0100

    add different output

--- src/components/Article/ArticleCell.js
+++ src/components/Article/ArticleCell.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import styles from './ArticleText.module.scss'
 
+const ArticleCellOutput = ({ output }) => {
+  const outputTypeClassName= `ArticleCellOutput_${output.output_type}`
+  return (
+    <blockquote className={`${outputTypeClassName}`}>
+      <div>
+        <div className="label">type:{output.output_type} | name:{output.name}</div>
+      </div>
+      {output.output_type === 'stream' && (
+        <pre>{output.text.join('\n')}</pre>
+      )}
+      {output.output_type === 'execute_result' && output.data['text/plain'] && (
+        <pre>{output.data['text/plain'].join('')}</pre>
+      )}
+      {output.output_type === 'display_data' && output.data['text/plain'] && (
+        <pre>{output.data['text/plain'].join('')}</pre>
+      )}
+      {output.output_type === 'display_data' && output.data['image/png'] && (
+        <img src={`data:image/png;base64,${output.data['image/png']}`} alt='display_data output'/>
+      )}
+    </blockquote>
+  )
+}
 
 const ArticleCell = ({ type, content='', idx, outputs=[], hideIdx, ...props }) => {
   if (type === 'markdown') {
@@ -16,15 +38,10 @@ const ArticleCell = ({ type, content='', idx, outputs=[], hideIdx, ...props }) =
       <div className={styles.ArticleCell} id={`P${idx}`}>
         {!hideIdx && (<div className={styles.ParagraphNumber}>{idx}</div>)}
         <pre className="bg-dark text-white p-3">{content}</pre>
-        <pre>{JSON.stringify(outputs)}</pre>
-        {outputs.length && outputs.map((output,i) => {
-          return (
-            <blockquote key={i} className='pl-3 py-2 pr-2' style={{borderLeft: '2px solid', background:'var(--gray-200)'}}>
-              <div>{output.output_type} {output.ename}</div>
-              <div>{output.evalue}</div>
-            </blockquote>
-          )
-        })}
+        {outputs.length
+          ? outputs.map((output,i) => <ArticleCellOutput output={output} key={i} />)
+          : <div>no output</div>
+        }
       </div>
     )
   }
