55479abbd47061b07988d8384d1ac6ed5a84efa5
danieleguido <gui.daniele@gmail.com>
2021-01-06 16:33:05 +0100

    move bibliography in a specific component file

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,30 +1,11 @@
 import React from 'react'
 import {groupBy} from 'lodash'
-import { useTranslation } from 'react-i18next'
 import ArticleText from '../components/Article'
 import ArticleHeader from '../components/Article/ArticleHeader'
+import ArticleBilbiography from '../components/Article/ArticleBibliography'
 import source from '../data/mock-ipynb.nbconvert.json'
 import { getArticleTreeFromIpynb } from '../logic/ipynb'
 
-import {Container, Col, Row} from 'react-bootstrap'
-
-
-const ArticleBilbiography = ({ articleTree }) => {
-  const { t } = useTranslation()
-
-  return (
-    <Container className="mt-5">
-      <Row>
-        <Col md={{ span: 7, offset: 2 }}>
-          <h2>{t('bibliography')}</h2>
-          <div dangerouslySetInnerHTML={{
-            __html: articleTree.formatBibliograhpy()
-          }}/>
-        </Col>
-      </Row>
-    </Container>
-  )
-}
 
 const Article = ({ ipynb }) => {
   // const { t } = useTranslation()
