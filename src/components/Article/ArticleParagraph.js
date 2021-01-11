1a67dd5464ef71f16399a6b4598d1d9d0514c854
danieleguido <gui.daniele@gmail.com>
2021-01-11 09:55:17 +0100

    allow fullwidth

--- src/components/Article/ArticleParagraph.js
+++ src/components/Article/ArticleParagraph.js
@@ -3,14 +3,21 @@ import { Container, Row, Col} from 'react-bootstrap'
 import { BootstrapColumLayout } from '../../constants'
 import ArticleCell from './ArticleCell'
 
-const ArticleParagraph = ({cell, idx}) => (
-  <Container>
-    <Row>
-      <Col {... BootstrapColumLayout}>
-        <ArticleCell {...cell} idx={idx}/>
-      </Col>
-    </Row>
-  </Container>
-)
+
+const ArticleParagraph = ({cell, progress, active, idx}) => {
+  if (cell.isFullWidth()) {
+    return <ArticleCell {...cell} progress={progress} active={active} idx={idx}/>
+  }
+  return (
+    <Container>
+      <Row>
+        <Col {... BootstrapColumLayout}>
+          <ArticleCell {...cell} idx={idx}/>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
 
 export default ArticleParagraph
