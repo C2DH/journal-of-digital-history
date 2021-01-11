b61b761d868ca7bad2fb0d3fddc68935fe141930
danieleguido <gui.daniele@gmail.com>
2021-01-11 18:35:07 +0100

    use Cell Content directly

--- src/components/Article/ArticleHeader.js
+++ src/components/Article/ArticleHeader.js
@@ -1,34 +1,35 @@
 import React from 'react'
 // import { useTranslation } from 'react-i18next'
 import { Container, Row, Col } from 'react-bootstrap'
-import ArticleCell from './ArticleCell'
+import ArticleCellContent from './ArticleCellContent'
 import {BootstrapColumLayout} from '../../constants'
-import styles from './ArticleText.module.scss'
 
 
 const ArticleHeader = ({ title=[], abstract=[], contributor=[] }) => {
   // const { t } = useTranslation()
 
   return (
-    <Container >
+    <Container className="ArticleHeader">
       <Row>
         <Col {...BootstrapColumLayout}>
           {title.map((paragraph, i) => (
-            <ArticleCell key={i} {...paragraph} hideIdx/>
+            <ArticleCellContent key={i} {...paragraph} hideIdx hideNum/>
           ))}
         </Col>
       </Row>
       <Row className="mt-5">
         <Col {...BootstrapColumLayout}>
-        {abstract.map((paragraph, i) => (
-          <ArticleCell key={i} {...paragraph} className={styles.AbstractArticleCell} hideIdx/>
-        ))}
+          <div className="ArticleHeader_abstract">
+            {abstract.map((paragraph, i) => (
+              <ArticleCellContent key={i} {...paragraph} hideIdx hideNum/>
+            ))}
+          </div>
         </Col>
       </Row>
-      <Row className="mt-5">
+      <Row className="mt-5" >
       {contributor.map((author,i) => (
         <Col key={i} md={{ offset: i % 2 === 0 ? 2: 0, span: 4}}>
-           <ArticleCell {...author} className={styles.AuthorArticleCell} hideIdx/>
+           <ArticleCellContent {...author} hideIdx hideNum/>
         </Col>
       ))}
       </Row>
