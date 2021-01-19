1bada1a5cd465c84d5c5b4f3f00afdab5f619fc2
danieleguido <gui.daniele@gmail.com>
2021-01-19 08:15:23 +0100

    Update Pointer.js

--- src/components/Graphics/Pointer.js
+++ src/components/Graphics/Pointer.js
@@ -1,17 +1,28 @@
 import React from 'react'
 
-const Pointer = ({ x, y, width, height, availableWidth, availableHeight, children }) => {
+const Pointer = ({
+  x, y,
+  width, height,
+  availableWidth, availableHeight,
+  horizontal=true,
+  children
+}) => {
   return (
     <div className="Pointer" style={{
       top: 0,
-      transform: `translate(${x}px, 0px)`,
+      transform: horizontal
+        ? `translate(${x}px, 0px)`
+        : `translate(0px, ${y}px)`,
       height,
       width,
       background: 'var(--accent)',
       zIndex:100,
+      pointerEvents: 'none'
     }}>
       <div className="position-absolute" style={{
-        transform: `translate(0px, ${y}px)`
+        transform: horizontal
+          ? `translate(0px, ${y}px)`
+          : `translate(${x}px, 0px)`
       }}>
         {children}
       </div>
