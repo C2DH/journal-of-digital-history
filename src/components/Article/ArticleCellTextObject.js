abc3ece7aeea449bcc9c9cec5b7d63179f7877f3
danieleguido <gui.daniele@gmail.com>
2021-01-25 22:34:31 +0100

    default value for text object is 8 column span

--- src/components/Article/ArticleCellTextObject.js
+++ src/components/Article/ArticleCellTextObject.js
@@ -3,16 +3,17 @@ import { Container, Row, Col } from 'react-bootstrap'
 import { markdownParser } from '../../logic/ipynb'
 
 const VegaWrapper = lazy(() => import('../Module/VegaWrapper'))
-
+const ImageWrapper = lazy(() => import('../Module/ImageWrapper'))
 
 const ArticleCellTextObject = ({ metadata, children, progress }) => {
   const textMetadata = metadata.jdh?.text ?? {}
   const objectMetadata = useMemo(() => metadata.jdh?.object ?? {}, [metadata.jdh])
+  const objectOutputs = useMemo(() => metadata.jdh?.outputs ?? [], [metadata.jdh])
   const textColumnLayout = textMetadata.bootstrapColumLayout ?? {
-    md: { offset:0, span: 6, order: 1 }
+    md: { offset:2, span: 4, order: 1 }
   }
   const objectColumnLayout = objectMetadata.bootstrapColumLayout ?? {
-    md: { offset:0, span: 6, order: 2 }
+    md: { offset:0, span: 4, order: 2 }
   }
   const objectContents = useMemo(() => {
     if (Array.isArray(objectMetadata.source)) {
@@ -27,26 +28,58 @@ const ArticleCellTextObject = ({ metadata, children, progress }) => {
   let objectWrapperStyle = {
     backgroundColor: objectMetadata.background?.color,
     border: objectMetadata.border,
+    width: '100%',
+    // h is necessary to calculate bounding box correctly.
+    // override with HeightRatio
     height: '100%',
-    width: '100%'
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  }
+  // flex alignment
+  if ([ 'start', 'flex-start', 'end', 'flex-end', 'center', 'space-between', 'space-around'].includes(objectMetadata.justifyContent)) {
+    objectWrapperStyle = {
+      ...objectWrapperStyle,
+      justifyContent: objectMetadata.justifyContent,
+    }
+  }
+  if ([
+    'start', 'flex-start', 'end', 'flex-end', 'center',
+    'baseline', 'first baseline'
+  ].includes(objectMetadata.alignItems)) {
+    objectWrapperStyle = {
+      ...objectWrapperStyle,
+      alignItems: objectMetadata.alignItems,
+    }
+  }
+
+  if (!isNaN(objectMetadata.heightRatio)) {
+    objectWrapperStyle = {
+      ...objectWrapperStyle,
+      height: window.innerHeight * objectMetadata.heightRatio
+    }
   }
 
   if (objectMetadata.position === 'sticky') {
     objectWrapperStyle = {
       ...objectWrapperStyle,
       position: 'sticky',
-      top: window.innerHeight *.1,
-      height: window.innerHeight *.8
+      top: objectMetadata.top ?? 'var(--spacer-3)',
+    }
+    if(isNaN(objectMetadata.heightRatio)) {
+      objectWrapperStyle.height = 'auto'
     }
   }
 
-  if(!isNaN(objectMetadata.ratio)) {
-    objectWrapperStyle = {
-      ...objectWrapperStyle,
-      paddingTop: `${objectMetadata.ratio * 100}%`,
-      boxSizing: 'border-box',
-    }
-  }
+
+  // if(!isNaN(objectMetadata.ratio)) {
+  //   objectWrapperStyle = {
+  //     ...objectWrapperStyle,
+  //     paddingTop: `${objectMetadata.ratio * 100}%`,
+  //     boxSizing: 'border-box',
+  //     position: 'relative'
+  //   }
+  // }
 
   return (
     <Container>
@@ -56,7 +89,10 @@ const ArticleCellTextObject = ({ metadata, children, progress }) => {
         </Col>
         <Col {... objectColumnLayout}>
           <div style={objectWrapperStyle} className={objectClassName.join(' ')}>
-          {['image', 'video', 'map'].includes(objectMetadata.type) && (
+          {objectMetadata.type === 'image' && objectOutputs.map((output, i) => (
+            <ImageWrapper metadata={objectMetadata} key={i} output={output} />
+          ))}
+          {['video', 'map'].includes(objectMetadata.type) && (
             <>
               {progress}
               <div dangerouslySetInnerHTML={{__html: objectContents}}></div>
