333b1904ab60a47dd20160984bb4053ad02b90e9
danieleguido <gui.daniele@gmail.com>
2021-01-25 22:31:07 +0100

    add result from local notebook

--- src/pages/Notebook.js
+++ src/pages/Notebook.js
@@ -1,16 +1,16 @@
-import React, { useMemo, useState } from 'react'
+import React, { useMemo, useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useParams, useHistory, generatePath } from 'react-router'
 import Article from './Article'
 import { Form, Button, Container, Row, Col } from 'react-bootstrap'
 import { useGetNotebookFromURL } from '../logic/api/fetchData'
-import { BootstrapColumLayout, StatusIdle, StatusNone } from '../constants'
+import { BootstrapColumLayout, StatusIdle, StatusNone, StatusSuccess } from '../constants'
 // url=aHR0cHM6Ly9naXRodWIuY29tL0MyREgvamRoLW5vdGVib29rL2Jsb2IvbWFzdGVyL3BvYy5pcHluYg
 // as base64 encoded for url=https://github.com/C2DH/jdh-notebook/blob/master/poc.ipynb
 
 
 const Notebook = () => {
-  const { i18n } = useTranslation()
+  const { i18n, t } = useTranslation()
   const { encodedUrl } = useParams()
   const history = useHistory()
   const [value, setValue] = useState(null)
@@ -24,6 +24,15 @@ const Notebook = () => {
   console.info('Notebook render:', url)
   // check url...
   const { status, item } = useGetNotebookFromURL(url)
+  const [uploadedNotebook, setUploadedNotebook] = useState(null)
+  // resize if document is loaded
+  useEffect(() => {
+    let timer
+    if(status === StatusSuccess) {
+      timer = setTimeout(() => window.dispatchEvent(new Event('resize')), 100)
+    }
+    return () => clearTimeout(timer)
+  }, [status])
   // fetch url if available.
   const handleSubmit = () => {
     history.push({
@@ -33,12 +42,38 @@ const Notebook = () => {
       })
     })
   }
+  const handleNotebookUpload = (e) => {
+    const [file] = e.target.files
+    if (file) {
+      const reader = new FileReader()
+      reader.addEventListener('load', ({ target }) => {
+        const jipynb = JSON.parse(target.result)
+        console.info('LOADED', jipynb?.content?.cells ? jipynb.content : jipynb)
+        setUploadedNotebook(jipynb?.content?.cells ? jipynb.content : jipynb)
+      })
+      reader.readAsText(file)
+    }
+  }
+
   if (status === StatusIdle) {
     return null
   }
   return (
     <div>
-    {item !== null && <Article ipynb={item} url={url}/>}
+    {item === null && uploadedNotebook
+      ? <Article ipynb={uploadedNotebook}/>
+      : null
+    }
+    {item !== null && !!item.last_modified && (
+      <div className="Notebook_last_modified p-2 bg-primary rounded" style={{
+        position: 'fixed',
+        bottom: 'var(--spacer-2)',
+        right: 'var(--spacer-2)',
+      }}>
+        {t('dates.precise', { date: new Date(item.last_modified) })}
+      </div>
+    )}
+    {item !== null && <Article ipynb={item.content ? item.content : item} url={url}/>}
     {status === StatusNone && (
         <Container className="mt-5">
           <Row>
@@ -65,6 +100,13 @@ const Notebook = () => {
                   variant="secondary"
                 >try notebook!
                 </Button>
+
+                  <Form.File
+                    id="custom-file"
+                    onChange={handleNotebookUpload}
+                    label="Upload notebook"
+                    custom
+                  />
               </Form>
             </Col>
           </Row>
