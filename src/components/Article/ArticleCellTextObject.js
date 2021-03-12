2322d9ca4822c53484e6f9943673530788dc99fa
Daniele Guido <gui.daniele@gmail.com>
2021-03-12 17:14:51 +0100

    add highlights

--- src/components/Article/ArticleCellTextObject.js
+++ src/components/Article/ArticleCellTextObject.js
@@ -1,8 +1,14 @@
 import React, { useMemo } from 'react'
 import { markdownParser } from '../../logic/ipynb'
 
-
-const ArticleCellTextObject = ({ metadata, children, progress }) => {
+const emptyHighlight = ({ idx }) => {
+  console.warn(
+    'Ouch! No listener has been provied for idx:', idx,
+    'Please make sure to pass that funcion as a property, e.g.',
+    '<ArticleCellTextObject onHighlight={yourAwesomeMethod} />'
+  )
+}
+const ArticleCellTextObject = ({ metadata, children, progress, onHighlight = emptyHighlight }) => {
   const objectContents = useMemo(() => {
     const textMetadata = metadata.jdh?.text?.source
     if (Array.isArray(textMetadata)) {
@@ -11,8 +17,30 @@ const ArticleCellTextObject = ({ metadata, children, progress }) => {
     return null
   }, [metadata])
 
+  /*
+    Get special link out of markdown
+  */
+  const handleClick = (e) => {
+    const href = e.target.getAttribute('href');
+    if (href && href.indexOf('@') === 0) {
+      const matchIndices = href.match(/^@([\d,]+)$/)
+      e.nativeEvent.stopImmediatePropagation()
+      e.preventDefault();
+
+      if (!matchIndices) {
+        console.warn('href value provided to trigger the `highligh` event is not readable, received:', href);
+      } else {
+        onHighlight({
+          idx: matchIndices[1].split(',')
+        });
+      }
+    }
+  }
+
   return (
-    <div dangerouslySetInnerHTML={{__html: objectContents}}></div>
+    <div onClick={handleClick}>
+      <div dangerouslySetInnerHTML={{__html: objectContents}}></div>
+    </div>
   )
 }
 
