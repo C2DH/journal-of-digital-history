7b3d3c093e60258c82ace5dcb20741c09ec6ee89
Daniele Guido <gui.daniele@gmail.com>
2021-05-27 14:47:24 +0200

    add issues page

--- src/App.js
+++ src/App.js
@@ -52,6 +52,7 @@ const AbstractSubmission = lazy(() => import('./pages/AbstractSubmission'))
 const AbstractSubmitted = lazy(() => import('./pages/AbstractSubmitted'))
 const Article = lazy(() => import('./pages/Article'))
 const Issue = lazy(() => import('./pages/Issue'))
+const Issues = lazy(() => import('./pages/Issues'))
 const Abstract = lazy(() => import('./pages/Abstract'))
 const MockAbstract = lazy(() => import('./pages/MockAbstract'))
 const TermsOfUse = lazy(() => import('./pages/TermsOfUse'))
@@ -106,6 +107,7 @@ function LangRoutes() {
       <Route path={`${path}/abstract/:id`}>
         <Abstract />
       </Route>
+      <Route path={`${path}/issues`} component={Issues} />
       <Route path={`${path}/issue/:id`} component={Issue} />
       <Route path={`${path}/abstract-submitted`}>
         <AbstractSubmitted />
---
+++ src/pages/Issues.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { Container, Row, Col } from 'react-bootstrap'
+import { useGetJSON } from '../logic/api/fetchData'
+import { BootstrapColumLayout } from '../constants'
+import LangLink from '../components/LangLink'
+
+const Issues = ({ match: { params: { issueId }}}) => {
+  const { t } = useTranslation()
+  const { data:issues, error, status } = useGetJSON({ url: `/mock-api/issues.json`, delay: 2000 })
+  console.info('Issues status', status, 'error', error)
+
+  return (
+    <Container className="Issues page mt-5">
+      <Row>
+        <Col {...BootstrapColumLayout}>
+          <h1>{t('pages.issues.title')}</h1>
+          <p>{t('pages.issues.introduction')}</p>
+        </Col>
+      </Row>
+
+      <Row>
+        {issues ? issues.map((issue, i) => (
+          <Col key={i} md={{span:4, offset: i % 2 === 0 ? 2 : 0}}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">
+              {Array(Math.round(Math.random() * 20)).fill(0).map(d => (
+                <circle cx="100" cy="100" r={80 * Math.random()} fill="transparent" stroke="#000" stroke-width={5 * Math.random()}/>
+              ))}
+            </svg>
+            <h3 className="d-block"><LangLink to={`/issue/${issue.id}`}>{issue.name}</LangLink></h3>
+            <p dangerouslySetInnerHTML={{__html: issue.description }} />
+          </Col>
+        )): null}
+      </Row>
+      <Row>
+        {error ? <pre>{JSON.stringify(error, null, 2)}</pre>: null}
+      </Row>
+    </Container>
+  )
+}
+
+export default Issues
