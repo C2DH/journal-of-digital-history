a584289bfb1bb7a38c55bc029c9717c83af08b4c
eliselavy <eliselavy@gmail.com>
2021-05-28 12:41:09 +0200

    update reference

--- src/models/ArticleReference.js
+++ src/models/ArticleReference.js
@@ -17,22 +17,36 @@ export default class ArticleReference {
       }
      if(ref.accessed && ref.accessed.year){
         year =ref.accessed.year
+      }
+     if(!ref.author && Array.isArray(ref.editor)){
+          this.shortRef = `<a data-href="${ref.id}"><span class="ArticleReference_pointer"></span>${ref.editor.map(d => d.family).join(', ').trim()} (Ed.) ${year}</a>`
+        }else{
+          this.shortRef = `<a data-href="${ref.id}"><span class="ArticleReference_pointer"></span>${ref.author?.map(d => d.family).join(', ').trim()} ${year}</a>`
+          if(ref.type ==='webpage'){
+            if(ref["container-title"]){
+              reference = ref["container-title"]
+              this.shortRef = `<a data-href="${ref.id}"><span class="ArticleReference_pointer"></span>${reference} ${year}</a>`
+            }else{
+              this.shortRef = `<a data-href="${ref.id}"><span class="ArticleReference_pointer"></span>${ref.author?.map(d => d.family).join(', ').trim()} ${year} ${year}</a>`
+            }
+         }
+         if(ref.type ==='article-magazine'){
+          if(!ref.title){
+            this.shortRef = `<a data-href="${ref.id}"><span class="ArticleReference_pointer"></span>${ref.author?.map(d => d.family).join(', ').trim()} ${year}</a>`
+          }else{
+          this.shortRef = `<a data-href="${ref.id}"><span class="ArticleReference_pointer"></span>${ref.title} ${year}</a>`
         }
-     if(ref.type ==='webpage'){
-        reference = ref["container-title"]
-        this.shortRef = `<a data-href="${ref.id}"><span class="ArticleReference_pointer"></span>${reference} ${year}</a>`
+
+
+      }
+
+
+
      }
-     if(ref.type ==='article-magazine'){
-        reference = ref["title"]
-        this.shortRef = `<a data-href="${ref.id}"><span class="ArticleReference_pointer"></span>${reference} ${year}</a>`
-     }
-     if(ref.type !=='article-magazine' && ref.type !=='webpage'){
-      if(!ref.author && Array.isArray(ref.editor)){
-        this.shortRef = `<a data-href="${ref.id}"><span class="ArticleReference_pointer"></span>${ref.editor.map(d => d.family).join(', ').trim()} (Ed.) ${year}</a>`
-      }else{
-        this.shortRef = `<a data-href="${ref.id}"><span class="ArticleReference_pointer"></span>${ref.author?.map(d => d.family).join(', ').trim()} ${year}</a>`
-     }
-     }
+
+
+
+
 
     }
   }
