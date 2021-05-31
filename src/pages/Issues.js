4574c87da85bdc2f661e46a9e4411d17a950f2f5
Daniele Guido <gui.daniele@gmail.com>
2021-05-31 12:40:44 +0200

    Update Issues.js

--- src/pages/Issues.js
+++ src/pages/Issues.js
@@ -7,7 +7,7 @@ import LangLink from '../components/LangLink'
 
 const Issues = ({ match: { params: { issueId }}}) => {
   const { t } = useTranslation()
-  const { data:issues, error, status } = useGetJSON({ url: `/mock-api/issues.json`, delay: 2000 })
+  const { data:issues, error, status } = useGetJSON({ url: `/mock-api/issues.json`, delay: 0 })
   console.info('Issues status', status, 'error', error)
 
   return (
@@ -22,17 +22,20 @@ const Issues = ({ match: { params: { issueId }}}) => {
       <Row>
         {issues ? issues.map((issue, i) => (
           <Col key={i} md={{span:4, offset: i % 2 === 0 ? 2 : 0}}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">
-              {Array(Math.round(Math.random() * 20)).fill(0).map(d => (
+            <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" style={{background: 'var(--primary)'}}>
+              {Array(Math.round(Math.random() * 100)).fill(0).map(d => (
+                <>
                 <circle
-                  cx={100}
-                  cy={100}
-                  // cx={100 + 5*Math.random()}
-                  // cy={100 + 5*Math.random()}
+                  // cx={100}
+                  // cy={100}
+                  cx={100 + 5*Math.random()}
+                  cy={100 + 5*Math.random()}
                   r={80 * Math.random()}
                   fill="transparent" stroke="#000"
+                  style={{opacity: .5}}
                   stroke-width={5 * Math.random()}
                 />
+                </>
               ))}
             </svg>
             <h3 className="d-block"><LangLink to={`/issue/${issue.id}`}>{issue.name}</LangLink></h3>
