4bc09dd0024864f2666beb16bdbe322ffe608fa9
Daniele Guido <gui.daniele@gmail.com>
2021-06-02 11:31:11 +0200

    add figure number from author text

--- src/components/Article/ArticleFigure.js
+++ src/components/Article/ArticleFigure.js
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import { useTranslation } from 'react-i18next'
 
 const ArticleFigure = ({ figure, children }) => {
+  const { t } = useTranslation()
+  // figure number, for translation label.
+  const n = figure.ref ? figure.ref.split('-').pop() : figure.num
   return (
     <figcaption className="ArticleFigure position-relative">
       <div className="ArticleFigure_figcaption_num">
-        <div className="mr-2">Fig. {figure.num}</div>
+        <div className="mr-2">{t('numbers.figure', { n })}</div>
       </div>
       {children}
     </figcaption>
