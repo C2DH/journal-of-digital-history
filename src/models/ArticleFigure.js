a222869944e4b787b663c7f1623da8beb49cb4a2
Daniele Guido <gui.daniele@gmail.com>
2021-06-02 14:23:05 +0200

    add comment

--- src/models/ArticleFigure.js
+++ src/models/ArticleFigure.js
@@ -1,6 +1,6 @@
 export default class ArticleFigure {
   constructor({
-    ref='figure-', // 'figure-12a' figure identifier.
+    ref='figure-', // 'figure-12a' figure identifier. Must start with constants/FigureRefPrefix
     type = '', // constants FigureImage, FigureVideo or FigureDatavis,
     module = '', // ModuleTextObject, ModuleObject or ModuleText
     idx = -1,
