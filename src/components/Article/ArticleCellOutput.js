7a5355833a441b5ccf95cbecb3638d0d99dcf9f2
Daniele Guido <gui.daniele@gmail.com>
2021-06-07 07:15:04 +0200

    fix style, do not display caption in tables

--- src/components/Article/ArticleCellOutput.js
+++ src/components/Article/ArticleCellOutput.js
@@ -19,7 +19,7 @@ const ArticleCellOutput = ({ output, hideLabel=false }) => {
     )
   }
   if (output.output_type === 'display_data' && output.data['text/html']) {
-    return (<div className={`ArticleCellOutput withHTML ${outputTypeClassName}`} dangerouslySetInnerHTML={{
+    return (<div className={`ArticleCellOutput withHTML mb-3 ${outputTypeClassName}`} dangerouslySetInnerHTML={{
       __html: getOutput(output.data['text/html'])
     }} />)
   }
