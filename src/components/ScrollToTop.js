076d6a69b8ab4e9462884a74d16c7dbd8fd99d41
Daniele Guido <gui.daniele@gmail.com>
2021-06-20 20:27:39 +0200

    put ll related scrolling in the scrolllToTop component

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -2,11 +2,28 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const { pathname, hash } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    let timeoutId = null
+    let timer;
+    if (hash=== '') {
+      window.scrollTo(0, 0)
+    } else {
+      clearTimeout(timeoutId);
+      timer = setTimeout(() => {
+        const id = hash.replace('#', '')
+        console.info('ScrollTo', id)
+        const element = document.getElementById(id)
+        if (element) {
+          element.scrollIntoView();
+        }
+      }, 0);
+    }
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [pathname, hash])
 
   return null;
 }
