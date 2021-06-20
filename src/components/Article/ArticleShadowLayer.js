c771d7dac87b143d39e2ff06d8a466216bd9a068
Daniele Guido <gui.daniele@gmail.com>
2021-06-20 20:31:29 +0200

    Update ArticleShadowLayer.js

--- src/components/Article/ArticleShadowLayer.js
+++ src/components/Article/ArticleShadowLayer.js
@@ -2,6 +2,9 @@ import React, { useEffect } from 'react'
 import { useSpring, animated } from 'react-spring'
 import ArticleHeader from './ArticleHeader'
 import ArticleShadowHandle from './ArticleShadowHandle'
+import ArticleScrollama from './ArticleScrollama'
+import ArticleCell from './ArticleCell'
+import { LayerHermeneutics } from '../../constants'
 // left to right
 // const layerTransition = (x, y, width, height) => `polygon(0px 0px, 0px ${height}px, ${x}px ${height}px, ${x}px 0px)`
 // right to left
@@ -9,7 +12,14 @@ const layerTransition = (x, y, width, height) => {
   return `polygon(${x - 20}px 0px, ${x}px ${height}px, ${width}px ${height}px, ${width}px 0px)`
 }
 
-const ArticleShadowLayer = ({ width, height, title, abstract, keywords, contributor, publicationDate, url, disclaimer, disabled=false }) => {
+const ArticleShadowLayer = ({
+  memoid='',
+  width, height,
+  title, abstract, keywords, contributor,
+  publicationDate, url, disclaimer,
+  disabled=false,
+  cells=[]
+}) => {
   const [props, setProps] = useSpring(() => ({ clipPath: [width, 50, width, height], x:0, y:0 }))//, config: { mass: 1, tension: 50, friction: 10 } }))
   const onDragHandler = ({ x, y, active }) => {
     if (x < width / 2) {
@@ -28,7 +38,7 @@ const ArticleShadowLayer = ({ width, height, title, abstract, keywords, contribu
   return (
     <div className="ArticleShadowLayer" style={{
       width,
-      minHeight: height
+      minHeight: height,
     }}>
       <animated.div style={{
         position: 'fixed',
@@ -36,18 +46,21 @@ const ArticleShadowLayer = ({ width, height, title, abstract, keywords, contribu
         width,
         minHeight: height,
         backgroundColor: 'var(--primary)',
-        zIndex: 2,
+        zIndex: 200,
         clipPath: props.clipPath.interpolate(layerTransition),
+        overflow: 'scroll',
+        pointerEvents: 'all',
       }}>
         <div className="page mt-5" style={{paddingTop: 100}}>
-          <ArticleHeader variant="text-white"
-            title={title}
-            keywords={keywords}
-            contributor={contributor}
-            publicationDate={publicationDate}
-            url={url}
-            disclaimer={disclaimer}
-          />
+          {cells.map((cell, i) => {
+            return (
+            <ArticleCell
+              mmoid={memoid}
+              {...cell}
+              idx={cell.idx}
+              active={false}
+            />)
+          })}
         </div>
       </animated.div>
       <ArticleShadowHandle onDrag={onDragHandler} width={width} height={height} disabled={disabled}/>
@@ -55,4 +68,4 @@ const ArticleShadowLayer = ({ width, height, title, abstract, keywords, contribu
   )
 }
 
-export default ArticleShadowLayer
+export default React.memo(ArticleShadowLayer)
