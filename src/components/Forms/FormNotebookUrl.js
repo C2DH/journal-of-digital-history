240e6b3399c3afef7b93c131b2743deb9ae17252
Daniele Guido <gui.daniele@gmail.com>
2021-06-25 10:38:36 +0200

    form moved to its own component

--- src/components/Forms/FormNotebookUrl.js
+++ src/components/Forms/FormNotebookUrl.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Form , Button} from 'react-bootstrap'
+import { Link2 } from 'react-feather'
 
-const FormNotebookUrl = ({initialValue = '', onChange}) => {
+const FormNotebookUrl = ({initialValue = '', onSubmit}) => {
   const { t } = useTranslation()
   const [value, setValue] = useState(initialValue)
   const handleSubmit = (e) => {
@@ -16,38 +17,58 @@ const FormNotebookUrl = ({initialValue = '', onChange}) => {
       // to
       // https://raw.githubusercontent.com/C2DH/jdh-notebook/features/template/author_guideline_template.ipynb
       // /proxy-githubusercontent/C2DH/jdh-notebook/features/template/author_guideline_template.ipynb
-      onChange({
+      onSubmit({
         value,
         domain,
         proxyValue: `/proxy-githubusercontent/${username}/${repo}/${filepath}`,
         origin: 'github'
       })
     } else {
-      onChange({ value, origin: 'unknown' })
+      // error
+      console.warn('Origin is not fully supported, things can go wrong... value:', value)
+      onSubmit({ value, origin: 'unknown' })
     }
     e.preventDefault();
   }
 
   return (
     <Form onSubmit={handleSubmit}>
-      <Form.Group className="my-5" controlId="formBasicEmail">
-        <Form.Label>Public notebook url</Form.Label>
+      <Form.Group className="mt-5 mb-3" controlId="">
+        <Form.Label>{t('forms.FormNotebookUrl.notebookUrl')}</Form.Label>
         <Form.Control
           defaultValue={value}
           onChange={(e) => setValue(e.target.value)}
           type="url"
           placeholder="https://"
         />
-        <Form.Text className="text-muted">
-          {t('FormNotebookUrl_HelpText')}
-        </Form.Text>
-        <Button variant="link" onClick={() => setValue("https://github.com/C2DH/jdh-notebook/blob/features/template/author_guideline_template.ipynb")}>
-          use guideline template
-        </Button>
+        <Form.Text className="text-muted" dangerouslySetInnerHTML={{
+          __html: t('forms.FormNotebookUrl.notebookUrlDescription')
+        }}/>
+        <p className="mt-2">
+          Check our notebooks
+          <Button className="d-inline mx-2" variant="link" onClick={() => setValue("https://github.com/C2DH/jdh-notebook/blob/master/author_guideline_template.ipynb")}>
+            guideline template
+          </Button>
+          or
+          <Button className="d-inline mx-2" variant="link" onClick={() => setValue("https://github.com/C2DH/jdh-notebook/blob/master/examples/cite_figures.ipynb")}>
+            how to cite figures
+          </Button>
+        </p>
+
+
       </Form.Group>
-      <Button onClick={handleSubmit} className="border border-dark" variant="primary" type="submit">
-        {t('FormNotebookUrl_GenerateLink')}
+      <Button onClick={handleSubmit}
+        variant="primary" type="submit"
+        style={{
+          borderRadius: '1.25rem',
+          paddingLeft: '1rem',
+          paddingRight: '1rem'
+        }}
+      >
+        <span className="mr-2">{t('FormNotebookUrl_GenerateLink')}</span>
+        <Link2 size="16"/>
       </Button>
+
     </Form>
   )
 }
