462b08796982847a60cd85a75c0de39c0206bc64
Daniele Guido <gui.daniele@gmail.com>
2021-06-28 08:14:47 +0200

    add ArticleFingerprint component

--- src/components/Issue/IssueArticlesGrid.js
+++ src/components/Issue/IssueArticlesGrid.js
@@ -1,30 +1,23 @@
 import React from 'react'
 import { Container, Row, Col} from 'react-bootstrap'
 // import { useTranslation } from 'react-i18next'
+import ArticleFingerprint from '../Article/ArticleFingerprint'
+
 
 const IssueArticlesGrid = ({articles = []}) => {
-  console.info('IssueArticlesGrid', articles)
+  // console.info('IssueArticlesGrid', articles)
+
   return (
     <Container>
       <Row>
-        {articles.map((article, i) => (
-          <Col key={i} md={{span:4, offset: i % 2 === 0 ? 2 : 0}}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">
-              {Array(Math.round(Math.random() * 20)).fill(0).map(d => (
-                <circle
-                  cx={100}
-                  cy={100}
-                  // cx={100 + 5*Math.random()}
-                  // cy={100 + 5*Math.random()}
-                  r={80 * Math.random()}
-                  fill="transparent" stroke="#000"
-                  stroke-width={5 * Math.random()}
-                />
-              ))}
-            </svg>
-            <h3 dangerouslySetInnerHTML={{__html: article.data.title }} />
-          </Col>
-        ))}
+        {articles.map((article, i) => {
+          return (
+            <Col key={i} md={{span:4, offset: i % 2 === 0 ? 2 : 0}}>
+              <ArticleFingerprint />
+              <h3 dangerouslySetInnerHTML={{__html: article.data.title }} />
+            </Col>
+          )
+        })}
       </Row>
     </Container>
   )
