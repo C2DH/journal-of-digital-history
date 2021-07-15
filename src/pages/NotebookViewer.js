1ef2198db4091a3114b65e15aa6096d18ee7291e
Daniele Guido <gui.daniele@gmail.com>
2021-07-15 13:24:32 +0200

    add animated loading component in NotebookViewer with percentage

--- src/pages/NotebookViewer.js
+++ src/pages/NotebookViewer.js
@@ -1,4 +1,5 @@
-import React, { useMemo } from 'react'
+import React, { useMemo, useEffect } from 'react'
+import { useSpring, animated, config } from 'react-spring'
 import { useTranslation } from 'react-i18next'
 import { Container, Row, Col } from 'react-bootstrap'
 import { useGetJSON } from '../logic/api/fetchData'
@@ -7,9 +8,13 @@ import { BootstrapColumLayout, StatusSuccess, StatusFetching, StatusIdle } from 
 import Article from './Article'
 import Loading from '../components/Loading'
 
-
+/**
+ * Loading bar inspired by
+ * https://codesandbox.io/s/github/pmndrs/react-spring/tree/master/demo/src/sandboxes/animating-auto
+ */
 const NotebookViewer = ({ match: { params: { encodedUrl }}}) => {
   const { t } = useTranslation()
+  const [animatedProps, api] = useSpring(() => ({ width : 0, opacity:1, config: config.slow }))
 
   const url = useMemo(() => {
     if (!encodedUrl || !encodedUrl.length) {
@@ -21,36 +26,62 @@ const NotebookViewer = ({ match: { params: { encodedUrl }}}) => {
       console.warn(e)
     }
   }, [ encodedUrl ])
-  const { data, error, status } = useGetJSON({ url, delay: 1000})
+  const onDownloadProgress = (e) => {
+    console.info('onDownloadProgress', e.total, e.loaded)
+    if (e.total && e.loaded) {
+      if (e.loaded < e.total) {
+        api.start({ width: 100 * e.loaded / e.total })
+      }
+    }
+  }
+  const { data, error, status } = useGetJSON({ url, delay: 1000, onDownloadProgress })
+
+  useEffect(() => {
+    if(status === StatusFetching) {
+      api.start({ width: 10, opacity: 1 })
+    } else if(status === StatusSuccess) {
+      api.start({ width: 100, opacity: 0 })
+    }
+  }, [status])
+
   if (error) {
     console.error(error)
     return <div>Error <pre>{JSON.stringify(error, null, 2)}</pre></div>
   }
-  console.info('Notebook render:', url ,'from', encodedUrl, status)
-
-  if (status !== StatusSuccess) {
-    return (
-      <Container className="page">
-        <Row>
-          <Col {...BootstrapColumLayout}>
-          {(status === StatusFetching || status === StatusIdle) && (
-            <>
-            <h1 className="my-5">{t('pages.loading.title')}</h1>
-            <Loading />
-            </>
-          )}
-          </Col>
-        </Row>
-      </Container>
-    )
-  }
+  // console.info('Notebook render:', url ,'from', encodedUrl, status)
   return (
-    <div>
-    {status === StatusSuccess
-      ? <Article ipynb={data} memoid={encodedUrl}/>
-      : null
-    }
-    </div>
+    <>
+      <div className="position-fixed w-100" style={{
+        top: 0,
+        left: 0,
+        zIndex: 3
+      }}>
+        <animated.div className="NotebookViewer_loading position-absolute" style={{
+          width: animatedProps.width.interpolate(x => `${x}%`),
+          opacity: animatedProps.opacity
+        }}/>
+        <animated.span className="NotebookViewer_loadingPercentage monospace" style={{
+          opacity: animatedProps.opacity
+        }}>{animatedProps.width.to(x => `${Math.round(x * 10000) / 10000}%`)}</animated.span>
+      </div>
+      {status === StatusSuccess ? null: (
+        <Container className="page">
+          <Row>
+            <Col {...BootstrapColumLayout}>
+            {(status === StatusFetching || status === StatusIdle) && (
+              <>
+              <h1 className="my-5">{t('pages.loading.title')}</h1>
+              <Loading />
+              </>
+            )}
+            </Col>
+          </Row>
+        </Container>
+      )}
+      {status !== StatusSuccess ? null: (
+        <Article ipynb={data} memoid={encodedUrl}/>
+      )}
+    </>
   )
 }
 
