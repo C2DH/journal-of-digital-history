f588c16f1b07d568ee3a95c7e643e81319d7118e
Daniele Guido <gui.daniele@gmail.com>
2021-09-17 12:19:17 +0200

    allow disabled property on LangLink component

--- src/components/LangLink.js
+++ src/components/LangLink.js
@@ -2,7 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { useToWithLang } from '../logic/language'
 
-export default function LangLink({ to, ...props }) {
+export default function LangLink({ to, disabled, ...props }) {
   const toWithLang = useToWithLang(to)
+  if(disabled) {
+    return <Link to={toWithLang} {...props} onClick={(e) => e.preventDefault()}/>
+  }
   return <Link to={toWithLang} {...props} />
 }
