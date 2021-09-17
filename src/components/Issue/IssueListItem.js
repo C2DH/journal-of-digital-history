4eefab07d6cc6d7801e2284be1af40b8ad3c6e26
Daniele Guido <gui.daniele@gmail.com>
2021-09-17 13:47:25 +0200

    Fix/issues loading (#185)
    * remove text
    
    * loading takes the whole height of the page not to have a glitch with the footer
    
    * fakeIssues should reflect the current n of issues
    
    * restyle
    
    * allow disabled property on LangLink component
    
    * use custom images according to pid
    
    * add image for the first issue

--- src/components/Issue/IssueListItem.js
+++ src/components/Issue/IssueListItem.js
@@ -1,47 +1,77 @@
 import React from 'react'
 import LangLink from '../LangLink'
-import {useBoundingClientRect} from '../../hooks/graphics'
+// import {useBoundingClientRect} from '../../hooks/graphics'
+import { useSpring, animated } from 'react-spring'
 
 
-const IssueListItem = ({ issue={}, isFake=false }) => {
-  const [{width: size }, ref] = useBoundingClientRect()
-
+const IssueListItem = ({ issue={}, isFake=false, ordering=0 }) => {
+  const props = useSpring({
+    to: { opacity: 1 }, from: { opacity: 0 },
+    delay: 100*(ordering + 1),
+    reset: isFake
+  })
+  // const [{width: size }, ref] = useBoundingClientRect()
+  // useEffect()
   return (
-    <div className="IssueListItem mt-5" ref={ref}>
-      <LangLink to={isFake ? '#' : `/issue/${issue.pid}`}>
-        <div className="squared  position-relative" style={{
+    <div className="IssueListItem mt-5">
+      <LangLink to={isFake ? '#' : `/issue/${issue.pid}`} disabled={isFake}>
+        <animated.div className="squared position-relative border rounded border-dark" style={{
           backgroundColor: 'var(--dark)',
-          overflow: 'hidden'
+          overflow: 'hidden',
+          ...props
         }}>
-          {isFake ? null : (
-            <svg xmlns="http://www.w3.org/2000/svg"
-              className="position-absolute grow animate-transform"
-              width={size} height={size}
-              style={{background: 'var(--dark)', top: 0 }}
-            >
-              {Array(Math.round(Math.random() * 100)).fill(0).map((d,j) => (
-                <circle
-                  key={j}
-                  // cx={100}
-                  // cy={100}
-                  cx={size/2 + 5*Math.random()}
-                  cy={size/2 + 5*Math.random()}
-                  r={size/3 * Math.random()}
-                  fill="transparent" stroke="var(--primary)"
-                  style={{opacity: .15}}
-                  strokeWidth={7 * Math.random()}
-                />
-              ))}
-            </svg>
-          )}
-        </div>
-        <h3 className="d-block mt-3 pb-0">
-          {issue.name}
+          {isFake ? null: <img src={`/img/issues/${issue.pid}.png`} style={{objectFit: 'cover'}} className="position-absolute top-0 w-100 h-100 left-0"/>}
+        </animated.div>
+        <h3 className="d-block mt-3 pb-0">{issue.name}
         </h3>
+        <p>
+          <span className="text-muted">{issue.pid}&nbsp;</span>
+          <span dangerouslySetInnerHTML={{__html: issue.description }} />
+        </p>
       </LangLink>
-      <p dangerouslySetInnerHTML={{__html: issue.description }} />
     </div>
   )
 }
+
+// const PreviousIssueListItem = ({ issue={}, isFake=false }) => {
+//   const [{width: size }, ref] = useBoundingClientRect()
+//
+//   return (
+//     <div className="IssueListItem mt-5" ref={ref}>
+//       <LangLink to={isFake ? '#' : `/issue/${issue.pid}`}>
+//         <div className="squared  position-relative" style={{
+//           backgroundColor: 'var(--dark)',
+//           overflow: 'hidden'
+//         }}>
+//           {isFake ? null : (
+//             <svg xmlns="http://www.w3.org/2000/svg"
+//               className="position-absolute grow animate-transform"
+//               width={size} height={size}
+//               style={{background: 'var(--dark)', top: 0 }}
+//             >
+//               {Array(Math.round(Math.random() * 100)).fill(0).map((d,j) => (
+//                 <circle
+//                   key={j}
+//                   // cx={100}
+//                   // cy={100}
+//                   cx={size/2 + 5*Math.random()}
+//                   cy={size/2 + 5*Math.random()}
+//                   r={size/3 * Math.random()}
+//                   fill="transparent" stroke="var(--primary)"
+//                   style={{opacity: .15}}
+//                   strokeWidth={7 * Math.random()}
+//                 />
+//               ))}
+//             </svg>
+//           )}
+//         </div>
+//         <h3 className="d-block mt-3 pb-0">
+//           {issue.name}
+//         </h3>
+//       </LangLink>
+//       <p dangerouslySetInnerHTML={{__html: issue.description }} />
+//     </div>
+//   )
+// }
 
 export default IssueListItem
