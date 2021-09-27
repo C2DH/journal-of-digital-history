82acbd1aa44632bc1933edbc4b6747ac13564005
Daniele Guido <gui.daniele@gmail.com>
2021-09-27 13:37:26 +0200

    add Dropdown with HTML content

--- src/components/SwitchLayer.js
+++ src/components/SwitchLayer.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import {Dropdown} from 'react-bootstrap'
+import {ChevronDown, Mail} from 'react-feather'
 import { useTranslation } from 'react-i18next'
 import { DisplayLayerHermeneutics, DisplayLayerNarrative } from '../constants'
 import { useArticleStore } from '../store'
 import '../styles/components/SwitchLayer.scss'
+
+
+const CustomToggle = React.forwardRef(function CustomToggle({ children, onClick }, ref) {
+  return (
+  <a
+    href=""
+    className="d-block"
+    ref={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+    style={{textAlign: 'right', boxShadow: 'none'}}
+  >
+    <span>{children}&nbsp;</span>
+    <ChevronDown size={12}/>
+  </a>
+)
+});
 
 
 const SwitchLayer = ({ disabled, className, binderUrl }) => {
@@ -18,15 +39,32 @@ const SwitchLayer = ({ disabled, className, binderUrl }) => {
         <li key={d} className={displayLayer === d ? 'active': ''}
           onClick={() => setDisplayLayer(d)}>{t(`layers.${d}`)}</li>
       ))}
-      { binderUrl
-        ? (
-          <li>
-            <a target="_blank"  rel="noreferrer" href={binderUrl}>
-              <img src="https://mybinder.org/static/images/badge_logo.svg?v=29bb88d0f5fe83fa7241a8bd5ab75c3abc719ea2ac9591b5448801ff197d0bba86db86dac02ae6150f36eb9dd433ec4496fc1cc36181187057e169a51d82f21d"/>
-            </a>
-          </li>
-        ): null
-      }
+      <Dropdown align="end" >
+    <Dropdown.Toggle menuVariant="dark" as={CustomToggle} id="dropdown-custom-components ">
+      Data
+    </Dropdown.Toggle>
+
+    <Dropdown.Menu align="end" className="shadow border" style={{maxWidth: 350, backgroundColor: 'var(--white)'}}>
+      <Dropdown.Header style={{whiteSpace: 'inherit'}}>
+        <p className="mb-2">
+          Discover the data behind this article.
+        </p>
+        { binderUrl
+          ? (
+              <a target="_blank"  rel="noreferrer" href={binderUrl}>
+                <img src="https://mybinder.org/static/images/badge_logo.svg"/>
+              </a>
+          ): <em>It seems that mybinder is not available for this notebook</em>
+
+        }
+        <p className="text-dark pt-2 mb-0">
+          <Mail size={16}/> Contact the authors to get full access to the data behind the notebook
+        </p>
+      </Dropdown.Header>
+
+    </Dropdown.Menu>
+  </Dropdown>
+
     </ul>
   )
 }
