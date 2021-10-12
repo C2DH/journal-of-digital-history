d0d13526a5debc44d8063f2d94b64d0c73d81471
Daniele Guido <gui.daniele@gmail.com>
2021-10-12 12:00:01 +0200

    fix links in IssueArticleGridItem

--- src/components/Issue/IssueArticleGridItem.js
+++ src/components/Issue/IssueArticleGridItem.js
@@ -7,17 +7,17 @@ import LangLink from '../LangLink'
 import { Badge } from 'react-bootstrap'
 import {useBoundingClientRect} from '../../hooks/graphics'
 import { extractMetadataFromArticle, stripHtml } from '../../logic/api/metadata'
-import { isMobile } from 'react-device-detect'
+// import { isMobile } from 'react-device-detect'
 import '../../styles/components/IssueArticleGridItem.scss'
 
 
-const IssueArticleGridItem = ({ article={}, isFake=false, num=0, total=1, isEditorial }) => {
+const IssueArticleGridItem = ({ article={}, isFake=false, num=0, isEditorial }) => {
   const [{width: size }, ref] = useBoundingClientRect()
   const { title, keywords, excerpt, contributor } = extractMetadataFromArticle(article)
   const { t } = useTranslation()
   return (
     <div className="IssueArticleGridItem" ref={ref}>
-      <LangLink to={isFake ? '#' : `/article/${article.abstract.pid}`}>
+
         <div className="squared" style={{
           backgroundColor: 'transparent',
           overflow: 'hidden'
@@ -25,22 +25,26 @@ const IssueArticleGridItem = ({ article={}, isFake=false, num=0, total=1, isEdit
           <ArticleFingerprint stats={article.fingerprint?.stats}  cells={article.fingerprint?.cells}
             size={size}/>
         </div>
-        {isEditorial ? <Badge bg="secondary">{t('editorial')}</Badge>: num}
+        {isEditorial ? <Badge bg="secondary" className="rounded">{t('editorial')}</Badge>: num}
         <h3 className="d-block mt-1 pb-0">
+          <LangLink to={isFake ? '#' : `/article/${article.abstract.pid}`}>
           {title.map(({content}, i) => (
             <ArticleCellContent key={i} content={stripHtml(content)} hideIdx hideNum/>
           ))}
+          </LangLink>
         </h3>
-        <div className="mb-2">
+
         {contributor.map(({content},i) => (
           <ArticleCellContent className="IssueArticleGridItem_contributor" key={i} content={content} hideIdx hideNum/>
         ))}
-        </div>
+
+        <LangLink to={isFake ? '#' : `/article/${article.abstract.pid}`} className="mb-2">
+
         <ArticleKeywords keywords={keywords}/>
         <blockquote className="d-none d-md-block" dangerouslySetInnerHTML={{
           __html: excerpt
         }} />
-      </LangLink>
+        </LangLink>
     </div>
   )
 }
