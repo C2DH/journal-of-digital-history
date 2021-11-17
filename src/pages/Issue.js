2748babf323d7cfe6416f7fcd2b9f902e1fc1786
Daniele Guido <gui.daniele@gmail.com>
2021-11-17 17:56:18 +0100

    move IssueArticlesGrid in its own file

--- src/pages/Issue.js
+++ src/pages/Issue.js
@@ -3,48 +3,11 @@ import { useTranslation } from 'react-i18next'
 import { Container, Row, Col } from 'react-bootstrap'
 import { useGetJSON } from '../logic/api/fetchData'
 import { BootstrapColumLayout } from '../constants'
-import IssueArticleGridItem from '../components/Issue/IssueArticleGridItem'
+import IssueArticlesGrid from '../components/Issue/IssueArticlesGrid'
 import ErrorViewer from './ErrorViewer'
 import Loading from './Loading'
 import { StatusSuccess } from '../constants'
 
-const IssueArticlesGrid = ({ issue, onError }) => {
-  // console.info('IssueArticlesGrid', articles)
-  const { data, error, status, errorCode } = useGetJSON({
-    url: `/api/articles/?pid=${issue.pid}`
-  })
-  if (typeof onError === 'function' && error) {
-    console.error('IssueArticlesGrid loading error:', errorCode, error)
-  }
-  if (status !== StatusSuccess ) {
-    return null
-  }
-
-  const editorials = []
-  const articles = []
-
-  for (let i=0,j=data.length; i < j; i++) {
-    if (data[i].tags.some((t) => t.name === process.env.REACT_APP_TAG_EDITORIAL)) {
-      editorials.push(data[i])
-    } else {
-      articles.push(data[i])
-    }
-  }
-  return (
-    <Row>
-        {editorials.map((article, i) => (
-          <Col key={i} lg={{ span: 4, offset:0}} md={{span:6, offset:0}}>
-            <IssueArticleGridItem article={article} isEditorial />
-          </Col>
-        ))}
-        {articles.map((article, i) => (
-          <Col key={i + editorials.length} lg={{ span: 4, offset:0}} md={{span:6, offset:0}}>
-            <IssueArticleGridItem article={article} num={i+1} total={articles.length}/>
-          </Col>
-        ))}
-    </Row>
-  )
-}
 
 const Issue = ({ match: { params: { id:issueId }}}) => {
   const { t } = useTranslation()
