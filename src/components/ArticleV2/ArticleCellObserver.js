af37dfce458e747c991dbefd4ffa5cf8f8a4cd80
Daniele Guido <gui.daniele@gmail.com>
2021-12-10 16:39:30 +0100

    add "active" class based on ArticleCellObserver

--- src/components/ArticleV2/ArticleCellObserver.js
+++ src/components/ArticleV2/ArticleCellObserver.js
@@ -1,20 +1,20 @@
 import React, {useEffect } from 'react'
 import { useOnScreen } from '../../hooks/graphics'
 
-const ArticleCellObserver = ({ cell, children, onVisibilityChange, ...rest }) => {
+const ArticleCellObserver = ({ cell, children, onCellIntersectionChange, className, ...rest }) => {
   const [{ isIntersecting, intersectionRatio }, ref] = useOnScreen({
     rootMargin: '-20% 0% -25% 0%',
     threshold: [0, 0.25, 0.75, 1]
   })
   useEffect(() => {
-    if (typeof onVisibilityChange === 'function') {
-      onVisibilityChange({ idx: cell.idx, isIntersecting })
+    if (typeof onCellIntersectionChange === 'function') {
+      onCellIntersectionChange({ idx: cell.idx, isIntersecting })
     } else {
       console.debug('[ArticleCellObserver] cell:', cell.idx, isIntersecting ? 'is in view' : 'disappeared' )
     }
   }, [intersectionRatio, isIntersecting])
   return (
-    <div ref={ref} {...rest}>
+    <div ref={ref} {...rest} className={`ArticleCellObserver ${className} ${isIntersecting ? 'active' : ''}`}>
       {children}
     </div>
   )
