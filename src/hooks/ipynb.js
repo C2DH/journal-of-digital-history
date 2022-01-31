3580c5853ffc71124898cdcb3c99a8445b4b1d70
Daniele Guido <gui.daniele@gmail.com>
2022-01-31 10:33:26 +0100

    useIpynbNotebookParagraphs hook memoize results based on given id

--- src/hooks/ipynb.js
+++ src/hooks/ipynb.js
@@ -4,11 +4,14 @@ import { getArticleTreeFromIpynb } from '../logic/ipynb'
 
 export const useIpynbNotebookParagraphs = ({ id, cells, metadata }) => {
   const treeRef = useRef(null)
-  if (!treeRef.current) {
-    console.info('useIpynbNotebookParagraphs() parsing article')
-    treeRef.current = getArticleTreeFromIpynb({ id, cells, metadata })
+  if (!treeRef.current || treeRef.current.id !== id) {
+    console.debug('[useIpynbNotebookParagraphs] getArticleTreeFromIpynb id:', id, 'cells:', cells.length)
+    treeRef.current = {
+      id,
+      articleTree: getArticleTreeFromIpynb({ id, cells, metadata })
+    }
   } else {
-    console.warn('useIpynbNotebookParagraphs parsing article using ref!')
+    console.debug('[useIpynbNotebookParagraphs] getArticleTreeFromIpynb not working id:', id)
   }
-  return treeRef.current
+  return treeRef.current.articleTree
 }
