a12874dc68c38916d8531306a7aa96553ba34623
Daniele Guido <gui.daniele@gmail.com>
2022-01-31 10:35:27 +0100

    NotFound page can show custom contents

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
+import { BootstrapColumLayout } from '../constants'
 
-const NotFound = ({ className='page', ...rest }) => {
+const NotFound = ({ className='page', children, ...rest }) => {
   const { t } = useTranslation()
   return (
     <Container className={className} {...rest} >
       <Row>
-        <Col md={{offset:2}}>
+        <Col {...BootstrapColumLayout}>
           <h1 className="my-5">{t('pages.notFound.title')}</h1>
           <p>{t('pages.notFound.subheading')}</p>
         </Col>
       </Row>
+
+      {!!children && (
+        <Row>
+          <Col {...BootstrapColumLayout}>
+            {children}
+          </Col>
+        </Row>
+      )}
     </Container>
   )
 }
