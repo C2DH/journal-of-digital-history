0c148096ebe21cf103bc32bcfc6646a74edd636d
Daniele Guido <gui.daniele@gmail.com>
2022-01-31 10:35:51 +0100

    Update Guidelines.js

--- src/pages/Guidelines.js
+++ src/pages/Guidelines.js
@@ -1,79 +1,57 @@
-import React, { useState  } from 'react'
+import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import { Download } from 'react-feather'
 import { useTranslation } from 'react-i18next'
-import ArticleText from '../components/Article/ArticleText'
-import ArticleNote from '../components/Article/ArticleNote'
-import ArticleCellContent from '../components/Article/ArticleCellContent'
-import { useIpynbNotebookParagraphs } from '../hooks/ipynb'
-import { useCurrentWindowDimensions } from '../hooks/graphics'
-import { BootstrapColumLayout, DisplayLayerNarrative } from '../constants'
 import { downloadFile } from '../logic/api/downloadData'
 import source from '../data/mock-api/authorGuideline.json'
-
+import StaticPageLoader from './StaticPageLoader'
+import Article from '../components/ArticleV2'
+import { BootstrapColumLayout } from '../constants'
+//
+const GuidelinesArticle = ({ data, status, isFake=false}) => {
+  const { t } = useTranslation()
+  console.debug('[GuidelinesArticle] props:', {status, isFake})
+  const memoid = process.env.REACT_APP_NOTEBOOK_GUIDELINES_URL + (isFake ? '-fake' : '')
+  return (
+    <>
+    <Article
+      pageBackgroundColor='var(--gray-100)'
+      ipynb={data}
+      memoid={memoid}
+      ignorePublicationStatus
+      plainTitle="Guidelines"
+    >
+      <Container>
+        <Row>
+          <Col {...BootstrapColumLayout}>
+            <a className="btn btn-outline-secondary btn-sm rounded " onClick={(e) => {
+              e.preventDefault()
+              downloadFile({
+                url: '/proxy-githubusercontent/C2DH/jdh-notebook/master/author_guideline_template.ipynb',
+                filename: 'author_guideline_template.ipynb'
+              })
+            }} href="#download">
+              <span className="d-flex align-items-center">
+              <Download className="me-2" size={16} />
+              {t('pages.guidelines.downloadArticleTemplate')}
+              </span>
+            </a>
+          </Col>
+        </Row>
+      </Container>
+    </Article>
+    </>
+  )
+}
 
 const Guidelines = () => {
-  const { t } = useTranslation()
-  const [selectedDataHref, setSelectedDataHref] = useState(null)
-  const articleTree = useIpynbNotebookParagraphs({
-    id: "",
-    cells: source.cells,
-    metadata: source.metadata
-  })
-  const { title, abstract } = articleTree.sections
-
-  const { height, width } =  useCurrentWindowDimensions()
   return (
-    <div className="page">
-    <Container >
-      <Row>
-        <Col {...BootstrapColumLayout}>
-          <div className="my-5">
-          {title.map((props, i) => (
-            <ArticleCellContent hideIdx hideNum {...props} key={i} idx=""/>
-          ))}
-          </div>
-          <h3>Introduction</h3>
-          {abstract.map((props, i) => (
-            <ArticleCellContent hideIdx hideNum {...props} key={i} idx=""/>
-          ))}
-        </Col>
-      </Row>
-      <Row>
-        <Col {...BootstrapColumLayout}>
-          <a className="btn btn-outline-secondary btn-sm rounded " onClick={(e) => {
-            e.preventDefault()
-            downloadFile({
-              url: '/proxy-githubusercontent/C2DH/jdh-notebook/master/author_guideline_template.ipynb',
-              filename: 'author_guideline_template.ipynb'
-            })
-          }} href="#download">
-            <span className="d-flex align-items-center">
-            <Download className="me-2" size={16} />
-            {t('pages.guidelines.downloadArticleTemplate')}
-            </span>
-          </a>
-        </Col>
-      </Row>
-    </Container>
-      <ArticleText
-        className='mt-0'
-        memoid={articleTree.id}
-        headingsPositions={articleTree.headingsPositions}
-        paragraphs={articleTree.paragraphs}
-        paragraphsPositions={articleTree.paragraphsPositions}
-        onDataHrefClick={(d) => setSelectedDataHref(d)}
-        height={height}
-        width={width}
-        disableSwitchLayer
-        anchorPrefix={DisplayLayerNarrative}
-      />
-      {articleTree.citationsFromMetadata
-        ? <ArticleNote articleTree={articleTree} selectedDataHref={selectedDataHref}/>
-        : null
-      }
-
-    </div>
-  );
+    <StaticPageLoader
+      url={process.env.REACT_APP_NOTEBOOK_GUIDELINES_URL}
+      Component={GuidelinesArticle}
+      fakeData={source}
+    />
+  )
 }
+
 export default Guidelines
