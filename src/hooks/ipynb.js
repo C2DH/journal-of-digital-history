1a209f04dc837f1d644da8465550b87c9df9683a
Daniele Guido <gui.daniele@gmail.com>
2022-02-01 14:05:35 +0100

    don't memoize empty articleTrees in useIpynbNotebookParagraphs hook

--- src/hooks/ipynb.js
+++ src/hooks/ipynb.js
@@ -5,13 +5,16 @@ import { getArticleTreeFromIpynb } from '../logic/ipynb'
 export const useIpynbNotebookParagraphs = ({ id, cells, metadata }) => {
   const treeRef = useRef(null)
   if (!treeRef.current || treeRef.current.id !== id) {
-    console.debug('[useIpynbNotebookParagraphs] getArticleTreeFromIpynb id:', id, 'cells:', cells.length)
+    console.debug('[useIpynbNotebookParagraphs] getArticleTreeFromIpynb id:', id, 'n. cells:', cells.length)
+    if (!cells.length) {
+      return getArticleTreeFromIpynb({ id, cells, metadata })
+    }
     treeRef.current = {
       id,
       articleTree: getArticleTreeFromIpynb({ id, cells, metadata })
     }
   } else {
-    console.debug('[useIpynbNotebookParagraphs] getArticleTreeFromIpynb not working id:', id)
+    console.debug('[useIpynbNotebookParagraphs] use memoized articleTree, id:', id)
   }
   return treeRef.current.articleTree
 }
