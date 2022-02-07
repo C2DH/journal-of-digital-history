0a19e26623b678c8eb1f96ced12e3300e0b14556
Daniele Guido <gui.daniele@gmail.com>
2022-02-07 15:37:10 +0100

    fix buttons on home_reel

--- src/components/HomeReel/HomeReel.js
+++ src/components/HomeReel/HomeReel.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useEffect } from 'react'
 import Slider from 'react-slick'
 import { useBoundingClientRect } from '../../hooks/graphics'
 import { useGetRawContents } from '../../logic/api/fetchData'
@@ -6,8 +6,16 @@ import { StatusSuccess } from '../../constants'
 import HomeReelItem from './HomeReelItem'
 import '../../styles/components/HomeReel.scss'
 
-const HomeReel = ({ height=180 }) => {
+const Forward = 1
+const Backward = 0
+
+const HomeReel = ({
+  height=180,
+  delay=1500
+}) => {
   const [{ width, left }, ref] = useBoundingClientRect()
+  const sliderTimer = useRef(null)
+  const sliderDirection = useRef(1)
   const slider = useRef(null)
   // load items
   const { data, status, error } = useGetRawContents({
@@ -46,30 +54,69 @@ const HomeReel = ({ height=180 }) => {
     )
   }
 
-  const settings = {
-    className: "slider variable-width",
-    dots: true,
-    infinite: false,
-    centerMode: true,
-    centerPadding: `0px`,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    pauseOnHover: true,
-    speed: 1000,
-    autoplay: true,
-    autoplaySpeed: 1500,
-    cssEase: "cubic-bezier(0.85, 0, 0.15, 1)"
-  };
-
   const onClickItemHandler = (e, item, idx) => {
     if (slider.current) {
       slider.current.slickGoTo(idx)
     }
   }
 
+  const afterChangeHandler = (args) => {
+    console.debug('[HomeReel] @afterChangeHandler', args)
+    playInfiniteSwingingTimer()
+  }
+
+  const onInitHandler = (args) => {
+    console.debug('[HomeReel] @onInitHandler', args)
+    // stat timer
+    playInfiniteSwingingTimer()
+  }
+  const stopInfiniteSwingingTimer = () => {
+    console.debug('[HomeReel] .stopInfiniteSwingingTimer')
+    clearTimeout(sliderTimer.current)
+  }
+  const playInfiniteSwingingTimer = () => {
+    if (!slider.current) {
+      console.warn('[HomeReel] Slider initialized, but no Ref is present. Try again in 0.1s')
+      sliderTimer.current = setTimeout(playInfiniteSwingingTimer, 100)
+      return
+    }
+    console.debug('[HomeReel] .playInfiniteSwingingTimer()')
+    sliderTimer.current = setTimeout(() => {
+      const idx = slider.current.innerSlider.state.currentSlide
+      const l = slider.current.innerSlider.state.slideCount
+      console.debug('[HomeReel] .playInfiniteSwingingTimer() current:', idx, l, sliderDirection.current)
+
+      if (sliderDirection.current === Forward) {
+        if (idx + 1 < l) {
+          slider.current.slickGoTo(idx + 1)
+        } else {
+          sliderDirection.current = Backward
+          // this is END edge, swithc direction, then next
+          slider.current.slickGoTo(idx - 1)
+        }
+      } else {
+        if (idx > 0) {
+          slider.current.slickGoTo(idx - 1)
+        } else {
+          sliderDirection.current = Forward
+          // this is END edge, swithc direction, then next
+          slider.current.slickGoTo(idx + 1)
+        }
+      }
+    }, slider.current.props.autoplaySpeed || delay)
+  }
+
+  useEffect(() => {
+    return function cleanup() {
+      clearTimeout(sliderTimer.current)
+    }
+  }, [])
+
   return (
-    <div className="HomeReel position-relative" ref={ref} style={{height}}>
+    <div className="HomeReel position-relative"
+      onMouseEnter={stopInfiniteSwingingTimer}
+      onMouseLeave={playInfiniteSwingingTimer}
+      ref={ref} style={{height}}>
       {width > 0 && items.length === 1 && (
         <HomeReelItem
           width={width}
@@ -84,11 +131,29 @@ const HomeReel = ({ height=180 }) => {
           right: -left,
           height,
         }}>
-          <Slider ref={slider} {...settings}>
+          <Slider
+            ref={slider}
+            className="slider variable-width"
+            dots
+            infinite={false}
+            centerMode
+            centerPadding="0px"
+            slidesToShow={1}
+            slidesToScroll={1}
+            variableWidth
+            pauseOnHover={false}
+            speed={1000}
+            autoplay={false}
+            autoplaySpeed={2750}
+            cssEase="cubic-bezier(0.85, 0, 0.15, 1)"
+            afterChange={afterChangeHandler}
+            onInit={onInitHandler}
+          >
             {items.map((item, i) => (
               <HomeReelItem
                 key={i}
                 onClick={(e, item) => onClickItemHandler(e, item, i)}
+                onMouseEnter={stopInfiniteSwingingTimer}
                 height={height}
                 width={width}
                 item={item}
--- src/components/HomeReel/HomeReelItem.js
+++ src/components/HomeReel/HomeReelItem.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Zap } from 'react-feather'
+import { Zap, ArrowRight } from 'react-feather'
 
 const HomeReelItem = ({
   item,
   onClick=function(){},
+  onMouseEnter,
   width,
   height
 }) => {
@@ -19,7 +20,7 @@ const HomeReelItem = ({
           fontSize: '10px'
         }}><b>DRAFT</b> &mdash; not visible in production!</div>
       )}
-      <div className="HomeReel_item p-3 me-5 d-flex align-items-top" onClick={onClickHandler}>
+      <div className="HomeReel_item p-3 me-5 d-flex align-items-top" onClick={onClickHandler} onMouseEnter={onMouseEnter}>
 
         {!!item.zap && (
           <div className="icon me-2">
@@ -32,7 +33,12 @@ const HomeReelItem = ({
         </h2>
         <p className="m-0">{item.description}</p>
         {item.url?.length > 0 && (
-          <a href={item.url} norel="noreferrer">{item.label || item.url}</a>
+          <a href={item.url}
+            className="HomeReel_btn btn btn-sm rounded mt-2"
+            norel="noreferrer"
+            onClick={(e) => e.stopPropagation()}
+          ><ArrowRight size={15}/>&nbsp;&nbsp;{item.label || item.url}
+          </a>
         )}
         </div>
       </div>
