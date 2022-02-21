179ec03b6e7134ad1152d4700ab0311d7b89f056
Daniele Guido <gui.daniele@gmail.com>
2022-02-21 16:47:36 +0100

    refactor JupyterCellListItem component to use the new cell model

--- src/components/FingerprintComposer/JupiterCellListItem.js
+++ src/components/FingerprintComposer/JupiterCellListItem.js
@@ -1,49 +1,99 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Jupitercell from './Jupitercell'
-import { Container, Col, Row, ButtonGroup, Button } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 
-
+/**
+ * Representation of a jupyter cell in the fingerprint vis
+ */
 const JupiterCellListItem = ({
   children,
-  hermeneutics=false,
-  data=false,
+  isHermeneutic=false,
+  isHeading=false,
+  type='markdown',
+  num="1 / 1",
+  initial={},
+  debug=false,
   onChange
 }) => {
-  const [tags, set] = useState({
-    hermeneutics,
-    data
+  const [cell, set] = useState({
+    isHermeneutic,
+    isHeading,
+    type,
+    changed: false,
   })
+
   const onClickHandler=(t) => {
     const ts = {
-      ...tags,
-      ...t
+      ...cell,
+      ...t,
+      changed: true
     }
     set(ts)
-    if (typeof onChange === 'function') {
-      onChange(ts)
-    }
   }
+
+  useEffect(() => {
+    console.debug('[JupiterCellListItem] updated cell:', cell)
+    if (typeof onChange === 'function' && cell.changed) {
+      onChange(cell)
+    }
+  }, [cell])
+
   return (
-    <Container>
-    <Row className="JupiterCellListItem">
-    <Col>
-    <ButtonGroup aria-lable="toggle">
-    <Button variant="outline-dark" size="sm" onClick={() => onClickHandler({hermeneutics:!tags.hermeneutics})}>
-      Hermeneutics
-    </Button>
-    <Button variant="outline-dark" size="sm" onClick={() => onClickHandler({data:!tags.data})}> Data </Button>
-    </ButtonGroup>
-    </Col>
-    <Col md={8}>
-      <Jupitercell
-        isHermeneutics={tags.hermeneutics}
-        isData={tags.data}
-      >
-        {children}
-      </Jupitercell>
-      </Col>
-    </Row>
-    </Container>
+    <div className="JupiterCellListItem">
+      <label className="fw-bold small">
+        {num}
+      </label>
+      <div className="JupiterCellListItem_buttons">
+
+        <Button
+          variant="outline-dark"
+          size="sm"
+          onClick={() => onClickHandler({
+            isHermeneutic: !cell.isHermeneutic
+          })}
+        >
+          Hermeneutics {cell.isHermeneutic && <b>ON</b>}
+        </Button>
+        <Button
+          className="ms-1"
+          variant="outline-accent"
+          size="sm"
+          onClick={() => onClickHandler({
+            type: cell.type==='markdown' ? 'code' : 'markdown'
+          })}
+        >
+          Data {cell.type==='code' && <b>ON</b>}
+        </Button>
+        <Button
+          className="ms-1"
+          variant="outline-dark"
+          size="sm"
+          onClick={() => onClickHandler({
+            isHeading: !cell.isHeading,
+            // force cell markdown type
+            type: 'markdown'
+          })}
+        >
+          Heading {cell.isHeading && <b>ON</b>}
+        </Button>
+      </div>
+      {debug === true && (
+        <div>
+          Current cell representation:
+          <pre>{JSON.stringify(cell, null, 2)}</pre>
+          Original cell:
+          <pre>{JSON.stringify(initial, null, 2)}</pre>
+        </div>
+      )}
+      <div className="mt-1">
+        <Jupitercell
+          isHermeneutics={cell.isHermeneutic}
+          isData={cell.type==='code'}
+        >
+          {children}
+        </Jupitercell>
+      </div>
+    </div>
   );
 }
 
