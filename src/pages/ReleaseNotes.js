386a9843c80439e37b27f2d903fa48a24154f37f
Daniele Guido <gui.daniele@gmail.com>
2022-02-28 11:45:44 +0100

    use StaticPageLoader in releaseNotes

--- src/pages/ReleaseNotes.js
+++ src/pages/ReleaseNotes.js
@@ -1,9 +1,9 @@
 import MarkdownIt from 'markdown-it'
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
-import { BootstrapColumLayout, StatusFetching } from '../constants'
-import { useGetJSON } from '../logic/api/fetchData'
+import { BootstrapFullColumLayout, StatusSuccess } from '../constants'
 import { useTranslation } from 'react-i18next'
+import StaticPageLoader from './StaticPageLoader'
 
 const markdownParser = MarkdownIt({
   html: false,
@@ -12,82 +12,58 @@ const markdownParser = MarkdownIt({
 })
 
 
-const FakeReleases = [
-  {
-    name: '*',
-    published_at: new Date(),
-    body: '',
-    html_url: '',
-    tag_name:'v0.0.0'
-  },
-  {
-    name: '*',
-    published_at: new Date(),
-    body: '',
-    html_url: '',
-    tag_name:'v0.0.0'
-  }
-]
-/**
- * ReleaseNotes page.
- * @component
- * @example
- * return (
- *   <ReleaseNotes />
- * )
- */
 const ReleaseNotes = () => {
   const { t } = useTranslation()
-  // load release notes from github
-  const { data:releases, status } = useGetJSON({
-    url: process.env.REACT_APP_GITHUB_RELEASES_API_ENDPOINT,
-    delay: 100,
-  })
-
   return (
-    <>
-    <Container className="ReleaseNotes page">
-      <Row>
-        <Col {...BootstrapColumLayout}>
-          <h1 className="my-5">{t('pages.releaseNotes.title')}</h1>
-          <p className="mb-5">
-            {t('pages.releaseNotes.subheading', {
-              n: Array.isArray(releases) ? releases.length : '...'
-            })}</p>
-        </Col>
-      </Row>
-      {(Array.isArray(releases) ? releases : FakeReleases).map((release, i) => (
-        <Row key={i}>
-          <Col {...BootstrapColumLayout}>
-          <h2>{release.name}</h2>
-          <a href={release.html_url} target="_blank" rel="noreferrer" >{release.tag_name}</a>
-          <p>
-            {t('dates.LLLL', { date: new Date(release.published_at)})}
-            &nbsp;&mdash;&nbsp;
-            {t('dates.fromNow', { date: new Date(release.published_at)})}
-          </p>
-          <p dangerouslySetInnerHTML={{
-            __html: markdownParser.render(release.body)
-          }} />
-          </Col>
-        </Row>
-      ))}
-      {status === StatusFetching && (
-        <Row>
-          <Col {...BootstrapColumLayout}>
-
-          </Col>
-          loading
-        </Row>
+    <StaticPageLoader
+      url={process.env.REACT_APP_GITHUB_RELEASES_API_ENDPOINT}
+      fakeData={[
+        {
+          name: 'Almost Serenity',
+          published_at: new Date(),
+          body: '...',
+          html_url: '',
+          tag_name:'v0.0.0'
+        },
+        {
+          name: '*',
+          published_at: new Date(),
+          body: '...',
+          html_url: '',
+          tag_name:'v0.0.0'
+        }
+      ]}
+      delay={100}
+      Component={({ data:releases, status }) => (
+        <Container className="ReleaseNotes page" style={{minHeight:'100vh'}}>
+          <Row>
+            <Col {...BootstrapFullColumLayout}>
+              <h1 className="my-5">{t('pages.releaseNotes.title')}</h1>
+              <p className="mb-5">
+                {t('pages.releaseNotes.subheading', {
+                  n: status === StatusSuccess ? releases.length: '*'
+                })}</p>
+            </Col>
+          </Row>
+          {releases.map((release, i) => (
+            <Row key={i}>
+              <Col {...BootstrapFullColumLayout}>
+              <h2>{release.name}</h2>
+              <a href={release.html_url} target="_blank" rel="noreferrer" >{release.tag_name}</a>
+              <p>
+                {t('dates.LLLL', { date: new Date(release.published_at)})}
+                &nbsp;&mdash;&nbsp;
+                {t('dates.fromNow', { date: new Date(release.published_at)})}
+              </p>
+              <p dangerouslySetInnerHTML={{
+                __html: markdownParser.render(release.body)
+              }} />
+              </Col>
+            </Row>
+          ))}
+        </Container>
       )}
-    </Container>
-    {/*
-      <pre>
-        {JSON.stringify(releases, null, 2)}
-        {status} === {StatusSuccess}
-      </pre>
-    */}
-    </>
+    />
   )
 }
 
