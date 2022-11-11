b496621ca4f6f3327aad704d12d6124d75ff9a03
Daniele Guido <gui.daniele@gmail.com>
2022-11-11 16:22:37 +0100

    calculate height using tags

--- src/components/Article/ArticleCellFigure.js
+++ src/components/Article/ArticleCellFigure.js
@@ -14,17 +14,29 @@ const ArticleCellFigure = ({
   figureColumnLayout,
   children,
   containerClassName,
+  active,
+  windowHeight = 100,
 }) => {
-  const isFluidContainer =
-    figure.isCover || (metadata.tags && metadata.tags.includes('full-width'))
+  const tags = Array.isArray(metadata.tags) ? metadata.tags : []
+  const isFluidContainer = figure.isCover || tags.includes('full-width')
+  // calculate optimal figure height and allow special h-tags. Default is windowHeight * .5
+  let figureHeight = tags.reduce((acc, tag) => {
+    const m = tag.match(/^h-(\d+)(px)?$/) // h-50 h-100 or h-100px
+    if (!m) {
+      return acc
+    }
+    if (m[2]) {
+      // e.g `h-500px`
+      return [m[1], m[2]].join('')
+    }
+    // e.g `h-50` `h-100` or `h-25` or `h-200`
+    return Math.max(200, (windowHeight * m[1]) / 100)
+  }, windowHeight * 0.5)
+  figureHeight = Math.max(200, figureHeight)
+
   const captions = outputs.reduce((acc, output) => {
-    if (
-      output.metadata &&
-      Array.isArray(output.metadata?.jdh?.object?.source)
-    ) {
-      acc.push(
-        markdownParser.render(output.metadata.jdh.object.source.join('\n'))
-      )
+    if (output.metadata && Array.isArray(output.metadata?.jdh?.object?.source)) {
+      acc.push(markdownParser.render(output.metadata.jdh.object.source.join('\n')))
     }
     return acc
   }, [])
@@ -36,10 +48,7 @@ const ArticleCellFigure = ({
   let columnLayout =
     figureColumnLayout ??
     outputs.reduce((acc, output) => {
-      if (
-        output.metadata &&
-        output.metadata.jdh?.object?.bootstrapColumLayout
-      ) {
+      if (output.metadata && output.metadata.jdh?.object?.bootstrapColumLayout) {
         acc = { acc, ...output.metadata.jdh?.object?.bootstrapColumLayout }
       }
       return acc
@@ -52,18 +61,21 @@ const ArticleCellFigure = ({
     }
   }
 
+  console.debug('[ArticleCellFigure] active, idx:', figure.idx, active)
+
   return (
-    <div className='ArticleCellFigure'>
+    <div className="ArticleCellFigure">
       <Container className={containerClassName} fluid={isFluidContainer}>
         <Row>
           <Col {...columnLayout}>
             <div>
-              <div className='anchor' id={figure.ref} />
+              <div className="anchor" id={figure.ref} />
               <ArticleCellOutputs
                 hideLabel
                 isJavascriptTrusted={isJavascriptTrusted}
                 cellIdx={figure.idx}
                 outputs={outputs}
+                height={figureHeight}
               />
             </div>
             {children}
@@ -72,7 +84,7 @@ const ArticleCellFigure = ({
       </Container>
       {figure.isCover ? null : (
         <Container>
-          <Row className='small'>
+          <Row className="small">
             <Col {...BootstrapColumLayout}>
               {sourceCode}
               <ArticleFigure figure={figure}>
