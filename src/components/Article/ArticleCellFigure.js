1c6af5fb2229922891729062bed45e8fce0d8f2d
Daniele Guido <gui.daniele@gmail.com>
2022-11-14 17:24:54 +0100

    Fix/scroll into view (#488)
    * add setTimeout to make browser breath before calculating top position of the cell to align
    * get **exact** height for the ArticleCellFigure using tags `h-123px` (images won't scale up)
    * get **aspect-ratio** for the ArticleCellFigure using tags `aspect-ratio-<width>-<height>px` (images will scale up to fit the aspect ratio)
    * use browser height / 2 as default minimal height for all ArticleCellFigure component
    * adapt style for  ArticleCellFigure component
    * add exception for cell without data not being `stderr`
    * add info to decipher aspectRatio in ArticleCellFigure

--- src/components/Article/ArticleCellFigure.js
+++ src/components/Article/ArticleCellFigure.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import ArticleCellOutputs from './ArticleCellOutputs'
 import ArticleFigure from './ArticleFigure'
 import { markdownParser } from '../../logic/ipynb'
 import { BootstrapColumLayout } from '../../constants'
 import { Container, Row, Col } from 'react-bootstrap'
+import '../../styles/components/Article/ArticleCellFigure.scss'
 
 const ArticleCellFigure = ({
   figure,
@@ -14,20 +15,41 @@ const ArticleCellFigure = ({
   figureColumnLayout,
   children,
   containerClassName,
+  windowHeight = 100,
 }) => {
-  const isFluidContainer =
-    figure.isCover || (metadata.tags && metadata.tags.includes('full-width'))
-  const captions = outputs.reduce((acc, output) => {
-    if (
-      output.metadata &&
-      Array.isArray(output.metadata?.jdh?.object?.source)
-    ) {
-      acc.push(
-        markdownParser.render(output.metadata.jdh.object.source.join('\n'))
-      )
+  const tags = Array.isArray(metadata.tags) ? metadata.tags : []
+  const isFluidContainer = figure.isCover || tags.includes('full-width')
+  // retrieve aspectRatio
+  // From https://css-tricks.com/aspect-ratio-boxes/
+  // Even when that is a little unintuitive, like for vertical padding.
+  // This isn’t a hack, but it is weird: padding-top and padding-bottom is based on the parent element’s width.
+  // So if you had an element that is 500px wide, and padding-top of 100%, the padding-top would be 500px.
+  const aspectRatio = tags.reduce((acc, tag) => {
+    const ratio = tag.match(/^aspect-ratio-(\d+)-(\d+)$/) // e.g. aspect-ratio-16-9 w/h
+    if (!ratio) {
+      return acc
     }
-    return acc
-  }, [])
+    return ratio[2] / (ratio[1] || 1)
+  }, undefined)
+  // get figure height if any has been specified with the tags. Otherwise default is windowHeight * .5
+  let figureHeight = tags.reduce((acc, tag) => {
+    const m = tag.match(/^h-(\d+)px$/) // h-100px
+    if (!m) {
+      return acc
+    }
+    return m[1]
+  }, Math.max(200, windowHeight * 0.5))
+
+  const captions = useMemo(
+    () =>
+      outputs.reduce((acc, output) => {
+        if (output.metadata && Array.isArray(output.metadata?.jdh?.object?.source)) {
+          acc.push(markdownParser.render(output.metadata.jdh.object.source.join('\n')))
+        }
+        return acc
+      }, []),
+    outputs,
+  )
 
   if (Array.isArray(metadata.jdh?.object?.source)) {
     captions.push(markdownParser.render(metadata.jdh.object.source.join('\n')))
@@ -36,10 +58,7 @@ const ArticleCellFigure = ({
   let columnLayout =
     figureColumnLayout ??
     outputs.reduce((acc, output) => {
-      if (
-        output.metadata &&
-        output.metadata.jdh?.object?.bootstrapColumLayout
-      ) {
+      if (output.metadata && output.metadata.jdh?.object?.bootstrapColumLayout) {
         acc = { acc, ...output.metadata.jdh?.object?.bootstrapColumLayout }
       }
       return acc
@@ -52,27 +71,47 @@ const ArticleCellFigure = ({
     }
   }
 
+  console.debug(
+    '[ArticleCellFigure] \n - idx:',
+    figure.idx,
+    '\n - aspectRatio:',
+    aspectRatio,
+    '\n - tags:',
+    tags,
+  )
+
   return (
-    <div className='ArticleCellFigure'>
+    <div className={`ArticleCellFigure ${aspectRatio ? 'with-aspect-ratio' : ''}`}>
       <Container className={containerClassName} fluid={isFluidContainer}>
         <Row>
           <Col {...columnLayout}>
-            <div>
-              <div className='anchor' id={figure.ref} />
+            <figure
+              style={
+                !isNaN(aspectRatio)
+                  ? {
+                      paddingTop: `${aspectRatio * 100}%`,
+                    }
+                  : {
+                      minHeight: figureHeight,
+                    }
+              }
+            >
+              <div className="anchor" id={figure.ref} />
               <ArticleCellOutputs
                 hideLabel
                 isJavascriptTrusted={isJavascriptTrusted}
                 cellIdx={figure.idx}
                 outputs={outputs}
+                height={!isNaN(aspectRatio) ? 'auto' : figureHeight}
               />
-            </div>
+            </figure>
             {children}
           </Col>
         </Row>
       </Container>
       {figure.isCover ? null : (
         <Container>
-          <Row className='small'>
+          <Row className="small">
             <Col {...BootstrapColumLayout}>
               {sourceCode}
               <ArticleFigure figure={figure}>
