445da0390f51c05b18b6917cb947fd1086c80e22
Daniele Guido <gui.daniele@gmail.com>
2022-11-18 14:25:20 +0100

    v4.0.2 - patch Hotfix/get homepage contents from wiki (#491)
    * add REACT_APP_WIKI_HOMEPAGE variable in env file
    
    * fix flashing Cookies message when LocalStorage it is not ready
    
    * Delete mock-home-ipynb.json
    
    * Update Home component and add scss style

--- src/components/Cookies/Cookies.js
+++ src/components/Cookies/Cookies.js
@@ -1,54 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Button, Form, Container } from 'react-bootstrap'
 import { useStore } from '../../store'
 import { TermsOfUseRoute } from '../../constants'
 import LangLink from '../LangLink'
 import styles from './Cookies.module.scss'
+import { useTimeout } from '../../hooks/timeout'
 
 const Cookies = ({ defaultAcceptCookies }) => {
-    const acceptCookies =  useStore((state) => state.acceptCookies);
-    const acceptAnalyticsCookies =  useStore((state) => state.acceptAnalyticsCookies);
-    const setAcceptCookies = useStore((state) => state.setAcceptCookies);
-    const setAcceptAnalyticsCookies = useStore((state) => state.setAcceptAnalyticsCookies);
-    const { t } = useTranslation()
-    const handleChange = (e) => {
-      setAcceptAnalyticsCookies(e.target.checked)
-    }
-    const handleClickAgree = () => {
-      setAcceptCookies()
-    }
-    if (defaultAcceptCookies || acceptCookies) {
-      return null
-    }
-    return (
-      <div style={{position: 'fixed', bottom: 0, background: 'var(--secondary)', color: 'white', width: '100%', zIndex:1060}}>
-        <Container className="py-4">
-          <p className={styles.disclaimer}>
-            We uses cookies and other data to deliver, maintain and improve
-            the platform (<b>"functional" cookies</b>).
-            {/*
+  const [isStoreReady, setStoreReady] = useState(false)
+  const acceptCookies = useStore((state) => state.acceptCookies)
+  const acceptAnalyticsCookies = useStore((state) => state.acceptAnalyticsCookies)
+  const setAcceptCookies = useStore((state) => state.setAcceptCookies)
+  const setAcceptAnalyticsCookies = useStore((state) => state.setAcceptAnalyticsCookies)
+  const { t } = useTranslation()
+  const handleChange = (e) => {
+    setAcceptAnalyticsCookies(e.target.checked)
+  }
+  const handleClickAgree = () => {
+    setAcceptCookies()
+  }
+  console.debug('[Cookies] \n - isStoreReady:', isStoreReady)
+
+  useTimeout(() => {
+    setStoreReady(true)
+  }, 2000)
+
+  if (defaultAcceptCookies || !isStoreReady) {
+    return null
+  }
+  console.debug('[Cookies] \n - acceptCookies:', acceptCookies)
+  if (acceptCookies) {
+    return null
+  }
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        bottom: 0,
+        background: 'var(--secondary)',
+        color: 'white',
+        width: '100%',
+        zIndex: 1060,
+      }}
+    >
+      <Container className="py-4">
+        <p className={styles.disclaimer}>
+          We uses cookies and other data to deliver, maintain and improve the platform (
+          <b>"functional" cookies</b>).
+          {/*
               <pre style={{color: 'white'}}>{JSON.stringify({acceptCookies, acceptAnalyticsCookies})}</pre>
             */}
-          </p>
-          <Form>
-            <Form.Switch
-              id="agree-analytics-data"
-              label="I also agree sending analytics data"
-              checked={acceptAnalyticsCookies === true}
-              onChange={handleChange}
-            />
-          </Form>
-            <p className={styles.agreement}>
-            By browsing this website you agree to our cookie policy.
-            Visit <LangLink to={TermsOfUseRoute.to}>{t(TermsOfUseRoute.label)}</LangLink> to review your options later.
-            </p>
-            <div className="mx-3 my-0 my-md-3">
-            <Button className={styles.AgreeButton} onClick={handleClickAgree}>Agree</Button>
-            </div>
-        </Container>
-      </div>
-    )
+        </p>
+        <Form>
+          <Form.Switch
+            id="agree-analytics-data"
+            label="I also agree sending analytics data"
+            checked={acceptAnalyticsCookies === true}
+            onChange={handleChange}
+          />
+        </Form>
+        <p className={styles.agreement}>
+          By browsing this website you agree to our cookie policy. Visit{' '}
+          <LangLink to={TermsOfUseRoute.to}>{t(TermsOfUseRoute.label)}</LangLink> to review your
+          options later.
+        </p>
+        <div className="mx-3 my-0 my-md-3">
+          <Button className={styles.AgreeButton} onClick={handleClickAgree}>
+            Agree
+          </Button>
+        </div>
+      </Container>
+    </div>
+  )
 }
 
 export default Cookies
---
+++ src/hooks/timeout.js
@@ -0,0 +1,22 @@
+import { useRef, useEffect } from 'react'
+
+export function useTimeout(callback, delay) {
+  const savedCallback = useRef(callback)
+
+  // Remember the latest callback if it changes.
+  useEffect(() => {
+    savedCallback.current = callback
+  }, [callback])
+
+  // Set up the timeout.
+  useEffect(() => {
+    // Don't schedule if no delay is specified.
+    if (delay === null) {
+      return
+    }
+
+    const id = setTimeout(() => savedCallback.current(), delay)
+
+    return () => clearTimeout(id)
+  }, [delay])
+}
