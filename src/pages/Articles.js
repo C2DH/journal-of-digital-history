a45b6238d76b202ce35d9cf23acea04a8b86aff2
Daniele Guido <gui.daniele@gmail.com>
2022-12-01 21:47:11 +0100

    add OrderByDropdown and react to query params

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -2,7 +2,7 @@ import React, { useRef, useState, useMemo, useLayoutEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import groupBy from 'lodash/groupBy'
 import { Container, Row, Col } from 'react-bootstrap'
-import { useSpring, config} from 'react-spring'
+import { useSpring, config } from 'react-spring'
 import { useHistory } from 'react-router'
 import StaticPageLoader from './StaticPageLoader'
 import IssueArticles from '../components/Issue/IssueArticles'
@@ -17,11 +17,15 @@ import {
   LayerNarrative,
   LayerHermeneutics,
   StatusSuccess,
-  IsMobile
+  IsMobile,
 } from '../constants'
 import { useBoundingClientRect } from '../hooks/graphics'
 import '../styles/pages/Articles.scss'
-
+import { useQueryParams, withDefault } from 'use-query-params'
+import { asEnumParam } from '../logic/params'
+import OrderByDropdown from '../components/OrderByDropdown'
+import { sort } from 'd3-array'
+import Article from '../models/Article'
 
 const ShowMoreLabel = ({ active, n }) => {
   const { t } = useTranslation()
@@ -29,39 +33,56 @@ const ShowMoreLabel = ({ active, n }) => {
     return null
   }
   return (
-    <span>{t(active ? 'dimensions.actions.showLess': 'dimensions.actions.showMore', { n })}</span>
+    <span>{t(active ? 'dimensions.actions.showLess' : 'dimensions.actions.showMore', { n })}</span>
   )
 }
+const OrderByQueryParam = 'orderBy'
+const OrderByIssue = 'issue'
+const OrderByPublicationDateAsc = 'dateAsc'
+const OrderByPublicationDateDesc = 'dateDesc'
 
-
-
+const AvailablesOrderByComparators = {
+  [OrderByIssue]: () => {},
+  [OrderByPublicationDateAsc]: (a, b) => a.publication_date - b.publication_date,
+  [OrderByPublicationDateDesc]: (a, b) => b.publication_date - a.publication_date,
+}
 
 const ArticlesGrid = ({
-  data:response=[],
+  data: response = [],
   url,
   issueId,
   status,
   // tag ategories to keep
-  categories=['narrative', 'tool']
+  categories = ['narrative', 'tool'],
 }) => {
   const { t } = useTranslation()
+  const [{ [OrderByQueryParam]: orderBy }, setQuery] = useQueryParams({
+    [OrderByQueryParam]: withDefault(
+      asEnumParam(Object.keys(AvailablesOrderByComparators)),
+      OrderByIssue,
+    ),
+  })
+
   const [selected, setSelected] = useState(null)
   // pagination api contains results in data
-  const data = response.results || []
-  const [{  width }, ref] = useBoundingClientRect()
+
+  const [{ width }, ref] = useBoundingClientRect()
   const history = useHistory()
-  const animatedRef = useRef({ idx: '', length: '', datum:{}});
+  const animatedRef = useRef({ idx: '', length: '', datum: {} })
   const [animatedProps, setAnimatedProps] = useSpring(() => ({
     from: { x: 0, y: 0, id: '0-0', color: 'red', backgroundColor: 'transparent' },
-    x : 0, y: 0, opacity:0,
+    x: 0,
+    y: 0,
+    opacity: 0,
     id: '0-0',
     color: 'var(--white)',
     backgroundColor: 'var(--secondary)',
-    config: config.stiff
+    config: config.stiff,
   }))
-
+  const data = (response.results || []).map((d) => new Article(d))
+  const articles = sort(data, AvailablesOrderByComparators[orderBy])
   const { articlesByIssue, issues, showFilters } = useMemo(() => {
-    if(status !== StatusSuccess) {
+    if (status !== StatusSuccess) {
       return {
         articlesByIssue: {},
         issues: [],
@@ -71,14 +92,14 @@ const ArticlesGrid = ({
     }
     const sortedItems = data.map((item, idx) => ({
       ...item,
-      idx
+      idx,
     }))
     const articlesByIssue = groupBy(sortedItems, 'issue.pid')
-    const issues = Object.keys(articlesByIssue).sort((a,b) => {
+    const issues = Object.keys(articlesByIssue).sort((a, b) => {
       return articlesByIssue[a][0].issue.pid < articlesByIssue[b][0].issue.pid
     })
     const showFilters = data.reduce((acc, d) => {
-      return acc || d.tags.some(t => categories.includes(t.category))
+      return acc || d.tags.some((t) => categories.includes(t.category))
     }, false)
     return { articlesByIssue, issues, showFilters }
   }, [url, status])
@@ -90,17 +111,23 @@ const ArticlesGrid = ({
 
     const TagListItem = (props) => {
       return (
-        <DimensionGroupListItem {...props}>{({ group, name:category }) => {
-            const tag = data[group.indices[0]].tags
-              .find(t => t.category === category && t.name === group.key)
+        <DimensionGroupListItem {...props}>
+          {({ group, name: category }) => {
+            const tag = data[group.indices[0]].tags.find(
+              (t) => t.category === category && t.name === group.key,
+            )
             return (
-              <div className="d-flex align-items-center flex-nowrap" title={tag.data?.info?.summary || group.key}>
+              <div
+                className="d-flex align-items-center flex-nowrap"
+                title={tag.data?.info?.summary || group.key}
+              >
                 <div>{group.key}</div>
                 <div className="badge mx-1">{tag.data.language}</div>
                 <div>({group.count})</div>
               </div>
             )
-          }}</DimensionGroupListItem>
+          }}
+        </DimensionGroupListItem>
       )
     }
 
@@ -111,7 +138,7 @@ const ArticlesGrid = ({
         if (IsMobile) {
           return 5
         }
-        if (activeGroups > 0 ) {
+        if (activeGroups > 0) {
           return 10
         }
         // according to group composition
@@ -120,21 +147,23 @@ const ArticlesGrid = ({
         }).length
         return Math.min(10, Math.max(wished, 10))
       },
-      fn: (d) => d.tags.filter(t => t.category === category).map(t => t.name),
-      sortFn: (a,b) => {
+      fn: (d) => d.tags.filter((t) => t.category === category).map((t) => t.name),
+      sortFn: (a, b) => {
         return a.indices.length === b.indices.length
           ? a.key > b.key
-            ? 1 : -1
-          : a.indices.length > b.indices.length ? -1 : 1
+            ? 1
+            : -1
+          : a.indices.length > b.indices.length
+          ? -1
+          : 1
       },
       isArray: true,
-      ListItem: category === 'tool' ? TagListItem : DimensionGroupListItem
+      ListItem: category === 'tool' ? TagListItem : DimensionGroupListItem,
     }))
   }, [status])
 
-
   const onArticleMouseMoveHandler = (e, datum, idx, article, bounds) => {
-    if (!isNaN(idx) && animatedRef.current.idx !== idx ) {
+    if (!isNaN(idx) && animatedRef.current.idx !== idx) {
       animatedRef.current.idx = idx
       animatedRef.current.length = article.fingerprint.cells.length
       animatedRef.current.datum = datum
@@ -146,17 +175,19 @@ const ArticlesGrid = ({
       x,
       y,
       id: [article.abstract.id || 0, isNaN(idx) ? 0 : idx].join('-'),
-      color: datum.type === 'code'
-        ? 'var(--white)'
-        : datum.isHermeneutic
+      color:
+        datum.type === 'code'
+          ? 'var(--white)'
+          : datum.isHermeneutic
           ? 'var(--secondary)'
           : 'var(--white)',
-      backgroundColor: datum.type === 'code'
-        ? 'var(--accent)'
-        : datum.isHermeneutic
+      backgroundColor:
+        datum.type === 'code'
+          ? 'var(--accent)'
+          : datum.isHermeneutic
           ? 'var(--primary)'
           : 'var(--secondary)',
-      opacity: 1
+      opacity: 1,
     })
   }
   const onArticleMouseOutHandler = () => {
@@ -167,14 +198,18 @@ const ArticlesGrid = ({
     e.stopPropagation()
     // link to specific cell in article
     const url = idx
-      ? `/en/article/${article.abstract.pid}?${DisplayLayerCellIdxQueryParam}=${idx}&${DisplayLayerQueryParam}=${datum.isHermeneutic ? LayerHermeneutics : LayerNarrative }`
+      ? `/en/article/${
+          article.abstract.pid
+        }?${DisplayLayerCellIdxQueryParam}=${idx}&${DisplayLayerQueryParam}=${
+          datum.isHermeneutic ? LayerHermeneutics : LayerNarrative
+        }`
       : `/en/article/${article.abstract.pid}`
-    history.push(url);
+    history.push(url)
   }
 
   const onFacetsSelectHandler = (name, indices) => {
     console.debug('[Articles] @onSelect', name, indices)
-    setSelected(indices)
+    setSelected({ indices })
   }
 
   useLayoutEffect(() => {
@@ -182,30 +217,43 @@ const ArticlesGrid = ({
     if (issueId && status === StatusSuccess) {
       console.debug('[Articles] goto issueId:', issueId)
       const element = document.getElementById('anchor-' + issueId)
-      element && element.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-        inline: "nearest"
-      })
+      element &&
+        element.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+          inline: 'nearest',
+        })
     }
   }, [status])
 
   useLayoutEffect(() => {
     setAnimatedProps.start({ opacity: 0 })
   }, [selected])
-  console.debug('[Articles] \n- data:', Array.isArray(data), data,'\n- issueId:', issueId)
-
+  console.debug(
+    '[Articles] \n- articles:',
+    Array.isArray(articles),
+    articles,
+    '\n- issueId:',
+    issueId,
+  )
 
   return (
     <Container ref={ref} className="Articles Issue page ">
-      <ArticleFingerprintTooltip
-        forwardedRef={animatedRef}
-        animatedProps={animatedProps}
-      />
+      <ArticleFingerprintTooltip forwardedRef={animatedRef} animatedProps={animatedProps} />
       <Row className="mb-3">
         <Col {...BootstrapColumLayout}>
           <h1 className="mt-5">{t('pages.articles.title')}</h1>
-          {showFilters && <p>{t('pages.articles.subheading')}</p>}
+          <div className="d-flex align-items-center mb-2">
+            {showFilters && <p className="me-2 mb-0">{t('pages.articles.subheading')}</p>}
+            <OrderByDropdown
+              values={Object.keys(AvailablesOrderByComparators).map((value) => ({
+                value,
+                label: t(`orderBy${value}`),
+              }))}
+              title={t(`orderBy${orderBy}`)}
+              onChange={({ value }) => setQuery({ [OrderByQueryParam]: value })}
+            />
+          </div>
         </Col>
       </Row>
       {showFilters && (
@@ -219,43 +267,53 @@ const ArticlesGrid = ({
               ShowMoreLabel={ShowMoreLabel}
               className="Articles_facets"
             />
-
-        </Col>
-      </Row>
+          </Col>
+        </Row>
       )}
 
-      {issues.map((id) => {
-        const issue = articlesByIssue[id][0].issue
-        return (
-          <React.Fragment key={id}>
-            <Row>
-              <Col {...BootstrapColumLayout}>
-                <a className="anchor" id={`anchor-${id}`} />
-                <Issue item={issue} />
-              </Col>
-            </Row>
-            <IssueArticles
-              selected={selected}
-              data={articlesByIssue[id]}
-              onArticleMouseMove={onArticleMouseMoveHandler}
-              onArticleClick={onArticleClickHandler}
-              onArticleMouseOut={onArticleMouseOutHandler}
+      {orderBy === OrderByIssue &&
+        issues.map((id) => {
+          const issue = articlesByIssue[id][0].issue
+          return (
+            <React.Fragment key={id}>
+              <Row>
+                <Col {...BootstrapColumLayout}>
+                  <a className="anchor" id={`anchor-${id}`} />
+                  <Issue item={issue} />
+                </Col>
+              </Row>
+              <IssueArticles
+                selected={selected}
+                data={articlesByIssue[id]}
+                onArticleMouseMove={onArticleMouseMoveHandler}
+                onArticleClick={onArticleClickHandler}
+                onArticleMouseOut={onArticleMouseOutHandler}
               />
-          </React.Fragment>
-        )
-      })}
+            </React.Fragment>
+          )
+        })}
+      {[OrderByPublicationDateAsc, OrderByPublicationDateDesc].includes(orderBy) && (
+        <IssueArticles
+          selected={selected}
+          data={articles}
+          onArticleMouseMove={onArticleMouseMoveHandler}
+          onArticleClick={onArticleClickHandler}
+          onArticleMouseOut={onArticleMouseOutHandler}
+          respectOrdering
+        />
+      )}
     </Container>
   )
 }
 
-const Articles = ({ match: { params: { id:issueId }}}) => {
+const Articles = ({
+  match: {
+    params: { id: issueId },
+  },
+}) => {
   console.debug('[Articles] issueId', issueId)
   return (
-    <StaticPageLoader
-      url="/api/articles?limit=100"
-      Component={ArticlesGrid}
-      issueId={issueId}
-    />
+    <StaticPageLoader url="/api/articles?limit=100" Component={ArticlesGrid} issueId={issueId} />
   )
 }
 
