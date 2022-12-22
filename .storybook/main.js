a5ebfa1531940c639e1da35a543454b06ae2f0c2
Daniele Guido <gui.daniele@gmail.com>
2022-12-22 10:38:19 +0100

    upgrade to react 18 and webpack 5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,11 @@
 module.exports = {
-  stories: [
-    '../src/**/*.stories.mdx',
-    '../src/**/*.stories.js',
-    // "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.js'
+  // "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/preset-create-react-app',
-    'storybook-dark-mode',
-  ],
+
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/preset-create-react-app', 'storybook-dark-mode'],
   framework: '@storybook/react',
-}
+  core: {
+    builder: 'webpack5'
+  }
+};
