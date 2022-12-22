6deb4f1d57efc42b4d4f0618d3bb00237571a2a9
Daniele Guido <gui.daniele@gmail.com>
2022-12-22 16:42:07 +0100

    add grouping algo

--- src/components/ToC.js
+++ src/components/ToC.js
@@ -4,9 +4,35 @@ import '../styles/components/ToC.scss'
 
 const ToC = ({ steps = [], width = 200, onClick, className = '' }) => {
   console.debug(
-    'ToC rendered',
+    '[ToC] rendered',
     steps.map((d) => d.active),
   )
+  // get tree of headings and figures last stuff
+  const groups = steps.reduce(
+    (acc, d, i, arr) => {
+      if (d.level.indexOf('H') !== 0) {
+        acc.buffer.push(d)
+        // if this is the last one
+        if (i + 1 === arr.length) {
+          acc.groups.push([...acc.buffer])
+          acc.buffer = []
+        }
+      } else {
+        if (acc.buffer.length) {
+          acc.groups.push([...acc.buffer])
+          acc.buffer = []
+        }
+        acc.groups.push(d)
+      }
+      if (i + 1 === arr.length) {
+        return acc.groups
+      }
+      return acc
+    },
+    { buffer: [], groups: [] },
+  )
+  console.debug('[ToC] rendered with groups:', groups)
+
   return (
     <div className={`ToC ${className}`} style={{ overflow: 'scroll' }}>
       {steps.map((step) => (
