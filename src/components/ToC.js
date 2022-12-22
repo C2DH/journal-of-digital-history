45496a229578e82b5335c68c79627a3489277b8d
Daniele Guido <gui.daniele@gmail.com>
2022-12-22 12:15:12 +0100

    Create ToC.js

---
+++ src/components/ToC.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ToCStep from './ToCStep'
+
+const ToC = ({ steps = [], width = 200, onClick, className = '' }) => {
+  return (
+    <div className={`ToC ${className}`} style={{ overflow: 'scroll' }}>
+      {steps.map((step) => (
+        <ToCStep key={step.id} {...step} width={width} onClick={onClick} />
+      ))}
+    </div>
+  )
+}
+
+export default ToC
