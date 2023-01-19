fee6d7c324f5ecbb2645aabbcee2c2993e26f1f6
Daniele Guido <gui.daniele@gmail.com>
2023-01-19 16:41:14 +0100

    Feature/with precomputed height (#507)
    * remove LazyFigure as it is redundant here
    
    * Create NotebookWithFigures.stories.js
    
    * add picture content holder
    
    * aspect ratio tags get precedence on height
    
    * Create NotebookWithBokeh.stories.js
    
    * Update NotebookWithFigures.stories.js
    
    * clean http uris and eslint
    
    * add netlify basic make command
    
    requires a _redirects file and .netlify folder ;)

--- Makefile
+++ Makefile
@@ -19,11 +19,17 @@ run-build:
 	REACT_APP_GIT_REVISION=$(shell git rev-parse --short HEAD) \
 	yarn build
 
-run-build-noindex:
+run-build-netlify:
 	REACT_APP_GIT_TAG=$(shell git describe --tags --abbrev=0 HEAD) \
 	REACT_APP_GIT_BRANCH=$(shell git rev-parse --abbrev-ref HEAD) \
 	REACT_APP_GIT_REVISION=$(shell git rev-parse --short HEAD) \
-	yarn build-noindex
+	yarn build && \
+	cp _redirects build/ && \
+	netlify deploy --alias=${BUILD_TAG} --dir=build
+
+run-deploy-netlify:
+	cp _redirects build/ && \
+	netlify deploy --alias=${BUILD_TAG} --dir=build
 
 build-docker-image:
 	docker build -t c2dhunilu/journal-of-digital-history:${BUILD_TAG} \
--- src/components/Article/ArticleCellFigure.js
+++ src/components/Article/ArticleCellFigure.js
@@ -5,7 +5,7 @@ import { markdownParser } from '../../logic/ipynb'
 import { BootstrapColumLayout } from '../../constants'
 import { Container, Row, Col } from 'react-bootstrap'
 import '../../styles/components/Article/ArticleCellFigure.scss'
-import LazyFigure from '../LazyFigure'
+// import LazyFigure from '../LazyFigure'
 
 const ArticleCellFigure = ({
   figure,
@@ -17,8 +17,9 @@ const ArticleCellFigure = ({
   children,
   containerClassName,
   windowHeight = 100,
-  lazy = true,
-  withTransition = false,
+  // lazy = false,
+  // withTransition = false,
+  active = false,
 }) => {
   const tags = Array.isArray(metadata.tags) ? metadata.tags : []
   const isFluidContainer = figure.isCover || tags.includes('full-width')
@@ -35,33 +36,38 @@ const ArticleCellFigure = ({
     return ratio[2] / (ratio[1] || 1)
   }, undefined)
   // get figure height if any has been specified with the tags. Otherwise default is windowHeight * .5
-  let figureHeight = tags.reduce((acc, tag) => {
-    const m = tag.match(/^h-(\d+)px$/) // h-100px
-    if (!m) {
-      return acc
-    }
-    return m[1]
-  }, Math.max(200, windowHeight * 0.5))
+  let figureHeight = figure.isCover
+    ? windowHeight * 0.8
+    : tags.reduce((acc, tag) => {
+        const m = tag.match(/^h-(\d+)px$/) // h-100px
+        if (!m) {
+          return acc
+        }
+        return m[1]
+      }, Math.max(200, windowHeight * 0.5))
 
-  const { captions, pictures } = useMemo(
+  const { captions, pictures, otherOutputs } = useMemo(
     () =>
       outputs.reduce(
         (acc, output = {}) => {
           if (output.metadata && Array.isArray(output.metadata?.jdh?.object?.source)) {
+            // look for catpions in the outputs metadata
             acc.captions.push(markdownParser.render(output.metadata.jdh.object.source.join('\n')))
           }
-          // get pictures as dict of encoded and src
-          acc.pictures.push(
-            ...Object.keys(output.data ?? {})
-              .filter((mimetype) => mimetype.indexOf('image/') === 0)
-              .map((mimetype) => ({
-                src: output.metadata?.jdh?.object?.src,
-                base64: `data:${mimetype};base64,${output.data[mimetype]}`,
-              })),
-          )
+          const mimetypes = Object.keys(output.data ?? {})
+          const mimetype = mimetypes.find((d) => d.indexOf('image/') === 0)
+          if (mimetype) {
+            acc.pictures.push({
+              ...output,
+              src: output.metadata?.jdh?.object?.src,
+              base64: `data:${mimetype};base64,${output.data[mimetype]}`,
+            })
+          } else {
+            acc.otherOutputs.push(output)
+          }
           return acc
         },
-        { captions: [], pictures: [] },
+        { captions: [], pictures: [], otherOutputs: [] },
       ),
     [figure.idx],
   )
@@ -95,14 +101,51 @@ const ArticleCellFigure = ({
     tags,
     '\n - n.pictures:',
     pictures.length,
+    '\n - active:',
+    active,
   )
 
   return (
-    <div className={`ArticleCellFigure ${aspectRatio ? 'with-aspect-ratio' : ''}`}>
+    <div className={`ArticleCellFigure ${active ? 'active' : ''}`}>
       <Container className={containerClassName} fluid={isFluidContainer}>
         <Row>
           <Col {...columnLayout}>
-            {pictures.length > 0 && lazy ? (
+            {otherOutputs.length > 0 && (
+              <figure>
+                <div className="anchor" id={figure.ref} />
+                <ArticleCellOutputs
+                  hideLabel
+                  isJavascriptTrusted={isJavascriptTrusted}
+                  cellIdx={figure.idx}
+                  outputs={otherOutputs}
+                />
+              </figure>
+            )}
+            {pictures.map(({ base64 }, i) => (
+              <figure
+                key={i}
+                style={
+                  !isNaN(aspectRatio)
+                    ? {
+                        paddingTop: `${aspectRatio * 100}%`,
+                        position: 'relative',
+                      }
+                    : {
+                        height: parseInt(figureHeight),
+                      }
+                }
+              >
+                <div
+                  className={`ArticleCellFigure_picture ${figure.isCover ? 'with-cover' : ''} ${
+                    !isNaN(aspectRatio) ? 'with-aspect-ratio' : ''
+                  }`}
+                  style={{
+                    backgroundImage: `url(${base64})`,
+                  }}
+                />
+              </figure>
+            ))}
+            {/* {pictures.length > 0 && lazy ? (
               pictures.map(({ src, base64 }, i) => (
                 <LazyFigure
                   aspectRatio={aspectRatio}
@@ -112,29 +155,7 @@ const ArticleCellFigure = ({
                   base64={base64}
                   withTransition={withTransition}
                 />
-              ))
-            ) : outputs.length === 0 ? null : (
-              <figure
-                style={
-                  !isNaN(aspectRatio)
-                    ? {
-                        paddingTop: `${aspectRatio * 100}%`,
-                      }
-                    : {
-                        minHeight: figureHeight,
-                      }
-                }
-              >
-                <div className="anchor" id={figure.ref} />
-                <ArticleCellOutputs
-                  hideLabel
-                  isJavascriptTrusted={isJavascriptTrusted}
-                  cellIdx={figure.idx}
-                  outputs={outputs}
-                  height={!isNaN(aspectRatio) ? 'auto' : figureHeight}
-                />
-              </figure>
-            )}
+              )) */}
             {children}
           </Col>
         </Row>
