b1e9e4c0a46687fb3004807044654fc3e1d3f5fe
Daniele Guido <gui.daniele@gmail.com>
2023-06-01 17:19:41 +0200

    Update Guidelines.v2.js

--- src/pages/Guidelines.v2.js
+++ src/pages/Guidelines.v2.js
@@ -4,6 +4,10 @@ import StaticPageLoader from './StaticPageLoader'
 import { BootstrapFullColumLayout } from '../constants'
 import MarkdownIt from 'markdown-it'
 import { useParams } from 'react-router-dom'
+import LangLink from '../components/LangLink'
+import { Book, BookOpen } from 'react-feather'
+import GuidelinesNotebookViewer from '../components/GuidelinesNotebookViewer'
+import { useCurrentWindowDimensions } from '../hooks/graphics'
 
 const markdownParser = MarkdownIt({
   html: false,
@@ -11,58 +15,83 @@ const markdownParser = MarkdownIt({
   typographer: true,
 })
 
-const GuidelineSection = ({ section = '' }) => {
-  // take the title from the first line
-  const parts = section.split('\n\n')
-  // render the title
-  const title = markdownParser.render(parts[0] + '\n\n')
-  return (
-    <section>
-      <div dangerouslySetInnerHTML={{ __html: title }} />
-      {title}
-      <pre>{JSON.stringify(parts, null, 2)}</pre>
-    </section>
-  )
-}
+// const GuidelineSection = ({ section = '' }) => {
+//   // take the title from the first line
+//   const parts = section.split('\n\n')
+//   // render the title
+//   const title = markdownParser.render(parts[0] + '\n\n')
+//   return (
+//     <section>
+//       <div dangerouslySetInnerHTML={{ __html: title }} />
+//       <div dangerouslySetInnerHTML={{ __html: markdownParser.render(parts[1] || '') }} />
+//     </section>
+//   )
+// }
 
 const GuidelinesShuffler = ({ data = '' }) => {
-  const { notebook } = useParams()
-  if (!data) return null
+  const { width, height } = useCurrentWindowDimensions()
+  const { notebook = '' } = useParams()
+  const safeNotebookId = notebook.replace(/[^A-Za-z_-]/g, '')
 
-  let sections = data.split(/##\s/g)
+  let sections = (data || '').split(/##\s/g)
   // parse html
   const html = markdownParser.render(sections.shift()).replace(/<h1[^>]*>/g, '<h1 class="my-5">')
 
   // get available notebookIds from ipynb filenames, one per href inside data
   // data contains markdown links
-  const availableNotebookIds = (data.match(/\[([^\]]+)\]\(([^)]+)\)/g) || []).map((href) =>
-    JSON.stringify(href),
+  const links = data.match(/\[([^\]]+)\]\(([^)]+)\)/g)
+
+  const availableNotebooks =
+    links === null
+      ? []
+      : links
+          .filter((l) => l.indexOf('.ipynb') !== -1)
+          .map((link) => {
+            const label = link.match(/\[([^\]]+)\]/).pop()
+            const key = link.match(/([A-Za-z_-]+)\.ipynb/).pop()
+            return { key, label, url: link.match(/\(([^)]+)\)/).pop() }
+          })
+  const availableNotebookIds = availableNotebooks.map((n) => n.key)
+  const notebookUrl = availableNotebooks.find((n) => n.key === safeNotebookId)?.url
+  console.info(
+    '[Guidelines] \n - safeNotebookId:',
+    safeNotebookId,
+    '\n - availableNotebookIds:',
+    availableNotebookIds,
+    '\n - notebookUrl:',
+    notebookUrl,
   )
-  // const availableNotebookIds = data
-  //   .match(/href="([^"]*)"/g)
-  //   ?.map((href) => href.replace(/href="([^"]*)"/, '$1'))
-  //   // eslint-disable-next-line no-debugger
-  //   debugger
 
   return (
-    <Container className="Guidelines page">
-      <Row>
-        <Col {...BootstrapFullColumLayout} dangerouslySetInnerHTML={{ __html: html }} />
-        <pre>
-          {JSON.stringify(notebook, null, 2)}
-          {JSON.stringify(sections, null, 2)}
-          {JSON.stringify(availableNotebookIds, null, 2)}
-        </pre>
-      </Row>
-
-      {sections.map((section, index) => (
-        <Row key={index}>
+    <GuidelinesNotebookViewer notebookUrl={notebookUrl} width={width} height={height}>
+      <Container className="Guidelines page">
+        <Row>
+          <Col {...BootstrapFullColumLayout} dangerouslySetInnerHTML={{ __html: html }} />
+        </Row>
+        <Row className="mb-5 ">
           <Col {...BootstrapFullColumLayout}>
-            <GuidelineSection section={`## ${section.trim()}`} />
+            {availableNotebooks.map((n, index) => (
+              <LangLink
+                className={`btn mx-1 btn-sm rounded ${
+                  safeNotebookId === n.key ? 'btn-secondary' : 'btn-outline-secondary'
+                }`}
+                to={`guidelines/${n.key}`}
+                key={index}
+              >
+                {n.label} {safeNotebookId !== n.key ? <Book size={16} /> : <BookOpen size={16} />}
+              </LangLink>
+            ))}
           </Col>
         </Row>
-      ))}
-    </Container>
+        {/* <Row className="p-1 pt-3 border-top border-secondary">
+          {sections.map((section, index) => (
+            <Col key={index} className="mb-5" md={{ span: 4, offset: index % 2 }}>
+              <GuidelineSection section={`## ${section.trim()}`} />
+            </Col>
+          ))}{' '}
+        </Row> */}
+      </Container>
+    </GuidelinesNotebookViewer>
   )
 }
 
