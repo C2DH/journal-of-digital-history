e9e860b02119acc8a2212abbe9ce5e8c7378e273
Daniele Guido <gui.daniele@gmail.com>
2023-06-01 17:18:37 +0200

    refactor useIpynbNotebookParagraphs hook
    To be deeply tested on the other pages

--- src/hooks/ipynb.js
+++ src/hooks/ipynb.js
@@ -1,20 +1,38 @@
-import { useRef } from 'react'
+import { useMemo } from 'react'
 import { getArticleTreeFromIpynb } from '../logic/ipynb'
 
-
 export const useIpynbNotebookParagraphs = ({ id, cells, metadata }) => {
-  const treeRef = useRef(null)
-  if (!treeRef.current || treeRef.current.id !== id) {
-    console.debug('[useIpynbNotebookParagraphs] getArticleTreeFromIpynb id:', id, 'n. cells:', cells.length)
-    if (!cells.length) {
-      return getArticleTreeFromIpynb({ id, cells, metadata })
-    }
-    treeRef.current = {
+  console.info('[useIpynbNotebookParagraphs] id:', id, 'n. cells:', cells.length)
+  const tree = useMemo(() => {
+    console.debug(
+      '[useIpynbNotebookParagraphs] getArticleTreeFromIpynb id:',
       id,
-      articleTree: getArticleTreeFromIpynb({ id, cells, metadata })
-    }
-  } else {
-    console.debug('[useIpynbNotebookParagraphs] use memoized articleTree, id:', id)
-  }
-  return treeRef.current.articleTree
+      'n. cells:',
+      cells.length,
+    )
+    return getArticleTreeFromIpynb({ id, cells, metadata })
+  }, [id])
+  // const treeRef = useRef(null)
+  // if (!treeRef.current || treeRef.current.id !== id) {
+  //   console.debug(
+  //     '[useIpynbNotebookParagraphs] getArticleTreeFromIpynb id:',
+  //     id,
+  //     'n. cells:',
+  //     cells.length,
+  //   )
+  //   if (!cells.length) {
+  //     return getArticleTreeFromIpynb({ id, cells, metadata })
+  //   }
+  //   treeRef.current = {
+  //     id,
+  //     articleTree: getArticleTreeFromIpynb({ id, cells, metadata }),
+  //   }
+  // } else {
+  //   console.debug(
+  //     '[useIpynbNotebookParagraphs] use memoized articleTree, id:',
+  //     id,
+  //     treeRef.current.id,
+  //   )
+  // }
+  return tree
 }
