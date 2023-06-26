f7261d3e0c8c7a751822900bfa5b76f2af5bbc0d
Daniele Guido <gui.daniele@gmail.com>
2023-06-26 17:15:36 +0200

    eslint and add inline scss

--- src/components/Article/ArticleCitationModal.js
+++ src/components/Article/ArticleCitationModal.js
@@ -1,24 +1,22 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Modal, ToggleButtonGroup, ToggleButton } from 'react-bootstrap'
 import Citation from '../Citation'
+import '../../styles/components/Article/ArticleCitationModal.scss'
 // import CopyToClipboardTrigger from '../CopyToClipboardTrigger'
 // import { CopyToClipboard } from 'react-copy-to-clipboard'
 // import {OverlayTrigger, Tooltip, Button} from 'react-bootstrap'
 const ReplaceSpecialChars = (text) => {
-  return text.replace(/&#38;quot;/g, "&quot;")
+  return text.replace(/&#38;quot;/g, '&quot;')
 }
 const ArticleCitationModal = (props) => {
   const choices = [
     { format: 'bibtex', label: 'bibtex' },
-    { format: 'html', template: 'apa', label: 'APA',
-      replaceFn: ReplaceSpecialChars
-    },
+    { format: 'html', template: 'apa', label: 'APA', replaceFn: ReplaceSpecialChars },
     // { format: 'html', template: 'mla', label: 'MLA' },
     // { format: 'html', template: 'vancouver', label: 'vancouver' },
-    { format: 'html', template: 'harvard1',
-      label: 'harvard', replaceFn:ReplaceSpecialChars }
+    { format: 'html', template: 'harvard1', label: 'harvard', replaceFn: ReplaceSpecialChars },
   ]
-  const [value, setValue] = useState(1);
+  const [value, setValue] = useState(1)
   const handleChange = (val) => {
     setValue(val)
   }
@@ -27,28 +25,35 @@ const ArticleCitationModal = (props) => {
       {...props}
       size="lg"
       aria-labelledby="contained-modal-title-vcenter"
-
       className="shadow"
       centered
     >
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Cite as ...
-        </Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">Cite as ...</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-      <ToggleButtonGroup type="radio" name="boh"  value={value} onChange={handleChange}>
-        {choices.map((d, i) => (
-          <ToggleButton key={i} id={`tbg-btn-${i}`} variant="outline-secondary" size="sm" value={i}>
-            {d.label}
-          </ToggleButton>
-        ))}
-      </ToggleButtonGroup>
-        <div className="mt-3 position-relative">
-         <Citation {...choices[value]} bibjson={props.bibjson} className="p-2 rounded all-copy" style={{
-          maxHeight: 100, overflow:"scroll",
-          backgroundColor: 'var(--gray-400)'
-        }} />
+        <ToggleButtonGroup type="radio" name="boh" value={value} onChange={handleChange}>
+          {choices.map((d, i) => (
+            <ToggleButton
+              key={i}
+              id={`tbg-btn-${i}`}
+              variant="outline-secondary"
+              size="sm"
+              value={i}
+            >
+              {d.label}
+            </ToggleButton>
+          ))}
+        </ToggleButtonGroup>
+        <div className="ArticleCitationModal_field mt-3 position-relative">
+          <Citation
+            {...choices[value]}
+            bibjson={props.bibjson}
+            className="p-2 rounded all-copy"
+            style={{
+              backgroundColor: 'var(--gray-400)',
+            }}
+          />
         </div>
       </Modal.Body>
     </Modal>
