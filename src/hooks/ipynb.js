90e3b513ab2e67be7f51e8b5934f90e1240b636e
Daniele Guido <gui.daniele@gmail.com>
2023-07-03 13:45:27 +0200

    add workaround for big serialisation task useIpynbNotebook
    fix #561

--- src/hooks/ipynb.js
+++ src/hooks/ipynb.js
@@ -1,5 +1,6 @@
-import { useMemo } from 'react'
+import { useMemo, useState, useEffect, useRef } from 'react'
 import { getArticleTreeFromIpynb } from '../logic/ipynb'
+import { StatusFetching, StatusIdle, StatusSuccess } from '../constants'
 
 export const useIpynbNotebookParagraphs = ({ id, cells, metadata }) => {
   console.info('[useIpynbNotebookParagraphs] \n - id:', id, '\n - n. cells:', cells.length)
@@ -15,4 +16,38 @@ export const useIpynbNotebookParagraphs = ({ id, cells, metadata }) => {
   }, [id])
   console.debug('[useIpynbNotebookParagraphs]  \n - id:', id, '\n - articleTree:', tree)
   return tree
+}
+
+export const useIpynbNotebook = ({ id, cells, metadata, enabled }) => {
+  const [status, setStatus] = useState(StatusIdle)
+  const treeId = useRef(null)
+  const timer = useRef(null)
+  const timerStatus = useRef(null)
+  const [tree, setTree] = useState(null)
+
+  useEffect(() => {
+    clearTimeout(timer.current)
+    clearTimeout(timerStatus.current)
+    if (!enabled) {
+      return
+    }
+    if (treeId.current === id) {
+      return
+    }
+    setStatus(StatusFetching)
+
+    timer.current = setTimeout(() => {
+      setTree(getArticleTreeFromIpynb({ id, cells, metadata }))
+    }, 0)
+
+    timerStatus.current = setTimeout(() => {
+      setStatus(StatusSuccess)
+    }, 100)
+    return () => {
+      clearTimeout(timer.current)
+      clearTimeout(timerStatus.current)
+    }
+  }, [id, enabled])
+
+  return { status, tree }
 }
