f98d13e733fc99e8d3312598c64f69a0abd51714
Daniele Guido <gui.daniele@gmail.com>
2023-08-23 17:07:43 +0200

    use variant btn-actions in ArticleCitation

--- src/components/Article/ArticleCitation.js
+++ src/components/Article/ArticleCitation.js
@@ -3,17 +3,19 @@ import { Button } from 'react-bootstrap'
 import { BookOpen } from 'react-feather'
 import ArticleCitationModal from './ArticleCitationModal'
 
-
 const ArticleCitation = (props) => {
   const [isVisible, setIsVisible] = useState(false)
 
   return (
     <div className="ArticleCitation" {...props}>
-      <Button size="sm" variant="outline-secondary" disabled={props.disabled} onClick={() => setIsVisible(true)}>
-        <BookOpen size={12}/>
-        <span className="ms-2">
-        Cite as ...
-        </span>
+      <Button
+        size="sm"
+        variant="actions"
+        disabled={props.disabled}
+        onClick={() => setIsVisible(true)}
+      >
+        <BookOpen size={12} />
+        <span className="ms-2">Cite as ...</span>
       </Button>
 
       <ArticleCitationModal
@@ -22,7 +24,7 @@ const ArticleCitation = (props) => {
         onHide={() => setIsVisible(false)}
       />
     </div>
-  );
+  )
 }
 
 export default ArticleCitation
