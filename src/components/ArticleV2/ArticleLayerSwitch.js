d3ea33bab8438c30d258903f8d9e5f367c5401dc
Daniele Guido <gui.daniele@gmail.com>
2023-08-23 17:11:07 +0200

    remove unused parameters

--- src/components/ArticleV2/ArticleLayerSwitch.js
+++ src/components/ArticleV2/ArticleLayerSwitch.js
@@ -1,34 +1,37 @@
 import React, { useEffect } from 'react'
-import { useQueryParams, StringParam, NumberParam, withDefault, } from 'use-query-params'
+import { useQueryParams, StringParam, NumberParam, withDefault } from 'use-query-params'
 import {
   DisplayLayerQueryParam,
   DisplayLayerNarrative,
-  DisplayLayerCellIdxQueryParam
+  DisplayLayerCellIdxQueryParam,
 } from '../../constants'
 
-const ArticleLayerSwitch = ({
-  layers=[],
-  onChange
-}) => {
-  const [{[DisplayLayerQueryParam]:layer}, setQuery] = useQueryParams({
-    [DisplayLayerCellIdxQueryParam]: withDefault(NumberParam, 0),
-    [DisplayLayerQueryParam]: withDefault(StringParam, DisplayLayerNarrative)
+const ArticleLayerSwitch = ({ layers = [], className = '', onChange }) => {
+  const [{ [DisplayLayerQueryParam]: layer }, setQuery] = useQueryParams({
+    // [DisplayLayerCellIdxQueryParam]: withDefault(NumberParam, 0),
+    [DisplayLayerQueryParam]: withDefault(StringParam, DisplayLayerNarrative),
   })
   useEffect(() => {
     if (typeof onChange === 'function') {
       onChange(layer)
-    } else {
-      console.warn('No onChange function detected, can\'t forward layer from useQueryParam', layer);
     }
   }, [layer])
 
   return (
     <>
-    {layers.map((d,i) => (
-      <div key={i} onClick={() =>setQuery({
-        [DisplayLayerQueryParam]: d
-      })}>{d}</div>
-    ))}
+      {layers.map((d, i) => (
+        <div
+          key={i}
+          className={[className, layer === d ? 'active' : ''].join('')}
+          onClick={() =>
+            setQuery({
+              [DisplayLayerQueryParam]: d,
+            })
+          }
+        >
+          {d}
+        </div>
+      ))}
     </>
   )
 }
