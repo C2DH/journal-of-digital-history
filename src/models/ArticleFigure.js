5f8e0b99d5f2f49dcb42138dfb2eae681a71c4b4
Daniele Guido <gui.daniele@gmail.com>
2023-09-28 17:03:31 +0200

    Update ArticleFigure.js

--- src/models/ArticleFigure.js
+++ src/models/ArticleFigure.js
@@ -1,3 +1,5 @@
+import { FigureRefPrefix } from '../constants'
+
 export default class ArticleFigure {
   constructor({
     ref = null, // 'figure-', // 'figure-12a' figure identifier. Must start with constants/FigureRefPrefix
@@ -6,7 +8,8 @@ export default class ArticleFigure {
     idx = -1,
     num = -1,
     isTable = false,
-    isCover = false
+    isCover = false,
+    refPrefix = FigureRefPrefix,
   }) {
     this.type = type
     this.module = module
@@ -15,11 +18,22 @@ export default class ArticleFigure {
     this.ref = ref
     this.isTable = isTable
     this.isCover = isCover
-    this.tNLabel = this.isTable ? 'numbers.table': 'numbers.figure'
-    this.tNum = typeof this.ref === 'string'
-      ? this.ref.lastIndexOf('-*') !== -1
-        ? this.num
-        : this.ref.split('-').pop()
-      : this.num
+    this.tNLabel = `numbers.${refPrefix.substring(0, refPrefix.length - 1)}`
+    // number in the table of contents. It is more important than `num` because it is used to sort the figures
+    this.refPrefix = refPrefix
+  }
+
+  setNum(num) {
+    this.num = num
+    if (typeof this.ref !== 'string') {
+      this.tNum = this.num
+    } else if (this.ref.lastIndexOf('-*') !== -1) {
+      this.tNum = this.num
+    }
+    const refNum = this.ref.split('-').pop()
+    if (isNaN(refNum)) {
+      return this.num
+    }
+    return parseInt(refNum)
   }
 }
