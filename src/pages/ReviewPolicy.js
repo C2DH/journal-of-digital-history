88d4a59be0b1ea7476a49dff0aed155c046c8d5d
Daniele Guido <gui.daniele@gmail.com>
2024-01-23 10:58:43 +0100

    Create ReviewPolicy.js

---
+++ src/pages/ReviewPolicy.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import WikiStaticPage from './WikiStaticPage'
+import { useTranslation } from 'react-i18next'
+
+
+const ReviewPolicy = () => {
+  const { t } = useTranslation()
+  return (
+    <WikiStaticPage
+      url={process.env.REACT_APP_WIKI_REVIEW_POLICY}
+    >
+      <h1 className="my-5" dangerouslySetInnerHTML={{
+        __html: t('pages.reviewPolicy.title')
+      }} />
+      <h2 className="my-5">{t('pages.reviewPolicy.subheading')}</h2>
+    </WikiStaticPage>
+  )
+}
+
+export default ReviewPolicy
