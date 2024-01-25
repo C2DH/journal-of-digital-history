0a73b783221bb59af0abbd35d880991f731c778b
Daniele Guido <gui.daniele@gmail.com>
2024-01-25 11:14:47 +0100

    add specific table css

--- src/pages/WikiStaticPage.js
+++ src/pages/WikiStaticPage.js
@@ -4,34 +4,42 @@ import { Container, Row, Col } from 'react-bootstrap'
 import StaticPageLoader from './StaticPageLoader'
 import { BootstrapFullColumLayout, StatusSuccess } from '../constants'
 import '../styles/pages/WikiStaticPage.scss'
+import PropTypes from 'prop-types'
 
 const markdownParser = MarkdownIt({
   html: false,
   linkify: true,
-  typographer: true
+  typographer: true,
 })
 
-const WikiStaticPage = ({ url='', children }) => (
+const WikiStaticPage = ({ url = '', className = '', children }) => (
   <StaticPageLoader
     url={url}
     raw
-    fakeData=''
+    fakeData=""
     delay={0}
-    Component={({ data='ciccio', status }) => (
-      <Container className="WikiStaticPage page" style={{ minHeight:'100vh' }}>
+    Component={({ data = '...', status }) => (
+      <Container className={`WikiStaticPage page ${className}`} style={{ minHeight: '100vh' }}>
         <Row>
-          <Col {...BootstrapFullColumLayout}>
-            {children}
-          </Col>
+          <Col {...BootstrapFullColumLayout}>{children}</Col>
         </Row>
         <Row>
-          <Col {...BootstrapFullColumLayout} dangerouslySetInnerHTML={{
-            __html: status === StatusSuccess ? markdownParser.render(data) : ''
-          }} />
+          <Col
+            {...BootstrapFullColumLayout}
+            dangerouslySetInnerHTML={{
+              __html: status === StatusSuccess ? markdownParser.render(data) : '',
+            }}
+          />
         </Row>
       </Container>
     )}
   />
 )
+
+WikiStaticPage.propTypes = {
+  url: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node,
+}
 
 export default WikiStaticPage
