b788ec8996930607e2a99ada12f6bb35c82aa0c9
Daniele Guido <gui.daniele@gmail.com>
2024-02-12 14:44:07 +0100

    remove date in Issue label, add name

--- src/components/Issue/IssueLabel.js
+++ src/components/Issue/IssueLabel.js
@@ -1,15 +1,21 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { useTranslation } from 'react-i18next'
 
-const IssueLabel = ({ pid, publication_date }) => {
+const IssueLabel = ({ pid = '', name }) => {
   const { t } = useTranslation()
   return (
     <React.Fragment>
       {pid.replace(/jdh0+(\d+)/, (m, n) => t('numbers.issue', { n }))}
       &nbsp;&middot;&nbsp;
-      {new Date(publication_date).getFullYear()}
+      {name}
     </React.Fragment>
   )
+}
+
+IssueLabel.propTypes = {
+  pid: PropTypes.string,
+  name: PropTypes.string,
 }
 
 export default IssueLabel
