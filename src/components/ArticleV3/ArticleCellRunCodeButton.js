7679a55b704606384301243fef7e0dca38a8740a
Daniele Guido <gui.daniele@gmail.com>
2024-07-19 11:56:14 +0200

    create simple button and related story

---
+++ src/components/ArticleV3/ArticleCellRunCodeButton.js
@@ -0,0 +1,64 @@
+import { Check, EyeClosed, OnePointCircle, PlaySolid, Xmark } from 'iconoir-react'
+import React from 'react'
+import './ArticleCellRunCodeButton.css'
+
+export const StatusIdle = 'idle'
+export const StatusScheduled = 'scheduled'
+export const StatusExecuting = 'executing'
+export const StatusSuccess = 'success'
+export const StatusError = 'error'
+export const StatusDisabled = 'disabled'
+
+export const AvailableStatuses = [
+  StatusIdle,
+  StatusScheduled,
+  StatusExecuting,
+  StatusSuccess,
+  StatusError,
+  StatusDisabled,
+]
+
+const StatusIcons = {
+  [StatusIdle]: PlaySolid,
+  [StatusExecuting]: OnePointCircle,
+  [StatusSuccess]: PlaySolid,
+  [StatusError]: Xmark,
+  [StatusScheduled]: PlaySolid,
+  [StatusDisabled]: EyeClosed,
+}
+
+const StatusLabels = {
+  [StatusIdle]: 'Run code',
+  [StatusExecuting]: 'Running',
+  [StatusSuccess]: 'Run code', // when success, you can run again ...
+  [StatusError]: 'Error',
+  [StatusScheduled]: '...',
+  [StatusDisabled]: 'Disabled',
+}
+
+const ArticleCellRunCodeButton = ({ status = StatusIdle }) => {
+  const disabled = [StatusDisabled, StatusError, StatusScheduled].includes(status)
+  const Component = StatusIcons[status]
+  const label = StatusLabels[status]
+  return (
+    <div className={`ArticleCellRunCodeButton ${status} d-flex align-items-center`}>
+      <button
+        disabled={disabled}
+        className="btn btn-sm btn-outline-white d-flex align-items-center"
+      >
+        <div className="ArticleCellRunCodeButton__iconWrapper me-2">
+          <Component height={16} width={16} />
+        </div>
+        {label}
+      </button>
+      {status === StatusSuccess && (
+        <div>
+          <Check />
+        </div>
+      )}
+      <div className="ms-2 ArticleCellRunCodeButton__status">{status}</div>
+    </div>
+  )
+}
+
+export default ArticleCellRunCodeButton
---
+++ src/stories/ArticleCellRunCodeButton.stories.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import ArticleCellRunCodeButton, {
+  StatusExecuting,
+  AvailableStatuses,
+} from '../components/ArticleV3/ArticleCellRunCodeButton'
+
+export default {
+  title: 'ArticleCell RUN CODE button',
+  component: ArticleCellRunCodeButton,
+  argTypes: {
+    status: {
+      options: AvailableStatuses,
+      control: {
+        type: 'select',
+        labels: AvailableStatuses,
+      },
+    },
+  },
+}
+
+const Template = (props) => {
+  return (
+    <div className="bg-dark p-5" style={{ height: 200 }}>
+      <ArticleCellRunCodeButton {...props} />
+    </div>
+  )
+}
+export const Default = Template.bind({})
+
+Default.args = {
+  status: StatusExecuting,
+}
