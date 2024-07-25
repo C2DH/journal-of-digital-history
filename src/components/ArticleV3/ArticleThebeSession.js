d8307b88a9dfa5ecf39b659f7e55e2886184df59
Daniele Guido <gui.daniele@gmail.com>
2024-07-25 17:07:32 +0200

    add a nice button to stop restart the kernel (upcoming)

--- src/components/ArticleV3/ArticleThebeSession.js
+++ src/components/ArticleV3/ArticleThebeSession.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import { useArticleThebe } from './ArticleThebeProvider'
+import ArticleThebeSessionButton, {
+  StatusReady,
+  StatusIdle,
+  StatusPreparing,
+} from './ArticleThebeSessionButton'
 
-const ArticleThebeSession = ({ debug = false }) => {
+const ArticleThebeSession = ({ debug = false, kernelName }) => {
   const { starting, ready } = useArticleThebe()
+  let status = StatusIdle
+
+  if (ready) {
+    status = StatusReady
+  } else if (starting) {
+    status = StatusPreparing
+  }
+
   if (debug)
     return (
       <div>
-        Hello session<pre>{JSON.stringify({ starting, ready })}</pre>{' '}
+        <ArticleThebeSessionButton status={status} kernelName={kernelName} />
       </div>
     )
   return null
