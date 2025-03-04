51f0602aa3a749e1abae27e54b3117dbaa50a9fe
salaun.marion <marion.salaun@uni.lu>
2025-03-04 11:06:15 +0100

    (feat) cleaning vite config

---
+++ src/assets/fonts/fonts.ts
@@ -0,0 +1,6 @@
+export const serifPro =
+  'https://fonts.googleapis.com/css2?family=Source+Serif+Pro:wght@400;700&display=swap'
+export const firaCode =
+  'https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;700&display=swap&subset=latin-ext'
+export const firaSans =
+  'https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;700&display=swap&subset=latin-ext'
