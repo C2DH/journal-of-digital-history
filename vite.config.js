aa8dca09b352e04b3dfeabcbf1251dcb25c8ae76
salaun.marion <marion.salaun@uni.lu>
2025-03-04 11:06:15 +0100

    (feat) cleaning vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import webfontDownload from 'vite-plugin-webfont-dl';
+import webfontDownload from 'vite-plugin-webfont-dl'
+import { serifPro, firaCode, firaSans } from './src/assets/fonts/fonts'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
@@ -17,54 +18,34 @@ export default defineConfig(({ mode }) => {
           exportType: 'default',
         },
       }),
-      webfontDownload([
-        "https://fonts.googleapis.com/css2?family=Source+Serif+Pro:wght@400;700&display=swap",
-        "https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;700&display=swap&subset=latin-ext",
-        "https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;700&display=swap&subset=latin-ext"
-      ]),
+      webfontDownload([serifPro, firaCode, firaSans]),
     ],
     server: {
       proxy: {
         '/api/explain': {
-          target: env.VITE_ENABLE_CODE_EXPLAINER_PROXY || 'http://localhost:5000',
+          target: env.VITE_ENABLE_CODE_EXPLAINER_PROXY,
           changeOrigin: true,
           pathRewrite: {
             '^/api/explain': '/explain',
           },
         },
         '/api': {
-          target: env.VITE_PROXY || 'http://localhost',
+          target: env.VITE_PROXY,
           changeOrigin: true,
         },
         '/proxy-githubusercontent': {
-          target: env.VITE_PROXY || 'http://localhost',
-          changeOrigin: true,
-        },
-      },
-    },
-    preview: {
-      proxy: {
-        '/api/explain': {
-          target: env.VITE_ENABLE_CODE_EXPLAINER_PROXY || 'http://localhost:5000',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api\/explain/, '/explain'),
-        },
-        '/api': {
-          target: env.VITE_PROXY || 'http://localhost',
-          changeOrigin: true,
-        },
-        '/proxy-githubusercontent': {
-          target: env.VITE_PROXY || 'http://localhost',
+          target: env.VITE_PROXY,
           changeOrigin: true,
         },
       },
     },
     build: {
-      minify: false,
       outDir: 'dist',
+      emptyOutDir: true,
     },
     define: {
       __APP_ENV__: JSON.stringify(env.VITE_APP_ENV),
     },
   }
 })
+
