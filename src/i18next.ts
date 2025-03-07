6c4a9c4bbd261ef05603d6f225a745dfbd4bd1d8
salaun.marion <marion.salaun@uni.lu>
2025-03-07 11:34:17 +0100

    (feat) storybook internationalisation works

---
+++ src/i18next.ts
@@ -0,0 +1,18 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import Backend from 'i18next-http-backend'
+import LanguageDetector from 'i18next-browser-languagedetector'
+
+i18n
+  .use(LanguageDetector)
+  .use(Backend)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    debug: true,
+    interpolation: {
+      escapeValue: false,
+    },
+  })
+
+export default i18n
