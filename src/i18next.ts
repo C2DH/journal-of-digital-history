9c25bce0b00a3cfc6a67a64894f4d7c42698f066
salaun.marion <marion.salaun@uni.lu>
2025-03-11 14:55:19 +0100

    (fix) move locales to src

--- src/i18next.ts
+++ src/i18next.ts
@@ -2,7 +2,7 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
-import translations from '../public/locales/en/translations.json'
+import translations from './locales/en/translations.json'
 
 import moment from 'moment'
 
