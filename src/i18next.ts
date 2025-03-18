8e642ed9051435b925c64d50c29c335f663a7618
salaun.marion <marion.salaun@uni.lu>
2025-03-18 16:28:48 +0100

    (fix) storybook internationalisation

--- src/i18next.ts
+++ src/i18next.ts
@@ -1,18 +1,27 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import Backend from 'i18next-http-backend'
-import LanguageDetector from 'i18next-browser-languagedetector'
-import translations from './locales/en/translations.json'
-
 import moment from 'moment'
+
+import translationEn from './locales/en/translation.json'
+import translationFr from './locales/fr/translation.json'
+
+const resources = {
+  en: {
+    translation: translationEn
+  },
+  fr: {
+    translation: translationFr
+  }
+};
 
 i18n
   .use(Backend)
   .use(initReactI18next)
-  .init({
-    resources: translations,
+  .init({ 
     fallbackLng: 'en',
-    debug: false,
+    resources,
+    debug: true,
     interpolation: {
       escapeValue: false,
       format: function (value, format) {
