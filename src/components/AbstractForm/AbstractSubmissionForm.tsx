62561490e7b3e80e7be6f111841c6e1cf8b51a3d
salaun.marion <marion.salaun@uni.lu>
2025-04-01 18:10:50 +0200

    (feat) dataset component works

--- src/components/AbstractForm/AbstractSubmissionForm.tsx
+++ src/components/AbstractForm/AbstractSubmissionForm.tsx
@@ -1,16 +1,24 @@
-import React, { useState } from 'react';
-import Ajv from 'ajv';
-import ajvformat from 'ajv-formats';
-import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
-import { FormData, ValidationErrors } from './interface/abstractSubmission';
-import { schema } from './schema';
+import React, { useState } from 'react'
+import Ajv from 'ajv'
+import ajvformat from 'ajv-formats'
+import { useTranslation } from 'react-i18next'
+import { Link } from 'react-router-dom'
+
+import { FormData, ValidationErrors } from './interface/abstractSubmission'
+import { schema } from './schema'
+import DatasetForm from './Dataset'
 
 function AbstractSubmissionForm() {
-  const { t } = useTranslation();
+  const { t } = useTranslation()
   const [formData, setFormData] = useState<FormData>({
     title: '',
     abstract: '',
+    dataset: [
+      {
+        link: '',
+        description: '',
+      },
+    ],
     contact: {
       firstName: '',
       lastName: '',
@@ -20,26 +28,27 @@ function AbstractSubmissionForm() {
       githubId: '',
     },
     termsAccepted: false,
-  });
-  const [errors, setErrors] = useState<ValidationErrors>({});
+  })
+  const [errors, setErrors] = useState<ValidationErrors>({})
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const ajv = new Ajv();
-    ajvformat(ajv);
-    const validate = ajv.compile(schema);
-    const isValid = validate(formData);
+    event.preventDefault()
+    const ajv = new Ajv()
+    ajvformat(ajv)
+    const validate = ajv.compile(schema)
+    const isValid = validate(formData)
 
     if (!isValid) {
-      setErrors(validate.errors as unknown as ValidationErrors);
+      console.log('Errors', validate.errors)
+      setErrors(validate.errors as unknown as ValidationErrors)
     } else {
-      console.log('Form submitted successfully:', formData);
+      console.log('Form submitted successfully:', formData)
     }
-  };
+  }
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { id, value, type } = event.target;
-    const checked = type === 'checkbox' ? (event.target as HTMLInputElement).checked : undefined;
+    const { id, value, type } = event.target
+    const checked = type === 'checkbox' ? (event.target as HTMLInputElement).checked : undefined
 
     setFormData((prev) => {
       if (id in prev.contact) {
@@ -49,111 +58,146 @@ function AbstractSubmissionForm() {
             ...prev.contact,
             [id]: value,
           },
-        };
+        }
       }
 
       return {
         ...prev,
         [id]: type === 'checkbox' ? checked : value,
-      };
-    });
-  };
+      }
+    })
+  }
 
   return (
     <form onSubmit={handleSubmit} className="container my-5">
-      <h3 className="progressiveHeading">
-        {t('pages.abstractSubmission.TitleAndAbstractSectionTitle')}
-      </h3>
-      <div className="form-group">
-        <label htmlFor="title">Title</label>
-        <input
-          type="text"
-          className="form-control"
-          id="title"
-          value={formData.title}
-          onChange={handleInputChange}
-        />
-        {errors.title && <div className="text-danger">{errors.title[0].message}</div>}
-      </div>
-      <div className="form-group">
-        <label htmlFor="abstract">Abstract</label>
-        <textarea
-          className="form-control"
-          id="abstract"
-          value={formData.abstract}
-          onChange={handleInputChange}
-        ></textarea>
-        {errors.abstract && <div className="text-danger">{errors.abstract[0].message}</div>}
+      <div className="title-abstract">
+        <h3 className="progressiveHeading">
+          {t('pages.abstractSubmission.TitleAndAbstractSectionTitle')}
+        </h3>
+        <div className="form-group">
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            value={formData.title}
+            onChange={handleInputChange}
+          />
+          {errors.title && <div className="text-danger">{errors.title[0].message}</div>}
+        </div>
+        <div className="form-group">
+          <label htmlFor="abstract">Abstract</label>
+          <textarea
+            className="form-control"
+            id="abstract"
+            value={formData.abstract}
+            onChange={handleInputChange}
+          ></textarea>
+          {errors.abstract && <div className="text-danger">{errors.abstract[0].message}</div>}
+        </div>
       </div>
       <hr />
-      <h3 className="progressiveHeading">
-        {t('pages.abstractSubmission.ContactPointSectionTitle')}
-      </h3>
-      <div className="form-group">
-        <label htmlFor="firstName">First Name</label>
-        <input
-          type="text"
-          className="form-control"
-          id="firstName"
-          value={formData.contact.firstName}
-          onChange={handleInputChange}
+      <div className="tools-code-data">
+        <DatasetForm
+          datasets={formData.dataset}
+          onChange={(index, field, value) => {
+            setFormData((prev) => {
+              const updatedDatasets = [...prev.dataset]
+              updatedDatasets[index] = { ...updatedDatasets[index], [field]: value }
+              return { ...prev, dataset: updatedDatasets }
+            })
+          }}
+          onAdd={() => {
+            setFormData((prev) => ({
+              ...prev,
+              dataset: [...prev.dataset, { link: '', description: '' }],
+            }))
+          }}
+          onRemove={(index) => {
+            setFormData((prev) => ({
+              ...prev,
+              dataset: prev.dataset.filter((_, i) => i !== index),
+            }))
+          }}
+          errors={
+            errors.dataset
+              ? Object.fromEntries(
+                  errors.dataset.map((error, index) => [index, { link: error.message, description: error.message }])
+                )
+              : undefined
+          }
         />
-        {errors.firstName && <div className="text-danger">{errors.firstName[0].message}</div>}
       </div>
-      <div className="form-group">
-        <label htmlFor="lastName">Last Name</label>
-        <input
-          type="text"
-          className="form-control"
-          id="lastName"
-          value={formData.contact.lastName}
-          onChange={handleInputChange}
-        />
-        {errors.lastName && <div className="text-danger">{errors.lastName[0].message}</div>}
-      </div>
-      <div className="form-group">
-        <label htmlFor="affiliation">Affiliation</label>
-        <input
-          type="text"
-          className="form-control"
-          id="affiliation"
-          value={formData.contact.affiliation}
-          onChange={handleInputChange}
-        />
-        {errors.affiliation && <div className="text-danger">{errors.affiliation[0].message}</div>}
-      </div>
-      <div className="form-group">
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          className="form-control"
-          id="email"
-          value={formData.contact.email}
-          onChange={handleInputChange}
-        />
-        {errors.email && <div className="text-danger">{errors.email[0].message}</div>}
-      </div>
-      <div className="form-group">
-        <label htmlFor="orcidUrl">ORCID URL</label>
-        <input
-          type="text"
-          className="form-control"
-          id="orcidUrl"
-          value={formData.contact.orcidUrl}
-          onChange={handleInputChange}
-        />
-        {errors.orcidUrl && <div className="text-danger">{errors.orcidUrl[0].message}</div>}
-      </div>
-      <div className="form-group">
-        <label htmlFor="githubId">GitHub Id</label>
-        <input
-          type="text"
-          className="form-control"
-          id="githubId"
-          value={formData.contact.githubId}
-          onChange={handleInputChange}
-        />
-        {errors.githubId && <div className="text-danger">{errors.githubId[0].message}</div>}
+      <div className="contact">
+        <h3 className="progressiveHeading">
+          {t('pages.abstractSubmission.ContactPointSectionTitle')}
+        </h3>
+        <div className="form-group">
+          <label htmlFor="firstName">First Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="firstName"
+            value={formData.contact.firstName}
+            onChange={handleInputChange}
+          />
+          {errors.firstName && <div className="text-danger">{errors.firstName[0].message}</div>}
+        </div>
+        <div className="form-group">
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="lastName"
+            value={formData.contact.lastName}
+            onChange={handleInputChange}
+          />
+          {errors.lastName && <div className="text-danger">{errors.lastName[0].message}</div>}
+        </div>
+        <div className="form-group">
+          <label htmlFor="affiliation">Affiliation</label>
+          <input
+            type="text"
+            className="form-control"
+            id="affiliation"
+            value={formData.contact.affiliation}
+            onChange={handleInputChange}
+          />
+          {errors.affiliation && <div className="text-danger">{errors.affiliation[0].message}</div>}
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            value={formData.contact.email}
+            onChange={handleInputChange}
+          />
+          {errors.email && <div className="text-danger">{errors.email[0].message}</div>}
+        </div>
+        <div className="form-group">
+          <label htmlFor="orcidUrl">ORCID URL</label>
+          <input
+            type="text"
+            className="form-control"
+            id="orcidUrl"
+            value={formData.contact.orcidUrl}
+            onChange={handleInputChange}
+          />
+          {errors.orcidUrl && <div className="text-danger">{errors.orcidUrl[0].message}</div>}
+        </div>
+        <div className="form-group">
+          <label htmlFor="githubId">GitHub Id</label>
+          <input
+            type="text"
+            className="form-control"
+            id="githubId"
+            value={formData.contact.githubId}
+            onChange={handleInputChange}
+          />
+          {errors.githubId && <div className="text-danger">{errors.githubId[0].message}</div>}
+        </div>
       </div>
       <div className="form-check">
         <input
@@ -170,13 +214,15 @@ function AbstractSubmissionForm() {
             Terms of Use
           </Link>
         </label>
-        {errors.termsAccepted && <div className="text-danger">{errors.termsAccepted[0].message}</div>}
+        {errors.termsAccepted && (
+          <div className="text-danger">{errors.termsAccepted[0].message}</div>
+        )}
       </div>
       <button type="submit" className="btn btn-primary">
         Submit
       </button>
     </form>
-  );
+  )
 }
 
-export default AbstractSubmissionForm;
+export default AbstractSubmissionForm
