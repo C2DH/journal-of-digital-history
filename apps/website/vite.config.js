177b2b383d490be633203a26ebe9d05b2431124d
salaun.marion <marion.salaun@uni.lu>
2025-05-16 11:25:15 +0200

    Stop tracking .yarn/install-state.gz

--- apps/website/vite.config.js
+++ apps/website/vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig, loadEnv } from 'vite'
-import path from 'path'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
