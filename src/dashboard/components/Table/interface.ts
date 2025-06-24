cad847b4bb0185f587a3e9c4e7725bd5ba5f3f3b
salaun.marion <marion.salaun@uni.lu>
2025-06-24 16:53:56 +0200

    (feat) Modal on all page

--- src/dashboard/components/Table/interface.ts
+++ src/dashboard/components/Table/interface.ts
@@ -6,6 +6,7 @@ export type TableProps = {
   sortOrder?: string
   setSortBy?: (header: string) => void
   setSortOrder?: (order: 'asc' | 'desc') => void
+  setModal?: (modal: { open: boolean; action?: string; row?: any }) => void
 }
 
 export interface renderCellProps {
