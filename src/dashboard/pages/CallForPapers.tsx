0ba4f8aa047a039b65e4ad4d1b823503e78dd86e
salaun.marion <marion.salaun@uni.lu>
2025-07-10 22:23:30 +0200

    (fix) published article and not separated + stories done

--- src/dashboard/pages/CallForPapers.tsx
+++ src/dashboard/pages/CallForPapers.tsx
@@ -10,7 +10,12 @@ import { Abstract, Callforpaper } from '../utils/types'
 type AbstractsByCallforpaperValue = {
   title?: string
   data: Abstract[]
-  headers?: string[]
+  error?: string
+}
+
+type AbstractsPublished = {
+  title?: string
+  data: Abstract[]
   error?: string
 }
 
@@ -25,12 +30,13 @@ const CallForPapers = () => {
   const [abstractsByCallForPaper, setAbstractsByCallForPaper] = useState<
     Record<number, AbstractsByCallforpaperValue>
   >({})
+  const [abstractsPublished, setAbstractsPublished] = useState<AbstractsPublished>({ data: [] })
 
   useEffect(() => {
     callforpapers.forEach((callforpaper) => {
       if (!abstractsByCallForPaper[callforpaper.id]) {
         api
-          .get(`/api/abstracts/?callpaper=${callforpaper.id}`)
+          .get(`/api/abstracts/?callpaper=${callforpaper.id}&status=!PUBLISHED`)
           .then((res) => {
             const data = res.data
             setAbstractsByCallForPaper((prev) => ({
@@ -53,6 +59,22 @@ const CallForPapers = () => {
           })
       }
     })
+    api
+      .get(`/api/abstracts?status=PUBLISHED&limit=1000`)
+      .then((res) => {
+        const data = res.data
+        setAbstractsPublished(() => ({
+          title: 'Abstracts published',
+          data: data.results || [],
+          error: undefined,
+        }))
+      })
+      .catch((err) => {
+        setAbstractsPublished(() => ({
+          data: [],
+          error: err.message || 'Failed to fetch abstracts',
+        }))
+      })
   }, [callforpapers])
 
   return (
@@ -66,17 +88,26 @@ const CallForPapers = () => {
         hasMore={hasMore}
         loadMore={loadMore}
       />
-      {Object.entries(abstractsByCallForPaper).map(([callforpaperId, abstracts]) => (
-        <div key={callforpaperId}>
-          <AccordeonCard
-            title={abstracts.title || ''}
-            item={`abstracts`}
-            headers={['pid', 'title', 'status']}
-            data={abstracts.data}
-            error={abstracts.error}
-          />
-        </div>
-      ))}
+      {Object.entries(abstractsByCallForPaper)
+        .sort((a, b) => Number(b[0]) - Number(a[0])) //inverse order by callforpaperId
+        .map(([callforpaperId, abstracts]) => (
+          <div key={callforpaperId}>
+            <AccordeonCard
+              title={abstracts.title || ''}
+              item={`abstracts`}
+              headers={['pid', 'title', 'status']}
+              data={abstracts.data}
+              error={abstracts.error}
+            />
+          </div>
+        ))}
+      <AccordeonCard
+        title={abstractsPublished.title || ''}
+        item={`abstracts`}
+        headers={['pid', 'title', 'status']}
+        data={abstractsPublished.data || []}
+        error={abstractsPublished.error}
+      />
     </div>
   )
 }
