8d5a22b70e8f7b49bee44f80de80605040918a69
salaun.marion <marion.salaun@uni.lu>
2025-09-04 16:19:05 +0200

    (temp) starting changes

--- src/dashboard/components/Card/Card.tsx
+++ src/dashboard/components/Card/Card.tsx
@@ -7,11 +7,8 @@ import { CardProps } from './interface'
 
 import { useInfiniteScroll } from '../../hooks/useInfiniteScroll'
 import { useItemsStore } from '../../store'
-import { isAbstract } from '../../utils/helpers/itemChecker'
 import { retrieveContactEmail } from '../../utils/helpers/retrieveContactEmail'
 import { RowCheckboxMap } from '../../utils/types'
-import ActionButtonLarge from '../Buttons/ActionButton/Large/ActionButtonLarge'
-import Counter from '../Counter/Counter'
 import Feedback from '../Feedback/Feedback'
 import Loading from '../Loading/Loading'
 import Modal from '../Modal/Modal'
@@ -104,11 +101,11 @@ const Card = ({
       />
       <div className={`${item} card`}>
         <div className="card-header">
-          <div className="card-header-title">
+          {/* <div className="card-header-title">
             <h1>{t(`${item}.item`)}</h1>
             {count && <Counter value={count} />}
-          </div>
-          {isAbstract(item) && (
+          </div> */}
+          {/* {isAbstract(item) && (
             <ActionButtonLarge
               actions={[
                 {
@@ -118,7 +115,7 @@ const Card = ({
               ]}
               active={checkedRows && Object.values(checkedRows).some((v) => v)}
             />
-          )}
+          )} */}
         </div>
         {count === 0 ? (
           <Feedback type="warning" message={'No item corresponds to your search'} />
