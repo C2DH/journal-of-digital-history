80615d10bcb16ed36383ccaebb0fc69a458ca2de
salaun.marion <marion.salaun@uni.lu>
2025-10-27 17:43:07 +0100

    (feat) dropdown menu log in JDH done

--- src/components/Me/Me.jsx
+++ src/components/Me/Me.jsx
@@ -1,9 +1,11 @@
 import '../../styles/components/Me.css'
 
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useGetJSON } from '../../logic/api/fetchData'
 import { generateColorList } from './helper'
+
+import { LogOut, OpenInBrowser, ProfileCircle } from 'iconoir-react'
 
 /**
  * React component that loads the username from the rest url /api/me.
@@ -13,6 +15,7 @@ import { generateColorList } from './helper'
  */
 const Me = () => {
   const { t } = useTranslation()
+  const [isOpen, setIsOpen] = useState(false)
   const { data, error } = useGetJSON({
     url: '/api/me',
     failSilently: true,
@@ -21,14 +24,28 @@ const Me = () => {
   console.debug('[Me]', error, data)
   if (error) return null
   if (!data) return null
+
   return (
-    <div className="Me">
+    <div className="me">
       <div>
         {t('welcomeBack')}
-        <a href="/admin" title={data.username}>
-          {data.first_name.length ? data.first_name : data.username}
-        </a>
-        <div className="Me_avatar" style={{ background: generateColorList(data.username) }}></div>
+        <button className="me-dropdown-button" onClick={() => setIsOpen(!isOpen)}>
+          {data.first_name.length ? data.first_name : data.username} {isOpen ? '▲' : '▼'}
+        </button>
+        {isOpen && (
+          <div className="me-dropdown-menu">
+            <a href={`/admin/auth/user/${data.id}/change/`} title="User settings">
+              <ProfileCircle /> My profile
+            </a>
+            <a href={`/dashboard/`} title="Switch to dashboard">
+              <OpenInBrowser /> Go to dashboard
+            </a>
+            <a href={`/admin/logout/`} title="Log out">
+              <LogOut /> Sign out
+            </a>
+          </div>
+        )}
+        <div className="me-avatar" style={{ background: generateColorList(data.username) }}></div>
       </div>
     </div>
   )
