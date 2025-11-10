d81f91628184f7cd55a3b2503695b4f79bca6d79
salaun.marion <marion.salaun@uni.lu>
2025-11-10 17:53:55 +0100

    (feat) deadline counter started

--- src/dashboard/pages/Home.tsx
+++ src/dashboard/pages/Home.tsx
@@ -8,6 +8,7 @@ import { Outlet } from 'react-router'
 import Badge from '../components/Badge/Badge'
 import CustomBarChart from '../components/CustomBarChart/CustomBarChart'
 import CustomPieChart from '../components/CustomPieChart/CustomPieChart'
+import Deadline from '../components/Deadline/Deadline'
 import SmallCard from '../components/SmallCard/SmallCard'
 import SmallTable from '../components/SmallTable /SmallTable'
 import { useSorting } from '../hooks/useSorting'
@@ -16,24 +17,28 @@ import { Abstract } from '../utils/types'
 
 const AbstractSubmittedCard = (submittedAbstracts: Abstract[]) => {
   return (
-    <SmallCard className="home-abstract-card chart">
-      <div className="home-abstract-card-header">
-        <h2 className="home-abstract-card-title">Abstracts</h2>{' '}
-        <Badge text="New" variant="accent" />
-      </div>
-      <SmallTable
-        item="abstracts"
-        headers={[
-          'pid',
-          'title',
-          'callpaper_title',
-          'submitted_date',
-          'contact_lastname',
-          'contact_firstname',
-        ]}
-        data={submittedAbstracts}
-      />
-    </SmallCard>
+    <>
+      {' '}
+      <Deadline />
+      <SmallCard className="home-abstract-card chart">
+        <div className="home-abstract-card-header">
+          <h2 className="home-abstract-card-title">Abstracts</h2>{' '}
+          <Badge text="New" variant="accent" />
+        </div>
+        <SmallTable
+          item="abstracts"
+          headers={[
+            'pid',
+            'title',
+            'callpaper_title',
+            'submitted_date',
+            'contact_lastname',
+            'contact_firstname',
+          ]}
+          data={submittedAbstracts}
+        />
+      </SmallCard>
+    </>
   )
 }
 
