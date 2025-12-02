b8a29cfd757e47fbd6af003efdf9a105963e14e1
salaun.marion <marion.salaun@uni.lu>
2025-12-02 17:51:09 +0100

    (feat) login page rebuilt with useQuery

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,7 @@
+import { useQueryClient } from '@tanstack/react-query'
 import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
+import { useNavigate } from 'react-router'
 
 import { userLoginRequest } from '../../logic/api/login'
 import Logo from '../Logo'
@@ -7,22 +9,28 @@ import './Login.css'
 
 const Login = () => {
   const { t } = useTranslation()
+  const navigate = useNavigate()
+  const qc = useQueryClient()
 
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const [error, setError] = useState<string | null>(null)
+  const [error, setError] = useState<string>('')
 
   const handleLogin = async (e) => {
     e.preventDefault()
-    setError(null)
+    setError('')
     try {
       const res = await userLoginRequest(username, password)
-      if (res && res.status === 200) {
-        window.location.reload()
+      if (res?.status === 200) {
+        qc.setQueryData(['auth:me'], username)
+        navigate('/', { replace: true })
+      } else if (res?.status === 401) {
+        setError(t('pages.login.invalidUser'))
+      } else if (res?.status === 500) {
+        setError(t('pages.login.cannotAuthenticate'))
       }
-    } catch (err) {
-      setError(t('error.invalidUser'))
-      console.error('Login error:', err)
+    } catch (err: any) {
+      console.error(err)
     }
   }
 
@@ -34,13 +42,13 @@ const Login = () => {
             <Logo />
             <h2>Journal of Digital History</h2>
           </div>
-
           {error && <div className="error">{error}</div>}
           <input
             type="text"
             placeholder="Username"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
+            autoComplete="username"
             required
           />
           <input
@@ -48,6 +56,7 @@ const Login = () => {
             placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            autoComplete="current-password"
             required
           />
           <button type="submit">{t('login.button')}</button>
