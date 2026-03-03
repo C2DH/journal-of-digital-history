8d675180a4e43201ea1d4037a7431ed53d198e0c
Frédéric Reis <frederic.reis@uni.lu>
2026-03-03 16:28:59 +0100

    #51-pm: new placeholder proposition

--- src/components/Cookies/CookiesPlaceholder.jsx
+++ src/components/Cookies/CookiesPlaceholder.jsx
@@ -6,15 +6,16 @@ const CookiesPlaceholder = ({
     height = '200px'
 }) => {
 
-  const setAcceptThirdPartyCookies = useStore((state) => state.setAcceptThirdPartyCookies);
+  // const setAcceptThirdPartyCookies = useStore((state) => state.setAcceptThirdPartyCookies);
+  const setShowCookieBanner = useStore((state) => state.setShowCookieBanner);
 
   return (
     <div style={{ height }} className={styles.CookiesPlaceholder}>
       <p>
-        <Trans i18nKey="cookies.placeholder.text">This embedded content (Vimeo, Sketchfab, etc.) is blocked to protect your privacy.</Trans>
+        <Trans i18nKey="cookies.placeholder.text">External content blocked for privacy.</Trans>
       </p>
       <p>
-        <Trans i18nKey="cookies.placeholder.consent">By clicking <a onClick={() => setAcceptThirdPartyCookies(true)}>here</a>, you consent to the use of third-party cookies on the entire website and this content will be displayed.</Trans>
+        <Trans i18nKey="cookies.placeholder.consent">Enable <a onClick={() => setShowCookieBanner(true)}>cookies</a> to view this media.</Trans>
       </p>
       </div>
   )
