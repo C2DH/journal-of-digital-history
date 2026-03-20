e2209278d7bd0e10e0c28d26d936650bc12044e9
Frédéric Reis <frederic.reis@uni.lu>
2026-03-20 16:54:02 +0100

    #51-pm: see what's new link display the cookie banner if third party cookies are refused

--- src/components/VideoRelease/VideoReleaseTrigger.jsx
+++ src/components/VideoRelease/VideoReleaseTrigger.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 import {useStore} from '../../store'
 
 const VideoReleaseTrigger = () => {
+  const acceptThirdPartyCookies = useStore((state) => state.acceptThirdPartyCookies);
   const setReleaseNotified = useStore((state) => state.setReleaseNotified)
+  const setShowCookieBanner = useStore((state) => state.setShowCookieBanner)
+
   return <a href="#" onClick={(e) => {
     e.preventDefault();
     setReleaseNotified(new Date(-1))
+
+    if (!acceptThirdPartyCookies)
+      setShowCookieBanner(true);
+
   }}>see what's new!</a>
 }
 export default VideoReleaseTrigger
