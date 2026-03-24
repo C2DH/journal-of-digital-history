69cdd10e945f2fea67615d097999586b6028bc2c
salaun.marion <marion.salaun@uni.lu>
2026-03-24 11:47:22 +0100

    (feat) Blob component created

---
+++ src/dashboard/components/Blob/Blob.tsx
@@ -0,0 +1,13 @@
+import './Blob.css'
+
+const Blob = () => {
+  return (
+    <div className="blob-container">
+      <div className="blob-bg"></div>
+      {/* <div className="shadow-container"></div> */}
+      <p className="loading-text">Loading ...</p>
+    </div>
+  )
+}
+
+export default Blob
