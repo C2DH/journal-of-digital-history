66b71c360a0c4bb4600367e302ad3d093cf8f857
Frédéric Reis <frederic.reis@uni.lu>
2026-04-20 19:05:08 +0200

    #1062: code review: use switch instead of if syntax

--- src/components/ArticleV3/ArticleThebeSession.jsx
+++ src/components/ArticleV3/ArticleThebeSession.jsx
@@ -61,32 +61,38 @@ const ArticleThebeSession = ({ debug = false, kernelName }) => {
   }
 
   const handleSession = () => {
-    if (status === StatusIdle) {
-      // Track when the user starts the kernel for the first time.
-      trackEvent({
-        category: MatomoCategoryArticleV3,
-        action: MatomoActionLaunchKernelClick,
-        name: String(kernelName || 'unknown'),
-      })
-      connectAndStart()
-    } else if (status === StatusPreparing) {
-      // Track when the user cancels the kernel while it is still starting.
-      trackEvent({
-        category: MatomoCategoryArticleV3,
-        action: MatomoActionStopKernelClick,
-        name: String(kernelName || 'unknown'),
-      })
-      shutdown()
-    } else if (status === StatusReady) {
-      // Track when the user restarts an already running kernel.
-      trackEvent({
-        category: MatomoCategoryArticleV3,
-        action: MatomoActionRestartKernelClick,
-        name: String(kernelName || 'unknown'),
-      })
-      restart()
+    switch (status) {
+      case StatusIdle:
+        // Track when the user starts the kernel for the first time.
+        trackEvent({
+          category: MatomoCategoryArticleV3,
+          action: MatomoActionLaunchKernelClick,
+          name: String(kernelName || 'unknown'),
+        });
+        connectAndStart();
+        break;
+      
+      case StatusPreparing:
+        // Track when the user cancels the kernel while it is still starting.
+        trackEvent({
+          category: MatomoCategoryArticleV3,
+          action: MatomoActionStopKernelClick,
+          name: String(kernelName || 'unknown'),
+        });
+        shutdown();
+        break;
+
+      case StatusReady:
+        // Track when the user restarts an already running kernel.
+        trackEvent({
+          category: MatomoCategoryArticleV3,
+          action: MatomoActionRestartKernelClick,
+          name: String(kernelName || 'unknown'),
+        });
+        restart();
     }
   }
+
   return (
     <div className="ArticleThebeSession p-2">
       <ArticleThebeSessionButton
