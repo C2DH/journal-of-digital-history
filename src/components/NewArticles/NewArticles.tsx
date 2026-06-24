e6e61f1bce2323db181dbd1c9821f4560befc043
salaun.marion <marion.salaun@uni.lu>
2026-06-24 11:17:17 +0200

    (fix) remove box shadow on newArticle card an fix responsive for tablet view

--- src/components/NewArticles/NewArticles.tsx
+++ src/components/NewArticles/NewArticles.tsx
@@ -1,21 +1,30 @@
 import './NewArticles.css'
 
+import { useState } from 'react'
+
+import { useIsTablet } from '../../dashboard/hooks/useIsMobile'
 import { useGetJSON } from '../../logic/api/fetchData'
 import NewArticle from './NewArticle'
 
 const NewArticles = ({ width }) => {
+  const [isTablet, setIsTablet] = useState(false)
   const { data, error, status } = useGetJSON({
-    url: `/api/articles/?limit=2&ordering=-publication_date&status=PUBLISHED`,
+    url: `/api/articles/?limit=${isTablet ? 1 : 2}&ordering=-publication_date&status=PUBLISHED`,
   })
 
   const articles = !error && status !== 'pending' ? (data?.results ?? []) : []
 
+  useIsTablet(setIsTablet)
+
   return (
-    <div className="homereel-newArticles" style={{ width: width, height: '180px' }}>
-      {articles.map((article) => (
-        <NewArticle key={article.id ?? article.abstract?.pid} article={article} />
-      ))}
-    </div>
+    <>
+      {' '}
+      <div className="homereel-newArticles" style={{ width: width, height: '180px' }}>
+        {articles.map((article) => (
+          <NewArticle key={article.abstract?.pid} article={article} />
+        ))}
+      </div>
+    </>
   )
 }
 
