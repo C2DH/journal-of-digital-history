83e80867fd9b73640d4ba2fc50b2103b56058f15
salaun.marion <marion.salaun@uni.lu>
2026-08-06 16:26:09 +0200

    (feat) redesigning month with no events with smaller border

--- src/components/Milestone/Card/MonthCard.tsx
+++ src/components/Milestone/Card/MonthCard.tsx
@@ -3,10 +3,17 @@ import './MonthCard.css'
 import EventCard from './EventCard'
 
 const MonthCard = ({ title, events }) => {
+  const noEvent = events.length === 0
+  let monthName = title
+
+  if (noEvent) {
+    monthName = title.substring(0, 3)
+  }
+
   return (
-    <div className="month-container">
-      <span className="month-title">{title}</span>
-      <div className="month-card">
+    <div className={`month-container ${noEvent ? 'disabled' : ''}`}>
+      <span className={`month-title ${noEvent ? 'disabled' : ''}`}>{monthName}</span>
+      <div className={`month-card ${noEvent ? 'disabled' : ''}`}>
         <div className="event-list">
           {events.map((event, index) => (
             <EventCard key={index} event={event} />
