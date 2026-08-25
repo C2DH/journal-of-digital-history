015ec0f5d48368bf1ee81956efeb8d7173c3490d
salaun.marion <marion.salaun@uni.lu>
2026-08-25 11:59:34 +0200

    feat(getMonths): change sorting for decreasing order for year and month

--- src/components/Milestone/helper.ts
+++ src/components/Milestone/helper.ts
@@ -4,11 +4,12 @@ export const getMonths = (timeline: any, month: number) => {
   if (!timeline) return []
   return Object.keys(timeline)
     .map(Number)
+    .sort((a, b) => b - a)
     .flatMap((year) => {
-      const start = DateTime.fromObject({ year: Number(year), month })
+      const start = DateTime.fromObject({ year: Number(year), month }).endOf('year')
 
       return Array.from({ length: month }, (_, i) => {
-        const date = start.minus({ months: month - 1 - i }).startOf('month')
+        const date = start.minus({ months: i }).startOf('month')
 
         return {
           key: date.toFormat('yyyy-MM'),
